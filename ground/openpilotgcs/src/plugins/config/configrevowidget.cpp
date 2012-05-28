f5d83ca5da86225b8bf991464604e7297c2ae842
Sambas <samik.korhonen@gmail.com>
2012-05-28 17:19:03 +0300

    Check if Homelocation is set before calibration
    Conflicts:
    
    	ground/openpilotgcs/src/plugins/config/configrevowidget.cpp

--- ground/openpilotgcs/src/plugins/config/configrevowidget.cpp
+++ ground/openpilotgcs/src/plugins/config/configrevowidget.cpp
@@ -830,8 +830,21 @@ void ConfigRevoWidget::doStartNoiseMeasurement()
     QMutexLocker lock(&sensorsUpdateLock);
     Q_UNUSED(lock);
 
-    RevoCalibration * revoCalibration = RevoCalibration::GetInstance(getObjectManager());
-    Q_ASSERT(revoCalibration);
+    HomeLocation * homeLocation = HomeLocation::GetInstance(getObjectManager());
+    Q_ASSERT(homeLocation);
+    HomeLocation::DataFields homeLocationData = homeLocation->getData();
+
+    //check if Homelocation is set
+    if(!homeLocationData.Set)
+    {
+        QMessageBox msgBox;
+        msgBox.setInformativeText(tr("<p>HomeLocation not SET.</p><p>Please set your HomeLocation and try again. Aborting calibration!</p>"));
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.setIcon(QMessageBox::Information);
+        msgBox.exec();
+        return;
+    }
 
     accel_accum_x.clear();
     accel_accum_y.clear();
