5e08231b83b0d40e94f7d76fb5f938944cc12934
James Cotton <peabody124@gmail.com>
2012-10-25 23:27:41 -0500

    Autotune + Sanitycheck: Now we can sanity check a configuration, only initialize the autotuning objects when autotune is running.  When they are available but not initialized it will block arming.
    BIG WARNING: This is imperfect.  In the case that they set up this configuration
    and then flip stabilization mode to relay tuning the board will lock up regardless.
    We need to make trying to play with invalid UAVOs not segfault to fix this.

--- flight/Modules/Stabilization/stabilization.c
+++ flight/Modules/Stabilization/stabilization.c
@@ -36,8 +36,6 @@
 #include "stabilizationsettings.h"
 #include "actuatordesired.h"
 #include "ratedesired.h"
-#include "relaytuning.h"
-#include "relaytuningsettings.h"
 #include "stabilizationdesired.h"
 #include "attitudeactual.h"
 #include "gyros.h"
@@ -125,8 +123,6 @@ int32_t StabilizationInitialize()
 
 	// Code required for relay tuning
 	sin_lookup_initalize();
-	RelayTuningSettingsInitialize();
-	RelayTuningInitialize();
 
 	return 0;
 }
