06fb1b6770d5669a197e3f762b50d394b7a9393a
James Cotton <peabody124@gmail.com>
2012-12-10 12:35:39 -0600

    AndroidGCS: Update some license information

--- androidgcs/src/org/openpilot/androidgcs/fragments/UavMapView.java
+++ androidgcs/src/org/openpilot/androidgcs/fragments/UavMapView.java
@@ -1,3 +1,25 @@
+/**
+ ******************************************************************************
+ * @file       UavMapView.java
+ * @author     PhoenixPilot, http://github.com/PhoenixPilot, Copyright (C) 2012
+ * @brief      A map view that uses OSMDroid (and caches data for off-line work)
+ * @see        The GNU Public License (GPL) Version 3
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package org.openpilot.androidgcs.fragments;
 
 import java.util.ArrayList;
