6a3e771485d6fb1aaf0661be1592ed7ca01bcc03
James Cotton <peabody124@gmail.com>
2012-12-21 11:32:04 -0600

    Fix: Rename the various tasks

--- androidgcs/src/org/openpilot/androidgcs/telemetry/tasks/ITelemTask.java
+++
@@ -1,8 +0,0 @@
-package org.openpilot.androidgcs.telemetry.tasks;
-
-import org.openpilot.uavtalk.UAVObjectManager;
-
-public interface ITelemTask {
-	public void connect(UAVObjectManager objMngr);
-	public void disconnect();
-}
