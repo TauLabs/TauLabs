ab017b4857e661b2b544739e1f2426f1086ca350
Edouard Lafargue <edouard@lafargue.name>
2013-01-03 12:03:26 -0800

    Add support for Quantec boards in GCS
    GCS now recognises the Quantec PID/VID, there is no need to pose as
    CopterControl anymore. Uploader support might not work 100% yet, working
    on it.

---
+++ ground/gcs/src/plugins/boards_quantec/Quantec.pluginspec
@@ -0,0 +1,10 @@
+<plugin name="OpenPilot" version="1.0.0" compatVersion="1.0.0">
+    <vendor>Above Ground Labs</vendor>
+    <copyright>(C) 2012 Above Ground Labs</copyright>
+    <license>The GNU Public License (GPL) Version 3</license>
+    <description>Hardware definitions for boards by the OpenPilot project</description>
+    <url>http://www.openpilot.org</url>
+    <dependencyList>
+        <dependency name="Core" version="1.0.0"/>
+    </dependencyList>
+</plugin>    
---
+++ ground/gcs/src/plugins/boards_quantec/quanton.cpp
@@ -0,0 +1,70 @@
+/**
+ ******************************************************************************
+ *
+ * @file       quanton.cpp
+ * @author     The PhoenixPilot Team, http://github.com/PhoenixPilot Copyright (C) 2013.
+ *
+ * @addtogroup GCSPlugins GCS Plugins
+ * @{
+ * @addtogroup Boards_Quantec Quantec boards support Plugin
+ * @{
+ * @brief Plugin to support boards by Quantec
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "quanton.h"
+
+/**
+ * @brief Quanton::Quanton
+ *  This is the Quanton board definition
+ */
+Quanton::Quanton(void)
+{
+    // Initialize our USB Structure definition here:
+    USBInfo board;
+    board.vendorID = 0x0fda;
+    board.productID = 0x0100;
+
+    setUSBInfo(board);
+
+}
+
+Quanton::~Quanton()
+{
+
+}
+
+QString Quanton::shortName()
+{
+    return QString("Quanton");
+}
+
+QString Quanton::boardDescription()
+{
+    return QString("The Quanton board by Quantec Gmbh");
+}
+
+/**
+ * @brief Quanton::getSupportedProtocols
+ *  TODO: this is just a stub, we'll need to extend this a lot with multi protocol support
+ * @return
+ */
+QStringList Quanton::getSupportedProtocols()
+{
+
+    return QStringList("uavtalk");
+}
