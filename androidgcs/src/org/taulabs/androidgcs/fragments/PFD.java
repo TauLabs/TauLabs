61af857614b74409944bad59ceaf410b2cd6f2ad
James Cotton <peabody124@gmail.com>
2013-02-04 21:29:20 -0600

    Android PFD: Add a heading view element
    This view is also added to the top of the PFD activity.

--- androidgcs/src/org/taulabs/androidgcs/fragments/PFD.java
+++ androidgcs/src/org/taulabs/androidgcs/fragments/PFD.java
@@ -25,6 +25,7 @@ package org.taulabs.androidgcs.fragments;
 
 import org.taulabs.androidgcs.R;
 import org.taulabs.androidgcs.views.AttitudeView;
+import org.taulabs.androidgcs.views.HeadingView;
 import org.taulabs.uavtalk.UAVObject;
 import org.taulabs.uavtalk.UAVObjectManager;
 
@@ -79,18 +80,25 @@ public class PFD extends ObjectManagerFragment {
 
 		double pitch = obj.getField("Pitch").getDouble();
 		double roll = obj.getField("Roll").getDouble();
+		double yaw = obj.getField("Yaw").getDouble();
 
 		// TODO: These checks, while sensible, are necessary because the
 		// callbacks aren't
 		// removed when we switch to different activities sharing this fragment
 		Activity parent = getActivity();
 		AttitudeView attitude = null;
-		if (parent != null)
+		HeadingView heading = null;
+		if (parent != null) {
 			attitude = (AttitudeView) parent.findViewById(R.id.attitude_view);
+			heading = (HeadingView) parent.findViewById(R.id.heading_view);
+		}
 		if (attitude != null) {
 			attitude.setRoll(roll);
 			attitude.setPitch(pitch);
 			attitude.invalidate();
+		}
+		if (heading != null) {
+			heading.setBearing(yaw);
 		}
 
 	}
