2a3c061244137725a68903942fa57e9e05953a45
lilvinz <lilvinz@nscit.com>
2013-02-06 22:04:27 +0100

    gcs translations: replaced "OpenPilot" by "Tau Labs"

--- ground/gcs/share/taulabs/translations/taulabs_fr.ts
+++ ground/gcs/share/taulabs/translations/taulabs_fr.ts
@@ -20,7 +20,7 @@
     </message>
     <message>
         <location line="+36"/>
-        <source>OpenPilot GCS - Plugin loader messages</source>
+        <source>Tau Labs GCS - Plugin loader messages</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -134,7 +134,7 @@
     </message>
     <message>
         <location line="-281"/>
-        <source>OpenPilot GCS</source>
+        <source>Tau Labs GCS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -182,12 +182,12 @@
     </message>
     <message>
         <location line="+130"/>
-        <source>About &amp;OpenPilot GCS</source>
+        <source>About &amp;Tau Labs GCS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+2"/>
-        <source>About &amp;OpenPilot GCS...</source>
+        <source>About &amp;Tau Labs GCS...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -356,7 +356,7 @@
     <name>Core::Internal::VersionDialog</name>
     <message>
         <location filename="../../../src/plugins/coreplugin/versiondialog.cpp" line="+57"/>
-        <source>About OpenPilot GCS</source>
+        <source>About Tau Labs GCS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -367,7 +367,7 @@
     </message>
     <message>
         <location line="+3"/>
-        <source>&lt;h3&gt;OpenPilot GCS %1&lt;/h3&gt;Based on Qt %2 (%3 bit)&lt;br/&gt;&lt;br/&gt;Built on %4 at %5&lt;br /&gt;&lt;br/&gt;%8&lt;br/&gt;Copyright 2008-%6 %7. All rights reserved.&lt;br/&gt;&lt;br/&gt;The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.&lt;br/&gt;</source>
+        <source>&lt;h3&gt;Tau Labs GCS %1&lt;/h3&gt;Based on Qt %2 (%3 bit)&lt;br/&gt;&lt;br/&gt;Built on %4 at %5&lt;br /&gt;&lt;br/&gt;%8&lt;br/&gt;Copyright 2008-%6 %7. All rights reserved.&lt;br/&gt;&lt;br/&gt;The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.&lt;br/&gt;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -1324,43 +1324,43 @@ Raison : %3</translation>
     </message>
     <message>
         <location filename="../../../src/plugins/welcome/communitywelcomepagewidget.cpp" line="+46"/>
-        <source>News From the OpenPilot Project</source>
+        <source>News From the Tau Labs Project</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+1"/>
-        <source>OpenPilot Websites</source>
+        <source>Tau Labs Websites</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+8"/>
-        <source>http://forums.openpilot.org/blog/rss/4-openpilot-development-blog</source>
+        <source>http://forums.Tau Labs.org/blog/rss/4-Tau Labs-development-blog</source>
         <extracomment>Add localized feed here only if one exists</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+3"/>
-        <source>OpenPilot Home</source>
+        <source>Tau Labs Home</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+1"/>
-        <source>OpenPilot Wiki</source>
+        <source>Tau Labs Wiki</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+1"/>
-        <source>OpenPilot Store</source>
+        <source>Tau Labs Store</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+1"/>
-        <source>OpenPilot Forums</source>
+        <source>Tau Labs Forums</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+1"/>
-        <source>OpenPilot Bug Tracker</source>
+        <source>Tau Labs Bug Tracker</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -1399,7 +1399,7 @@ Raison : %3</translation>
     </message>
     <message>
         <location filename="../../../src/plugins/welcome/welcomemode.ui"/>
-        <source>Help us make OpenPilot even better</source>
+        <source>Help us make Tau Labs even better</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -6170,7 +6170,7 @@ Update</source>
     </message>
     <message>
         <location line="-366"/>
-        <source>OpenPilot GCS</source>
+        <source>Tau Labs GCS</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -6627,7 +6627,7 @@ Update</source>
     <name>opmap_edit_waypoint_dialog</name>
     <message>
         <location filename="../../../src/plugins/opmap/opmap_edit_waypoint_dialog.ui"/>
-        <source>OpenPilot GCS Edit Waypoint</source>
+        <source>Tau Labs GCS Edit Waypoint</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -6731,7 +6731,7 @@ Update</source>
     <name>opmap_waypointeditor_dialog</name>
     <message>
         <location filename="../../../src/plugins/opmap/opmap_waypointeditor_dialog.ui"/>
-        <source>OpenPilot GCS Waypoint Editor</source>
+        <source>Tau Labs GCS Waypoint Editor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -7219,7 +7219,7 @@ new home location unless it is in indoor mode.</source>
     </message>
     <message>
         <location/>
-        <source>Retrieve settings from OpenPilot</source>
+        <source>Retrieve settings from Tau Labs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -7229,7 +7229,7 @@ new home location unless it is in indoor mode.</source>
     </message>
     <message>
         <location/>
-        <source>Send to OpenPilot but don&apos;t write in SD.</source>
+        <source>Send to Tau Labs but don&apos;t write in SD.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -7290,7 +7290,7 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <location/>
-        <source>Retrieve settings from OpenPilot</source>
+        <source>Retrieve settings from Tau Labs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -7311,7 +7311,7 @@ Applies and Saves all settings to SD</source>
     </message>
     <message>
         <location/>
-        <source>Send to OpenPilot but don&apos;t write in SD.
+        <source>Send to Tau Labs but don&apos;t write in SD.
 Be sure to set the Neutral position on all sliders before sending!</source>
         <translation type="unfinished"></translation>
     </message>
@@ -7342,7 +7342,7 @@ Uncheck/Check to restart calibration.</source>
     </message>
     <message>
         <location/>
-        <source>Indicates whether OpenPilot is getting a signal from the RC receiver.</source>
+        <source>Indicates whether Tau Labs is getting a signal from the RC receiver.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -7505,9 +7505,9 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Set the serial speed of your telemetry modem here. It is the speed between the OpenPilot board and the modem, and could be different from the radio link speed.&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Set the serial speed of your telemetry modem here. It is the speed between the Tau Labs board and the modem, and could be different from the radio link speed.&lt;/p&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Beware of not locking yourself out! You should only modify this setting when the OpenPilot board is connected through the USB port.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Beware of not locking yourself out! You should only modify this setting when the Tau Labs board is connected through the USB port.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -7522,7 +7522,7 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <location/>
-        <source>Send to OpenPilot but don&apos;t write in SD.
+        <source>Send to Tau Labs but don&apos;t write in SD.
 Beware of not locking yourself out!</source>
         <translation type="unfinished"></translation>
     </message>
@@ -7533,7 +7533,7 @@ Beware of not locking yourself out!</source>
     </message>
     <message>
         <location/>
-        <source>Retrieve settings from OpenPilot</source>
+        <source>Retrieve settings from Tau Labs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
