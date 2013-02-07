81086283dc1161e1c30d6fc27ea173198ed49cd5
James Cotton <peabody124@gmail.com>
2013-02-07 12:08:51 -0600

    Android Alarms: Add a widget to action bar summarizing alarms
    Work on #290

---
+++ androidgcs/src/org/taulabs/androidgcs/views/AlarmsSummary.java
@@ -0,0 +1,83 @@
+/**
+ ******************************************************************************
+ * @file       AlarmsSummary.java
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2013
+ * @brief      A view to show the current alarm status
+ * @see        The GNU Public License (GPL) Version 3
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.taulabs.androidgcs.views;
+
+import org.taulabs.androidgcs.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class AlarmsSummary extends LinearLayout {
+
+	public enum AlarmsStatus {
+		GOOD,
+		WARNING,
+		ERROR,
+		CRITICAL
+	};
+
+	public AlarmsSummary(Context context) {
+		super(context);
+		initAlarmsSummaryView(context);
+	}
+
+	public AlarmsSummary(Context context, AttributeSet ats, int defaultStyle) {
+		super(context, ats, defaultStyle);
+		initAlarmsSummaryView(context);
+	}
+
+	public AlarmsSummary(Context context, AttributeSet ats) {
+		super(context, ats);
+		initAlarmsSummaryView(context);
+	}
+
+
+	public void initAlarmsSummaryView(Context context) {
+		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.alarms_summary, this);
+		setAlarmsStatus(AlarmsStatus.GOOD);
+		invalidate();
+	}
+
+	public void setAlarmsStatus(AlarmsStatus status) {
+		TextView alarmsIcon = (TextView) findViewById(R.id.alarms_icon);
+		switch (status) {
+		case GOOD:
+			alarmsIcon.setText("G");
+			break;
+		case WARNING:
+			alarmsIcon.setText("W");
+			break;
+		case ERROR:
+			alarmsIcon.setText("E");
+			break;
+		case CRITICAL:
+			alarmsIcon.setText("C");
+			break;
+		}
+	}
+
+}
