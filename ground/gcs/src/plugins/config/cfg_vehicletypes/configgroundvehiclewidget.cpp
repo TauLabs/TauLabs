8062d2e44894a80fe0895a1cfde6739bd2a01ab1
James Cotton <peabody124@gmail.com>
2013-02-08 10:34:18 -0600

    Ground vehicle should make the motor a motor
    Previously the default configuration of a car let the motor
    run while disarmed.  This is dangerous and counterintuitive
    to the rest of the GCS.  Now motors are configured as motor
    types (duh).

--- ground/gcs/src/plugins/config/cfg_vehicletypes/configgroundvehiclewidget.cpp
+++ ground/gcs/src/plugins/config/cfg_vehicletypes/configgroundvehiclewidget.cpp
@@ -299,7 +299,7 @@ bool ConfigGroundVehicleWidget::setupGroundVehicleMotorcycle(QString airframeTyp
 
     //motor
     int channel = m_aircraft->gvMotor2ChannelBox->currentIndex()-1;
-    setMixerType(mixer, channel, VehicleConfig::MIXERTYPE_SERVO);
+    setMixerType(mixer, channel, VehicleConfig::MIXERTYPE_MOTOR);
     setMixerVectorValue(mixer, channel, VehicleConfig::MIXERVECTOR_THROTTLECURVE1, 127);
     setMixerVectorValue(mixer, channel, VehicleConfig::MIXERVECTOR_YAW, 127);
 
