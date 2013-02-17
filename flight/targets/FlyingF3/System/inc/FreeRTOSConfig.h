601f4a38e0188dd0980ab37d3d3354b38eefad1e
lilvinz <lilvinz@nscit.com>
2013-02-17 14:53:41 +0100

    Revert "all targets: reduce FreeRTOS minimal stack size to 48 words"
    This reverts commit a9943501f4d237c8f996171e8b0cbc287f9bd0b8.

--- flight/targets/FlyingF3/System/inc/FreeRTOSConfig.h
+++ flight/targets/FlyingF3/System/inc/FreeRTOSConfig.h
@@ -26,7 +26,7 @@
 #define configCPU_CLOCK_HZ						(SYSCLK_FREQ)	// really the NVIC clock ...
 #define configTICK_RATE_HZ						((portTickType )1000)
 #define configMAX_PRIORITIES					((unsigned portBASE_TYPE)5)
-#define configMINIMAL_STACK_SIZE				((unsigned short)48)
+#define configMINIMAL_STACK_SIZE				((unsigned short)512)
 #define configTOTAL_HEAP_SIZE					((size_t)(180 * 1024))	// this is minimum, not total
 #define configMAX_TASK_NAME_LEN					(16)
 
