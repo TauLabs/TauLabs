TauLabs/TauLabs

commit c94cd501392408cbff1e04a42afebb4dc38560cd

Use the same configuration gadget for CC and Revo

ground/gcs/src/plugins/config/configgadgetwidget.cpp
@@ -29,7 +29,6 @@
 #include "configgadgetwidget.h"
 
 #include "configvehicletypewidget.h"
-#include "configccattitudewidget.h"
 #include "configinputwidget.h"
 #include "configoutputwidget.h"
 #include "configstabilizationwidget.h"
@@ -38,8 +37,7 @@
 #include "configtxpidwidget.h"
 #include "config_cc_hw_widget.h"
 #include "configpipxtremewidget.h"
-#include "configrevowidget.h"
-#include "defaultattitudewidget.h"
+#include "configattitudewidget.h"
 #include "defaulthwsettingswidget.h"
 #include "uavobjectutilmanager.h"
 
@@ -94,8 +92,8 @@ ConfigGadgetWidget::ConfigGadgetWidget(QWidget *parent) : QWidget(parent)
     icon = new QIcon();
     icon->addFile(":/configgadget/images/ins_normal.png", QSize(), QIcon::Normal, QIcon::Off);
     icon->addFile(":/configgadget/images/ins_selected.png", QSize(), QIcon::Selected, QIcon::Off);
-    qwd = new DefaultAttitudeWidget(this);
-    ftw->insertTab(ConfigGadgetWidget::sensors, qwd, *icon, QString("INS"));
+    qwd = new ConfigAttitudeWidget(this);
+    ftw->insertTab(ConfigGadgetWidget::sensors, qwd, *icon, QString("Attitude"));
 
     icon = new QIcon();
     icon->addFile(":/configgadget/images/stabilization_normal.png", QSize(), QIcon::Normal, QIcon::Off);
@@ -173,19 +171,12 @@ void ConfigGadgetWidget::resizeEvent(QResizeEvent *event)
 
 void ConfigGadgetWidget::onAutopilotDisconnect() {
     ftw->setCurrentIndex(ConfigGadgetWidget::hardware);
-    ftw->removeTab(ConfigGadgetWidget::sensors);
-
-    QIcon *icon = new QIcon();
-    icon->addFile(":/configgadget/images/ins_normal.png", QSize(), QIcon::Normal, QIcon::Off);
-    icon->addFile(":/configgadget/images/ins_selected.png", QSize(), QIcon::Selected, QIcon::Off);
-    QWidget *qwd = new DefaultAttitudeWidget(this);
-    ftw->insertTab(ConfigGadgetWidget::sensors, qwd, *icon, QString("INS"));
     ftw->removeTab(ConfigGadgetWidget::hardware);
 
-    icon = new QIcon();
+    QIcon *icon = new QIcon();
     icon->addFile(":/configgadget/images/hardware_normal.png", QSize(), QIcon::Normal, QIcon::Off);
     icon->addFile(":/configgadget/images/hardware_selected.png", QSize(), QIcon::Selected, QIcon::Off);
-    qwd = new DefaultHwSettingsWidget(this, false);
+    QWidget *qwd = new DefaultHwSettingsWidget(this, false);
     ftw->insertTab(ConfigGadgetWidget::hardware, qwd, *icon, QString("Hardware"));
     ftw->setCurrentIndex(ConfigGadgetWidget::hardware);
 
@@ -203,21 +194,14 @@ void ConfigGadgetWidget::onAutopilotConnect() {
         int board = utilMngr->getBoardModel();
         if ((board & 0xff00) == 1024) {
             // CopterControl family
-            //Delete the current sensor panel, replace with CC sensor panel:
             ftw->setCurrentIndex(ConfigGadgetWidget::hardware);
-            ftw->removeTab(ConfigGadgetWidget::sensors);
-            QIcon *icon = new QIcon();
-            icon->addFile(":/configgadget/images/ins_normal.png", QSize(), QIcon::Normal, QIcon::Off);
-            icon->addFile(":/configgadget/images/ins_selected.png", QSize(), QIcon::Selected, QIcon::Off);
-            QWidget *qwd = new ConfigCCAttitudeWidget(this);
-            ftw->insertTab(ConfigGadgetWidget::sensors, qwd, *icon, QString("Attitude"));
 
             // Delete the current hardware panel, replace with CopterControl/CC3D hardware configuration
             ftw->removeTab(ConfigGadgetWidget::hardware);
-            icon = new QIcon();
+            QIcon *icon = new QIcon();
             icon->addFile(":/configgadget/images/hardware_normal.png", QSize(), QIcon::Normal, QIcon::Off);
             icon->addFile(":/configgadget/images/hardware_selected.png", QSize(), QIcon::Selected, QIcon::Off);
-            qwd = new ConfigCCHWWidget(this);
+            QWidget *qwd = new ConfigCCHWWidget(this);
             ftw->insertTab(ConfigGadgetWidget::hardware, qwd, *icon, QString("Hardware"));
             ftw->setCurrentIndex(ConfigGadgetWidget::hardware);
         } else if ((board & 0xff00) == 0x0900 || // RevoMini
@@ -228,22 +212,12 @@ void ConfigGadgetWidget::onAutopilotConnect() {
                    (board & 0xff00) == 0x8600    // Quanton
                    ) {
             // Non-CopterControl family
-            //Delete the current sensor panel, replace with INS sensor configuration
             ftw->setCurrentIndex(ConfigGadgetWidget::hardware);
-            ftw->removeTab(ConfigGadgetWidget::sensors);
-
-            QIcon *icon = new QIcon();
-            icon->addFile(":/configgadget/images/ins_normal.png", QSize(), QIcon::Normal, QIcon::Off);
-            icon->addFile(":/configgadget/images/ins_selected.png", QSize(), QIcon::Selected, QIcon::Off);
-            QWidget *qwd = new ConfigRevoWidget(this);
-            ftw->insertTab(ConfigGadgetWidget::sensors, qwd, *icon, QString("INS"));
-
-            // Delete the current hardware panel, replace with generic hardware configuration
             ftw->removeTab(ConfigGadgetWidget::hardware);
-            icon = new QIcon();
+            QIcon *icon = new QIcon();
             icon->addFile(":/configgadget/images/hardware_normal.png", QSize(), QIcon::Normal, QIcon::Off);
             icon->addFile(":/configgadget/images/hardware_selected.png", QSize(), QIcon::Normal, QIcon::On);
-            qwd = new DefaultHwSettingsWidget(this, true);
+            QWidget *qwd = new DefaultHwSettingsWidget(this);
             ftw->insertTab(ConfigGadgetWidget::hardware, qwd, *icon, QString("Hardware"));
             ftw->setCurrentIndex(ConfigGadgetWidget::hardware);
         } else {

ground/gcs/src/plugins/config/attitude.ui
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <ui version="4.0">
- <class>RevoSensorsWidget</class>
- <widget class="QWidget" name="RevoSensorsWidget">
+ <class>AttitudeWidget</class>
+ <widget class="QWidget" name="AttitudeWidget">
   <property name="geometry">
    <rect>
     <x>0</x>

ground/gcs/src/plugins/config/ccattitude.ui
@@ -1,474 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>ccattitude</class>
- <widget class="QWidget" name="ccattitude">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>711</width>
-    <height>958</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <layout class="QGridLayout" name="gridLayout_2">
-   <item row="7" column="0">
-    <layout class="QHBoxLayout" name="horizontalLayout_2">
-     <item>
-      <spacer name="horizontalSpacer">
-       <property name="orientation">
-        <enum>Qt::Horizontal</enum>
-       </property>
-       <property name="sizeHint" stdset="0">
-        <size>
-         <width>40</width>
-         <height>20</height>
-        </size>
-       </property>
-      </spacer>
-     </item>
-     <item>
-      <widget class="QPushButton" name="ccAttitudeHelp">
-       <property name="sizePolicy">
-        <sizepolicy hsizetype="Fixed" vsizetype="Fixed">
-         <horstretch>0</horstretch>
-         <verstretch>0</verstretch>
-        </sizepolicy>
-       </property>
-       <property name="maximumSize">
-        <size>
-         <width>32</width>
-         <height>32</height>
-        </size>
-       </property>
-       <property name="text">
-        <string/>
-       </property>
-       <property name="icon">
-        <iconset resource="../coreplugin/core.qrc">
-         <normaloff>:/core/images/helpicon.svg</normaloff>:/core/images/helpicon.svg</iconset>
-       </property>
-       <property name="iconSize">
-        <size>
-         <width>32</width>
-         <height>32</height>
-        </size>
-       </property>
-       <property name="flat">
-        <bool>true</bool>
-       </property>
-      </widget>
-     </item>
-     <item>
-      <widget class="QPushButton" name="applyButton">
-       <property name="text">
-        <string>Apply</string>
-       </property>
-      </widget>
-     </item>
-     <item>
-      <widget class="QPushButton" name="saveButton">
-       <property name="toolTip">
-        <string>Click to permanently save the accel bias in the CopterControl Flash.</string>
-       </property>
-       <property name="text">
-        <string>Save</string>
-       </property>
-      </widget>
-     </item>
-    </layout>
-   </item>
-   <item row="1" column="0">
-    <widget class="QGroupBox" name="groupBox_sixPointCal">
-     <property name="title">
-      <string>Six Point Calibration (not required for multirotor)</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout_7">
-      <item row="0" column="1" colspan="4">
-       <widget class="QTextEdit" name="sixPointCalibInstructions"/>
-      </item>
-      <item row="3" column="3">
-       <widget class="QPushButton" name="sixPointSave">
-        <property name="enabled">
-         <bool>false</bool>
-        </property>
-        <property name="text">
-         <string>Save Position</string>
-        </property>
-       </widget>
-      </item>
-      <item row="3" column="1">
-       <spacer name="horizontalSpacer_2">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>40</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item row="3" column="0">
-       <widget class="QPushButton" name="sixPointStart">
-        <property name="text">
-         <string>Start</string>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="0">
-       <widget class="QGraphicsView" name="sixPointHelp">
-        <property name="toolTip">
-         <string>Nice paper plane, eh?</string>
-        </property>
-        <property name="styleSheet">
-         <string notr="true">background:transparent</string>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0" colspan="5">
-       <widget class="QProgressBar" name="sixPointProgress">
-        <property name="value">
-         <number>0</number>
-        </property>
-       </widget>
-      </item>
-      <item row="3" column="2">
-       <widget class="QPushButton" name="sixPointCancel">
-        <property name="enabled">
-         <bool>false</bool>
-        </property>
-        <property name="text">
-         <string>Cancel</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item row="2" column="0">
-    <widget class="QGroupBox" name="groupBox_2">
-     <property name="title">
-      <string>Leveling Calibration</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout_6">
-      <item row="0" column="0" colspan="2">
-       <widget class="QLabel" name="label">
-        <property name="text">
-         <string>Place aircraft very flat, and then click level</string>
-        </property>
-        <property name="wordWrap">
-         <bool>true</bool>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="1">
-       <widget class="QCheckBox" name="zeroGyroBiasOnArming">
-        <property name="toolTip">
-         <string>If enabled, a fast recalibration of gyro zero point will be done
-whenever the frame is armed. Do not move the airframe while
-arming it in that case!</string>
-        </property>
-        <property name="text">
-         <string>Zero gyros while arming aircraft</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="0">
-       <widget class="QPushButton" name="levelButton">
-        <property name="toolTip">
-         <string>Launch horizontal calibration.</string>
-        </property>
-        <property name="text">
-         <string>Level</string>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0" colspan="2">
-       <widget class="QProgressBar" name="levelProgress">
-        <property name="value">
-         <number>0</number>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item row="0" column="0">
-    <widget class="QGroupBox" name="groupBox">
-     <property name="title">
-      <string>Rotate virtual attitude relative to board</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout">
-      <item row="0" column="1">
-       <widget class="QLabel" name="label_3">
-        <property name="text">
-         <string>Pitch</string>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="0">
-       <widget class="QLabel" name="label_2">
-        <property name="text">
-         <string>Roll</string>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="2">
-       <widget class="QLabel" name="label_4">
-        <property name="text">
-         <string>Yaw</string>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="2">
-       <widget class="QDoubleSpinBox" name="yawRotation">
-        <property name="minimum">
-         <double>-180.000000000000000</double>
-        </property>
-        <property name="maximum">
-         <double>180.000000000000000</double>
-        </property>
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:AttitudeSettings</string>
-          <string>fieldname:BoardRotation</string>
-          <string>element:Yaw</string>
-          <string>scale:100.0</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="1">
-       <widget class="QDoubleSpinBox" name="pitchRotation">
-        <property name="minimum">
-         <double>-90.000000000000000</double>
-        </property>
-        <property name="maximum">
-         <double>90.000000000000000</double>
-        </property>
-        <property name="singleStep">
-         <double>0.010000000000000</double>
-        </property>
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:AttitudeSettings</string>
-          <string>fieldname:BoardRotation</string>
-          <string>element:Pitch</string>
-          <string>scale:100.0</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0">
-       <widget class="QDoubleSpinBox" name="rollRotation">
-        <property name="minimum">
-         <double>-180.000000000000000</double>
-        </property>
-        <property name="maximum">
-         <double>180.000000000000000</double>
-        </property>
-        <property name="singleStep">
-         <double>0.010000000000000</double>
-        </property>
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:AttitudeSettings</string>
-          <string>fieldname:BoardRotation</string>
-          <string>element:Roll</string>
-          <string>scale:100.0</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item row="5" column="0">
-    <widget class="QGroupBox" name="groupBox_3">
-     <property name="sizePolicy">
-      <sizepolicy hsizetype="Minimum" vsizetype="Preferred">
-       <horstretch>0</horstretch>
-       <verstretch>0</verstretch>
-      </sizepolicy>
-     </property>
-     <property name="title">
-      <string>Complimentary filter tuning</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout_3">
-      <item row="2" column="0">
-       <widget class="QLabel" name="label_6">
-        <property name="text">
-         <string>Accelerometer Ki</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="1">
-       <widget class="QDoubleSpinBox" name="AccelKi">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="decimals">
-         <number>6</number>
-        </property>
-        <property name="maximum">
-         <double>0.010000000000000</double>
-        </property>
-        <property name="singleStep">
-         <double>0.000001000000000</double>
-        </property>
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:AttitudeSettings</string>
-          <string>fieldname:AccelKi</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="1">
-       <widget class="QDoubleSpinBox" name="AccelKp">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="decimals">
-         <number>3</number>
-        </property>
-        <property name="maximum">
-         <double>0.200000000000000</double>
-        </property>
-        <property name="singleStep">
-         <double>0.001000000000000</double>
-        </property>
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:AttitudeSettings</string>
-          <string>fieldname:AccelKp</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0">
-       <widget class="QLabel" name="label_5">
-        <property name="text">
-         <string>Accelerometer Kp</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="2">
-       <widget class="QLabel" name="horizontalSpacerLabel">
-        <property name="text">
-         <string/>
-        </property>
-       </widget>
-      </item>
-      <item row="3" column="0">
-       <widget class="QLabel" name="label_7">
-        <property name="text">
-         <string>Accel filter tau</string>
-        </property>
-       </widget>
-      </item>
-      <item row="3" column="1">
-       <widget class="QDoubleSpinBox" name="AccelTau">
-        <property name="decimals">
-         <number>3</number>
-        </property>
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:AttitudeSettings</string>
-          <string>fieldname:AccelTau</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item row="4" column="0">
-    <widget class="QGroupBox" name="groupBox_4">
-     <property name="title">
-      <string>Sensor Ranges (changes require reboot)</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout_4">
-      <item row="0" column="2">
-       <widget class="QLabel" name="horizontalSpacerLabel_2">
-        <property name="text">
-         <string/>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0">
-       <widget class="QLabel" name="label_9">
-        <property name="text">
-         <string>Accel Range</string>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="1">
-       <widget class="QComboBox" name="accelRange">
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:HwCopterControl</string>
-          <string>fieldname:AccelRange</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="1">
-       <widget class="QComboBox" name="gyroRange">
-        <property name="objrelation" stdset="0">
-         <stringlist>
-          <string>objname:HwCopterControl</string>
-          <string>fieldname:GyroRange</string>
-         </stringlist>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="0">
-       <widget class="QLabel" name="label_8">
-        <property name="text">
-         <string>Gyro Range</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item row="6" column="0">
-    <spacer name="verticalSpacer_2">
-     <property name="orientation">
-      <enum>Qt::Vertical</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>20</width>
-       <height>40</height>
-      </size>
-     </property>
-    </spacer>
-   </item>
-  </layout>
- </widget>
- <resources>
-  <include location="../coreplugin/core.qrc"/>
- </resources>
- <connections/>
-</ui>

ground/gcs/src/plugins/config/config.pro
@@ -19,13 +19,11 @@ HEADERS += calibration.h \
     configoutputwidget.h \
     configvehicletypewidget.h \
     config_cc_hw_widget.h \
-    configccattitudewidget.h \
     configpipxtremewidget.h \
     cfg_vehicletypes/configccpmwidget.h \
     configstabilizationwidget.h \
     assertions.h \
     calibration.h \
-    defaultattitudewidget.h \
     defaulthwsettingswidget.h \
     inputchannelform.h \
     configcamerastabilizationwidget.h \
@@ -35,7 +33,7 @@ HEADERS += calibration.h \
     cfg_vehicletypes/configgroundvehiclewidget.h \
     cfg_vehicletypes/configfixedwingwidget.h \
     cfg_vehicletypes/vehicleconfig.h \
-    configrevowidget.h \
+    configattitudewidget.h \
     config_global.h \
     mixercurve.h \
     dblspindelegate.h \
@@ -54,14 +52,12 @@ SOURCES += calibration.cpp \
     configoutputwidget.cpp \
     configvehicletypewidget.cpp \
     config_cc_hw_widget.cpp \
-    configccattitudewidget.cpp \
     configstabilizationwidget.cpp \
     configpipxtremewidget.cpp \
-    defaultattitudewidget.cpp \
     defaulthwsettingswidget.cpp \
     inputchannelform.cpp \
     configcamerastabilizationwidget.cpp \
-    configrevowidget.cpp \
+    configattitudewidget.cpp \
     configtxpidwidget.cpp \
     cfg_vehicletypes/configmultirotorwidget.cpp \
     cfg_vehicletypes/configgroundvehiclewidget.cpp \
@@ -80,13 +76,11 @@ FORMS += airframe.ui \
     stabilization.ui \
     input.ui \
     output.ui \
-    ccattitude.ui \
-    defaultattitude.ui \
     defaulthwsettings.ui \
     inputchannelform.ui \
     camerastabilization.ui \
     outputchannelform.ui \
-    revosensors.ui \
+    attitude.ui \
     txpid.ui \
     pipxtreme.ui \
     mixercurve.ui \

ground/gcs/src/plugins/config/configattitudewidget.cpp
@@ -1,8 +1,9 @@
 /**
  ******************************************************************************
  *
- * @file       ConfigRevoWidget.h
+ * @file       ConfigAttitudeWidget.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @author     PhoenixPilot, http://github.com/PhoenixPIlot Copyright (C) 2012.
  * @addtogroup GCSPlugins GCS Plugins
  * @{
  * @addtogroup ConfigPlugin Config Plugin
@@ -24,7 +25,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-#include "configrevowidget.h"
+#include "ConfigAttitudeWidget.h"
 
 #include "math.h"
 #include <QDebug>
@@ -59,7 +60,7 @@
 // Uncomment this to enable 6 point calibration on the accels
 #define SIX_POINT_CAL_ACCEL
 
-const double ConfigRevoWidget::maxVarValue = 0.1;
+const double ConfigAttitudeWidget::maxVarValue = 0.1;
 
 // *****************
 
@@ -74,9 +75,9 @@ public:
 
 // *****************
 
-ConfigRevoWidget::ConfigRevoWidget(QWidget *parent) :
+ConfigAttitudeWidget::ConfigAttitudeWidget(QWidget *parent) :
     ConfigTaskWidget(parent),
-    m_ui(new Ui_RevoSensorsWidget())
+    m_ui(new Ui_AttitudeWidget())
 {
     m_ui->setupUi(this);
 
@@ -270,13 +271,13 @@ ConfigRevoWidget::ConfigRevoWidget(QWidget *parent) :
     refreshWidgetsValues();
 }
 
-ConfigRevoWidget::~ConfigRevoWidget()
+ConfigAttitudeWidget::~ConfigAttitudeWidget()
 {
     // Do nothing
 }
 
 
-void ConfigRevoWidget::showEvent(QShowEvent *event)
+void ConfigAttitudeWidget::showEvent(QShowEvent *event)
 {
     Q_UNUSED(event)
     // Thit fitInView method should only be called now, once the
@@ -286,7 +287,7 @@ void ConfigRevoWidget::showEvent(QShowEvent *event)
     m_ui->sixPointHelp->fitInView(paperplane,Qt::KeepAspectRatio);
 }
 
-void ConfigRevoWidget::resizeEvent(QResizeEvent *event)
+void ConfigAttitudeWidget::resizeEvent(QResizeEvent *event)
 {
     Q_UNUSED(event)
     m_ui->sensorsBargraph->fitInView(sensorsBargraph, Qt::KeepAspectRatio);
@@ -296,7 +297,7 @@ void ConfigRevoWidget::resizeEvent(QResizeEvent *event)
 /**
   Rotate the paper plane
   */
-void ConfigRevoWidget::displayPlane(int position)
+void ConfigAttitudeWidget::displayPlane(int position)
 {
     QString displayElement;
     switch(position) {
@@ -331,7 +332,7 @@ void ConfigRevoWidget::displayPlane(int position)
 /**
   * Connect sensor updates and timeout for measuring the noise
   */
-void ConfigRevoWidget::doStartNoiseMeasurement()
+void ConfigAttitudeWidget::doStartNoiseMeasurement()
 {
     QMutexLocker lock(&sensorsUpdateLock);
     Q_UNUSED(lock);
@@ -394,7 +395,7 @@ void ConfigRevoWidget::doStartNoiseMeasurement()
   * Called when any of the sensors are updated.  Stores the sample for measuring the
   * variance at the end
   */
-void ConfigRevoWidget::doGetNoiseSample(UAVObject * obj)
+void ConfigAttitudeWidget::doGetNoiseSample(UAVObject * obj)
 {
     QMutexLocker lock(&sensorsUpdateLock);
     Q_UNUSED(lock);
@@ -495,7 +496,7 @@ void ConfigRevoWidget::doGetNoiseSample(UAVObject * obj)
 /**
   Draws the sensor variances bargraph
   */
-void ConfigRevoWidget::drawVariancesGraph()
+void ConfigAttitudeWidget::drawVariancesGraph()
 {
     INSSettings * insSettings = INSSettings::GetInstance(getObjectManager());
     Q_ASSERT(insSettings);
@@ -546,7 +547,7 @@ void ConfigRevoWidget::drawVariancesGraph()
   * Called by the ConfigTaskWidget parent when RevoCalibration is updated
   * to update the UI
   */
-void ConfigRevoWidget::refreshWidgetsValues(UAVObject *)
+void ConfigAttitudeWidget::refreshWidgetsValues(UAVObject *)
 {
     drawVariancesGraph();
 

ground/gcs/src/plugins/config/configattitudewidget.h
@@ -3,6 +3,7 @@
  *
  * @file       configahrstwidget.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @author     PhoenixPilot, http://github.com/PhoenixPIlot Copyright (C) 2012.
  * @addtogroup GCSPlugins GCS Plugins
  * @{
  * @addtogroup ConfigPlugin Config Plugin
@@ -24,10 +25,10 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-#ifndef CONFIGREVOWIDGET_H
-#define CONFIGREVOWIDGET_H
+#ifndef ConfigAttitudeWidget_H
+#define ConfigAttitudeWidget_H
 
-#include "ui_revosensors.h"
+#include "ui_attitude.h"
 #include "configtaskwidget.h"
 #include "calibration.h"
 
@@ -44,18 +45,18 @@
 
 class Ui_Widget;
 
-class ConfigRevoWidget: public ConfigTaskWidget
+class ConfigAttitudeWidget: public ConfigTaskWidget
 {
     Q_OBJECT
 
 public:
-    ConfigRevoWidget(QWidget *parent = 0);
-    ~ConfigRevoWidget();
+    ConfigAttitudeWidget(QWidget *parent = 0);
+    ~ConfigAttitudeWidget();
     
 private:
     void drawVariancesGraph();
 
-    Ui_RevoSensorsWidget *m_ui;
+    Ui_AttitudeWidget *m_ui;
     QGraphicsSvgItem *paperplane;
     QGraphicsSvgItem *sensorsBargraph;
     QGraphicsSvgItem *accel_x;
@@ -115,4 +116,4 @@ protected:
 
 };
 
-#endif // ConfigRevoWidget_H
+#endif // ConfigAttitudeWidget_H

commit 3bd5ca14d93b137adc5e70c77e5daff75afa0b45

Fix bug introduced by c94cd501392408cbff1e04a42afebb4dc38560cd, by whic the hardware configuration tab was no longer correctly updated on autopilot connect.

ground/gcs/src/plugins/config/configgadgetwidget.cpp
@@ -3,7 +3,7 @@
  *
  * @file       configgadgetwidget.cpp
  * @author     E. Lafargue & The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- * @author     PhoenixPilot, http://github.com/PhoenixPilot, Copyright (C) 2012
+ * @author     Tau Labs, http://www.taulabs.org, Copyright (C) 2013
  * @addtogroup GCSPlugins GCS Plugins
  * @{
  * @addtogroup ConfigPlugin Config Plugin
@@ -217,7 +217,7 @@ void ConfigGadgetWidget::onAutopilotConnect() {
             QIcon *icon = new QIcon();
             icon->addFile(":/configgadget/images/hardware_normal.png", QSize(), QIcon::Normal, QIcon::Off);
             icon->addFile(":/configgadget/images/hardware_selected.png", QSize(), QIcon::Normal, QIcon::On);
-            QWidget *qwd = new DefaultHwSettingsWidget(this);
+            QWidget *qwd = new DefaultHwSettingsWidget(this, true);
             ftw->insertTab(ConfigGadgetWidget::hardware, qwd, *icon, QString("Hardware"));
             ftw->setCurrentIndex(ConfigGadgetWidget::hardware);
         } else {