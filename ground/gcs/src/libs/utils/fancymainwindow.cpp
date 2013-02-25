c7038831e3e393871ea50836884de2c1dc697e65
James Cotton <peabody124@gmail.com>
2013-02-24 23:29:11 -0600

    Qt5: More utils updates

--- ground/gcs/src/libs/utils/fancymainwindow.cpp
+++ ground/gcs/src/libs/utils/fancymainwindow.cpp
@@ -1,61 +1,121 @@
-/**
- ******************************************************************************
- *
- * @file       fancymainwindow.cpp
- * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- *             Parts by Nokia Corporation (qt-info@nokia.com) Copyright (C) 2009.
- * @brief      
- * @see        The GNU Public License (GPL) Version 3
- * @defgroup   
- * @{
- * 
- *****************************************************************************/
-/* 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 3 of the License, or 
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
- * for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
+/****************************************************************************
+**
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+****************************************************************************/
 
 #include "fancymainwindow.h"
 
+#include "qtcassert.h"
+
+#include <QList>
+#include <QHash>
+
 #include <QAction>
-#include <QtCore/QSettings>
+#include <QContextMenuEvent>
+#include <QMenu>
 #include <QDockWidget>
-#include <QtCore/QSettings>
+#include <QSettings>
 
+static const char lockedKeyC[] = "Locked";
+static const char stateKeyC[] = "State";
+static const int settingsVersion = 2;
+static const char dockWidgetActiveState[] = "DockWidgetActiveState";
 
-using namespace Utils;
+namespace Utils {
 
-FancyMainWindow::FancyMainWindow(QWidget *parent)
-    : QMainWindow(parent),
-    m_locked(true),
-    m_handleDockVisibilityChanges(true)
+/*! \class Utils::FancyMainWindow
+
+    \brief MainWindow with dock widgets and additional "lock" functionality
+    (locking the dock widgets in place) and "reset layout" functionality.
+
+    The dock actions and the additional actions should be accessible
+    in a Window-menu.
+*/
+
+struct FancyMainWindowPrivate
 {
+    FancyMainWindowPrivate();
+
+    bool m_locked;
+    bool m_handleDockVisibilityChanges;
+
+    QAction m_menuSeparator1;
+    QAction m_toggleLockedAction;
+    QAction m_menuSeparator2;
+    QAction m_resetLayoutAction;
+    QDockWidget *m_toolBarDockWidget;
+};
+
+FancyMainWindowPrivate::FancyMainWindowPrivate() :
+    m_locked(true),
+    m_handleDockVisibilityChanges(true),
+    m_menuSeparator1(0),
+    m_toggleLockedAction(FancyMainWindow::tr("Locked"), 0),
+    m_menuSeparator2(0),
+    m_resetLayoutAction(FancyMainWindow::tr("Reset to Default Layout"), 0),
+    m_toolBarDockWidget(0)
+{
+    m_toggleLockedAction.setCheckable(true);
+    m_toggleLockedAction.setChecked(m_locked);
+    m_menuSeparator1.setSeparator(true);
+    m_menuSeparator2.setSeparator(true);
+}
+
+FancyMainWindow::FancyMainWindow(QWidget *parent) :
+    QMainWindow(parent), d(new FancyMainWindowPrivate)
+{
+    connect(&d->m_toggleLockedAction, SIGNAL(toggled(bool)),
+            this, SLOT(setLocked(bool)));
+    connect(&d->m_resetLayoutAction, SIGNAL(triggered()),
+            this, SIGNAL(resetLayout()));
+}
+
+FancyMainWindow::~FancyMainWindow()
+{
+    delete d;
 }
 
 QDockWidget *FancyMainWindow::addDockForWidget(QWidget *widget)
 {
     QDockWidget *dockWidget = new QDockWidget(widget->windowTitle(), this);
-    dockWidget->setObjectName(widget->windowTitle());
     dockWidget->setWidget(widget);
+    // Set an object name to be used in settings, derive from widget name
+    const QString objectName = widget->objectName();
+    if (objectName.isEmpty())
+        dockWidget->setObjectName(QLatin1String("dockWidget") + QString::number(dockWidgets().size() + 1));
+    else
+        dockWidget->setObjectName(objectName + QLatin1String("DockWidget"));
     connect(dockWidget->toggleViewAction(), SIGNAL(triggered()),
         this, SLOT(onDockActionTriggered()), Qt::QueuedConnection);
     connect(dockWidget, SIGNAL(visibilityChanged(bool)),
             this, SLOT(onDockVisibilityChange(bool)));
     connect(dockWidget, SIGNAL(topLevelChanged(bool)),
             this, SLOT(onTopLevelChanged()));
-    m_dockWidgets.append(dockWidget);
-    m_dockWidgetActiveState.append(true);
+    dockWidget->setProperty(dockWidgetActiveState, true);
     updateDockWidget(dockWidget);
     return dockWidget;
 }
@@ -63,16 +123,18 @@ QDockWidget *FancyMainWindow::addDockForWidget(QWidget *widget)
 void FancyMainWindow::updateDockWidget(QDockWidget *dockWidget)
 {
     const QDockWidget::DockWidgetFeatures features =
-            (m_locked) ? QDockWidget::DockWidgetClosable | QDockWidget::DockWidgetFloatable
+            (d->m_locked) ? QDockWidget::DockWidgetClosable | QDockWidget::DockWidgetFloatable
                        : QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetClosable | QDockWidget::DockWidgetFloatable;
-    QWidget *titleBarWidget = dockWidget->titleBarWidget();
-    if (m_locked && !titleBarWidget && !dockWidget->isFloating())
-        titleBarWidget = new QWidget(dockWidget);
-    else if ((!m_locked || dockWidget->isFloating()) && titleBarWidget) {
-        delete titleBarWidget;
-        titleBarWidget = 0;
+    if (dockWidget->property("managed_dockwidget").isNull()) { // for the debugger tool bar
+        QWidget *titleBarWidget = dockWidget->titleBarWidget();
+        if (d->m_locked && !titleBarWidget && !dockWidget->isFloating())
+            titleBarWidget = new QWidget(dockWidget);
+        else if ((!d->m_locked || dockWidget->isFloating()) && titleBarWidget) {
+            delete titleBarWidget;
+            titleBarWidget = 0;
+        }
+        dockWidget->setTitleBarWidget(titleBarWidget);
     }
-    dockWidget->setTitleBarWidget(titleBarWidget);
     dockWidget->setFeatures(features);
 }
 
@@ -87,11 +149,8 @@ void FancyMainWindow::onDockActionTriggered()
 
 void FancyMainWindow::onDockVisibilityChange(bool visible)
 {
-    if (!m_handleDockVisibilityChanges)
-        return;
-    QDockWidget *dockWidget = qobject_cast<QDockWidget *>(sender());
-    int index = m_dockWidgets.indexOf(dockWidget);
-    m_dockWidgetActiveState[index] = visible;
+    if (d->m_handleDockVisibilityChanges)
+        sender()->setProperty(dockWidgetActiveState, visible);
 }
 
 void FancyMainWindow::onTopLevelChanged()
@@ -102,18 +161,18 @@ void FancyMainWindow::onTopLevelChanged()
 void FancyMainWindow::setTrackingEnabled(bool enabled)
 {
     if (enabled) {
-        m_handleDockVisibilityChanges = true;
-        for (int i = 0; i < m_dockWidgets.size(); ++i)
-            m_dockWidgetActiveState[i] = m_dockWidgets[i]->isVisible();
+        d->m_handleDockVisibilityChanges = true;
+        foreach (QDockWidget *dockWidget, dockWidgets())
+            dockWidget->setProperty(dockWidgetActiveState, dockWidget->isVisible());
     } else {
-        m_handleDockVisibilityChanges = false;
+        d->m_handleDockVisibilityChanges = false;
     }
 }
 
 void FancyMainWindow::setLocked(bool locked)
 {
-    m_locked = locked;
-    foreach (QDockWidget *dockWidget, m_dockWidgets) {
+    d->m_locked = locked;
+    foreach (QDockWidget *dockWidget, dockWidgets()) {
         updateDockWidget(dockWidget);
     }
 }
@@ -130,17 +189,24 @@ void FancyMainWindow::showEvent(QShowEvent *event)
     handleVisibilityChanged(true);
 }
 
+void FancyMainWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+    QMenu *menu = createPopupMenu();
+    menu->exec(event->globalPos());
+    delete menu;
+}
+
 void FancyMainWindow::handleVisibilityChanged(bool visible)
 {
-    m_handleDockVisibilityChanges = false;
-    for (int i = 0; i < m_dockWidgets.size(); ++i) {
-        QDockWidget *dockWidget = m_dockWidgets.at(i);
+    d->m_handleDockVisibilityChanges = false;
+    foreach (QDockWidget *dockWidget, dockWidgets()) {
         if (dockWidget->isFloating()) {
-            dockWidget->setVisible(visible && m_dockWidgetActiveState.at(i));
+            dockWidget->setVisible(visible
+                && dockWidget->property(dockWidgetActiveState).toBool());
         }
     }
     if (visible)
-        m_handleDockVisibilityChanges = true;
+        d->m_handleDockVisibilityChanges = true;
 }
 
 void FancyMainWindow::saveSettings(QSettings *settings) const
@@ -153,7 +219,7 @@ void FancyMainWindow::saveSettings(QSettings *settings) const
     }
 }
 
-void FancyMainWindow::restoreSettings(QSettings *settings)
+void FancyMainWindow::restoreSettings(const QSettings *settings)
 {
     QHash<QString, QVariant> hash;
     foreach (const QString &key, settings->childKeys()) {
@@ -165,22 +231,105 @@ void FancyMainWindow::restoreSettings(QSettings *settings)
 QHash<QString, QVariant> FancyMainWindow::saveSettings() const
 {
     QHash<QString, QVariant> settings;
-    settings["State"] = saveState();
-    settings["Locked"] = m_locked;
-    for (int i = 0; i < m_dockWidgetActiveState.count(); ++i) {
-        settings[m_dockWidgets.at(i)->objectName()] =
-                           m_dockWidgetActiveState.at(i);
+    settings.insert(QLatin1String(stateKeyC), saveState(settingsVersion));
+    settings.insert(QLatin1String(lockedKeyC), d->m_locked);
+    foreach (QDockWidget *dockWidget, dockWidgets()) {
+        settings.insert(dockWidget->objectName(),
+                dockWidget->property(dockWidgetActiveState));
     }
     return settings;
 }
 
 void FancyMainWindow::restoreSettings(const QHash<QString, QVariant> &settings)
 {
-    QByteArray ba = settings.value("State", QByteArray()).toByteArray();
+    QByteArray ba = settings.value(QLatin1String(stateKeyC), QByteArray()).toByteArray();
     if (!ba.isEmpty())
-        restoreState(ba);
-    m_locked = settings.value("Locked", true).toBool();
-    for (int i = 0; i < m_dockWidgetActiveState.count(); ++i) {
-        m_dockWidgetActiveState[i] = settings.value(m_dockWidgets.at(i)->objectName(), false).toBool();
+        restoreState(ba, settingsVersion);
+    d->m_locked = settings.value(QLatin1String("Locked"), true).toBool();
+    d->m_toggleLockedAction.setChecked(d->m_locked);
+    foreach (QDockWidget *widget, dockWidgets()) {
+        widget->setProperty(dockWidgetActiveState,
+            settings.value(widget->objectName(), false));
     }
 }
+
+QList<QDockWidget *> FancyMainWindow::dockWidgets() const
+{
+    return findChildren<QDockWidget *>();
+}
+
+bool FancyMainWindow::isLocked() const
+{
+    return d->m_locked;
+}
+
+static bool actionLessThan(const QAction *action1, const QAction *action2)
+{
+    QTC_ASSERT(action1, return true);
+    QTC_ASSERT(action2, return false);
+    return action1->text().toLower() < action2->text().toLower();
+}
+
+QMenu *FancyMainWindow::createPopupMenu()
+{
+    QList<QAction *> actions;
+    QList<QDockWidget *> dockwidgets = findChildren<QDockWidget *>();
+    for (int i = 0; i < dockwidgets.size(); ++i) {
+        QDockWidget *dockWidget = dockwidgets.at(i);
+        if (dockWidget->property("managed_dockwidget").isNull()
+                && dockWidget->parentWidget() == this) {
+            actions.append(dockwidgets.at(i)->toggleViewAction());
+        }
+    }
+    qSort(actions.begin(), actions.end(), actionLessThan);
+    QMenu *menu = new QMenu(this);
+    foreach (QAction *action, actions)
+        menu->addAction(action);
+    menu->addAction(&d->m_menuSeparator1);
+    menu->addAction(&d->m_toggleLockedAction);
+    menu->addAction(&d->m_menuSeparator2);
+    menu->addAction(&d->m_resetLayoutAction);
+    return menu;
+}
+
+QAction *FancyMainWindow::menuSeparator1() const
+{
+    return &d->m_menuSeparator1;
+}
+
+QAction *FancyMainWindow::toggleLockedAction() const
+{
+    return &d->m_toggleLockedAction;
+}
+
+QAction *FancyMainWindow::menuSeparator2() const
+{
+    return &d->m_menuSeparator2;
+}
+
+QAction *FancyMainWindow::resetLayoutAction() const
+{
+    return &d->m_resetLayoutAction;
+}
+
+void FancyMainWindow::setDockActionsVisible(bool v)
+{
+    foreach (const QDockWidget *dockWidget, dockWidgets())
+        dockWidget->toggleViewAction()->setVisible(v);
+    d->m_toggleLockedAction.setVisible(v);
+    d->m_menuSeparator1.setVisible(v);
+    d->m_menuSeparator2.setVisible(v);
+    d->m_resetLayoutAction.setVisible(v);
+}
+
+QDockWidget *FancyMainWindow::toolBarDockWidget() const
+{
+    return d->m_toolBarDockWidget;
+}
+
+void FancyMainWindow::setToolBarDockWidget(QDockWidget *dock)
+{
+    d->m_toolBarDockWidget = dock;
+}
+
+} // namespace Utils
--- ground/gcs/src/libs/utils/fancymainwindow.h
+++ ground/gcs/src/libs/utils/fancymainwindow.h
@@ -1,38 +1,36 @@
-/**
- ******************************************************************************
- *
- * @file       fancymainwindow.h
- * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- *             Parts by Nokia Corporation (qt-info@nokia.com) Copyright (C) 2009.
- * @brief      
- * @see        The GNU Public License (GPL) Version 3
- * @defgroup   
- * @{
- * 
- *****************************************************************************/
-/* 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 3 of the License, or 
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
- * for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
+/****************************************************************************
+**
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+****************************************************************************/
 
 #ifndef FANCYMAINWINDOW_H
 #define FANCYMAINWINDOW_H
 
 #include "utils_global.h"
-
-#include <QtCore/QList>
-#include <QtCore/QHash>
 
 #include <QMainWindow>
 
@@ -42,31 +40,55 @@ QT_END_NAMESPACE
 
 namespace Utils {
 
+struct FancyMainWindowPrivate;
+
 class QTCREATOR_UTILS_EXPORT FancyMainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    FancyMainWindow(QWidget *parent = 0);
+    explicit FancyMainWindow(QWidget *parent = 0);
+    virtual ~FancyMainWindow();
 
+    /* The widget passed in should have an objectname set
+     * which will then be used as key for QSettings. */
     QDockWidget *addDockForWidget(QWidget *widget);
-    QList<QDockWidget *> dockWidgets() const { return m_dockWidgets; }
+    QList<QDockWidget *> dockWidgets() const;
 
     void setTrackingEnabled(bool enabled);
-    bool isLocked() const { return m_locked; }
+    bool isLocked() const;
 
     void saveSettings(QSettings *settings) const;
-    void restoreSettings(QSettings *settings);
+    void restoreSettings(const QSettings *settings);
     QHash<QString, QVariant> saveSettings() const;
     void restoreSettings(const QHash<QString, QVariant> &settings);
 
+    // Additional context menu actions
+    QAction *menuSeparator1() const;
+    QAction *toggleLockedAction() const;
+    QAction *menuSeparator2() const;
+    QAction *resetLayoutAction() const;
+
+    // Overwritten to add locked/reset.
+    virtual QMenu *createPopupMenu();
+
+
+    QDockWidget *toolBarDockWidget() const;
+    void setToolBarDockWidget(QDockWidget *dock);
+
+signals:
+    // Emitted by resetLayoutAction(). Connect to a slot
+    // restoring the default layout.
+    void resetLayout();
+
 public slots:
     void setLocked(bool locked);
+    void setDockActionsVisible(bool v);
 
 protected:
     void hideEvent(QHideEvent *event);
     void showEvent(QShowEvent *event);
-
+    void contextMenuEvent(QContextMenuEvent *event);
 private slots:
     void onDockActionTriggered();
     void onDockVisibilityChange(bool);
@@ -76,10 +98,7 @@ private:
     void updateDockWidget(QDockWidget *dockWidget);
     void handleVisibilityChanged(bool visible);
 
-    QList<QDockWidget *> m_dockWidgets;
-    QList<bool> m_dockWidgetActiveState;
-    bool m_locked;
-    bool m_handleDockVisibilityChanges; //todo
+    FancyMainWindowPrivate *d;
 };
 
 } // namespace Utils
