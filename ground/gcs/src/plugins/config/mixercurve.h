25245099c8ca4160539fa4abd560e46ab922b2d8
James Cotton <peabody124@gmail.com>
2013-02-26 11:25:34 -0600

    Fix: forgot copyright in patch two back

--- ground/gcs/src/plugins/config/mixercurve.h
+++ ground/gcs/src/plugins/config/mixercurve.h
@@ -3,6 +3,7 @@
  *
  * @file       mixercurve.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @author     Tau Labs, http://github.com/TauLabs Copyright (C) 2013.
  * @addtogroup GCSPlugins GCS Plugins
  * @{
  * @addtogroup ConfigPlugin Config Plugin
