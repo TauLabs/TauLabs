bcb20a28c0b6eba748ae05eb12827b5f00d502ed
James Cotton <peabody124@gmail.com>
2013-03-12 09:18:08 -0500

    Camera Tracking: Allow POI mode for yaw on higher end platforms
    Previously the define was hardcoded into the module which meant the
    sanity check didn't check properly and all airframes needed it.

--- flight/targets/Revolution/System/inc/pios_config.h
+++ flight/targets/Revolution/System/inc/pios_config.h
@@ -102,6 +102,8 @@
 #define PIOS_TELEM_PRIORITY_QUEUE       /* Enable a priority queue in telemetry */
 //#define PIOS_QUATERNION_STABILIZATION   /* Stabilization options */
 
+#define CAMERASTAB_POI_MODE
+
 /* Alarm Thresholds */
 #define HEAP_LIMIT_WARNING		1000
 #define HEAP_LIMIT_CRITICAL		500
