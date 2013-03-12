64b40694f4f166de718ea311cfd87f37dbbecfd1
James Cotton <peabody124@gmail.com>
2013-03-12 10:51:31 -0500

    Tablet Control: Allow control to be passed to the tablet
    When the transmitter flight mode switch is in the tablet control
    position then the tablet is processed and sets all the control
    objects.

--- flight/Modules/Control/tablet_control.c
+++ flight/Modules/Control/tablet_control.c
@@ -28,24 +28,155 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+#include "openpilot.h"
 #include "control.h"
 #include "tablet_control.h"
+
+#include "flightstatus.h"
+#include "gpsposition.h"
+#include "homelocation.h"
+#include "pathdesired.h"
+#include "positionactual.h"
+#include "tabletinfo.h"
+#include "systemsettings.h"
+
+//! Private methods
+int32_t tabletInfo_to_ned(TabletInfoData *tabletInfo, float *NED);
+
+//! Private constants
+#define HOME_ALTITUDE_OFFSET 5
+#define FOLLOWME_RADIUS      20
 
 //! Initialize the tablet controller
 int32_t tablet_control_initialize()
 {
+	TabletInfoInitialize();
 	return 0;
 }
 
 //! Process updates for the tablet controller
 int32_t tablet_control_update()
 {
+	// TODO: Determine what to do when there are insufficient updates
+	// from the tablet.  For now the transmitter is the authority so
+	// that is what determines whether to fall into failsafe.
 	return 0;
 }
 
 //! Use the values for the tablet controller
 int32_t tablet_control_select()
 {
+	TabletInfoData tabletInfo;
+	TabletInfoGet(&tabletInfo);
+
+	FlightStatusData flightStatus;
+	FlightStatusGet(&flightStatus);
+
+	PathDesiredData pathDesired;
+	PathDesiredGet(&pathDesired);
+
+	uint8_t mode = flightStatus.FlightMode;
+
+	switch(tabletInfo.TabletModeDesired) {
+		case TABLETINFO_TABLETMODEDESIRED_POSITIONHOLD:
+			if (mode != FLIGHTSTATUS_FLIGHTMODE_POSITIONHOLD) {
+				mode = FLIGHTSTATUS_FLIGHTMODE_POSITIONHOLD;
+
+				PositionActualData positionActual;
+				PositionActualGet(&positionActual);
+
+				pathDesired.End[0] = positionActual.North;
+				pathDesired.End[1] = positionActual.East;
+				pathDesired.End[2] = -HOME_ALTITUDE_OFFSET;
+				pathDesired.Mode = PATHDESIRED_MODE_FLYENDPOINT;
+				pathDesired.StartingVelocity = 5;
+				pathDesired.EndingVelocity = 5;
+
+				PathDesiredSet(&pathDesired);				
+			}
+			break;
+		case TABLETINFO_TABLETMODEDESIRED_RETURNTOHOME:
+			mode = FLIGHTSTATUS_FLIGHTMODE_POSITIONHOLD;
+
+			pathDesired.End[0] = 0;
+			pathDesired.End[1] = 0;
+			pathDesired.End[2] = -HOME_ALTITUDE_OFFSET;
+			pathDesired.Mode = PATHDESIRED_MODE_FLYENDPOINT;
+			pathDesired.StartingVelocity = 5;
+			pathDesired.EndingVelocity = 5;
+
+			PathDesiredSet(&pathDesired);
+
+			break;
+		case TABLETINFO_TABLETMODEDESIRED_RETURNTOTABLET:
+		{
+			float NED[3];
+
+			mode = FLIGHTSTATUS_FLIGHTMODE_POSITIONHOLD;
+			tabletInfo_to_ned(&tabletInfo, NED);
+
+			pathDesired.End[0] = NED[0];
+			pathDesired.End[1] = NED[1];
+			pathDesired.End[2] = -HOME_ALTITUDE_OFFSET;
+			pathDesired.Mode = PATHDESIRED_MODE_FLYENDPOINT;
+			pathDesired.StartingVelocity = 5;
+			pathDesired.EndingVelocity = 5;
+
+			PathDesiredSet(&pathDesired);
+		}
+
+			break;
+		case TABLETINFO_TABLETMODEDESIRED_PATHPLANNER:
+			mode = FLIGHTSTATUS_FLIGHTMODE_PATHPLANNER;
+			break;
+		case TABLETINFO_TABLETMODEDESIRED_FOLLOWME:
+		{
+			// Follow the tablet location at a fixed height, but always following by
+			// a set radius
+			float NED[3];
+			tabletInfo_to_ned(&tabletInfo, NED);
+
+			PositionActualData positionActual;
+			PositionActualGet(&positionActual);
+
+			float DeltaN = NED[0] - positionActual.North;
+			float DeltaE = NED[1] - positionActual.East;
+			float dist = sqrt(DeltaN * DeltaN + DeltaE * DeltaE);
+
+			// If outside the follow radius code to the nearest point on the border
+			// otherwise stay in the same location
+			if (dist > FOLLOWME_RADIUS) {
+				float frac = (dist - FOLLOWME_RADIUS) / dist;
+				pathDesired.End[0] = positionActual.North + frac * DeltaN;
+				pathDesired.End[1] = positionActual.East + frac * DeltaE;
+				pathDesired.End[2] = -HOME_ALTITUDE_OFFSET;
+				pathDesired.Mode = PATHDESIRED_MODE_FLYENDPOINT;
+			} else {
+				pathDesired.End[0] = positionActual.North;
+				pathDesired.End[1] = positionActual.East;
+				pathDesired.End[2] = -HOME_ALTITUDE_OFFSET;
+				pathDesired.Mode = PATHDESIRED_MODE_FLYENDPOINT;
+			}
+			pathDesired.StartingVelocity = 5;
+			pathDesired.EndingVelocity = 5;
+
+			PathDesiredSet(&pathDesired);
+		}
+			break;
+		case TABLETINFO_TABLETMODEDESIRED_LAND:
+		default:
+			AlarmsSet(SYSTEMALARMS_ALARM_MANUALCONTROL, SYSTEMALARMS_ALARM_ERROR);
+			break;
+	}
+
+	// Update mode if changed
+	if (mode != flightStatus.FlightMode) {
+		flightStatus.FlightMode = mode;
+		FlightStatusSet(&flightStatus);
+	}		
+
+	return 0;
+
 	return 0;
 }
 
@@ -53,4 +184,43 @@ int32_t tablet_control_select()
 enum control_events tablet_control_get_events()
 {
 	return CONTROL_EVENTS_DISARM;
+}
+
+
+/**
+ * Convert the tablet information (in LLA * 10e6) to NED reference frame
+ * @param[in] tabletInfo The information from the tablet
+ * @param[out] NED Computed NED values
+ */
+int32_t tabletInfo_to_ned(TabletInfoData *tabletInfo, float *NED)
+{
+	// TODO: Abstract out this code and also precompute the part based
+	// on home location.
+
+	HomeLocationData homeLocation;
+	HomeLocationGet(&homeLocation);
+
+	GPSPositionData gpsPosition;
+	GPSPositionGet(&gpsPosition);
+
+	const float DEG2RAD = 3.141592653589793f / 180.0f;
+	float lat = homeLocation.Latitude / 10.0e6f * DEG2RAD;
+	float alt = homeLocation.Altitude;
+
+	float T[3];
+	T[0] = alt+6.378137E6f;
+	T[1] = cosf(lat)*(alt+6.378137E6f);
+	T[2] = -1.0f;
+
+	// Tablet altitude is in WSG84 but we use height above the geoid elsewhere so use the
+	// GPS GeoidSeparatino as a proxy
+	float dL[3] = {(tabletInfo->Latitude - homeLocation.Latitude) / 10.0e6f * DEG2RAD,
+		(tabletInfo->Longitude - homeLocation.Longitude) / 10.0e6f * DEG2RAD,
+		(tabletInfo->Altitude + gpsPosition.GeoidSeparation - homeLocation.Altitude)};
+
+	NED[0] = T[0] * dL[0];
+	NED[1] = T[1] * dL[1];
+	NED[2] = T[2] * dL[2];
+
+	return 0;
 }
