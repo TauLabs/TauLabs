5c978740ec14c2d9776095d62f1f492f606446ed
guilhermito <guilhermito.santiago@gmail.com>
2013-03-25 14:17:53 +0000

    PIOS DMA
    Changed interrupt subscription functions to support up to 3 subscribers

--- flight/PiOS/STM32F30x/pios_dma_hooks.c
+++ flight/PiOS/STM32F30x/pios_dma_hooks.c
@@ -26,7 +26,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc., 
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-
+#define HANDLER(X)      if(pios_dma_handler_map[X][0]==NULL)PIOS_DMA_Default_Handler();for(uint8_t i=0;i<PIOS_DMA_MAX_HANDLERS_PER_CHANNEL;++i){if(pios_dma_handler_map[X][i]==NULL){return;}else{pios_dma_handler_map[X][i]();}}
 #include <pios_dma.h>
 
 static void PIOS_DMA_11_irq_handler(void);
@@ -41,6 +41,7 @@ static void PIOS_DMA_22_irq_handler(void);
 static void PIOS_DMA_23_irq_handler(void);
 static void PIOS_DMA_24_irq_handler(void);
 static void PIOS_DMA_25_irq_handler(void);
+
 void DMA1_Channel1_IRQHandler() __attribute__ ((alias ("PIOS_DMA_11_irq_handler")));
 void DMA1_Channel2_IRQHandler() __attribute__ ((alias ("PIOS_DMA_12_irq_handler")));
 void DMA1_Channel3_IRQHandler() __attribute__ ((alias ("PIOS_DMA_13_irq_handler")));
@@ -55,38 +56,38 @@ void DMA2_Channel4_IRQHandler() __attribute__ ((alias ("PIOS_DMA_24_irq_handler"
 void DMA2_Channel5_IRQHandler() __attribute__ ((alias ("PIOS_DMA_25_irq_handler")));
 
 static void PIOS_DMA_11_irq_handler(void){
-        pios_dma_handler_map[0]();
+        HANDLER(0);
 }
 static void PIOS_DMA_12_irq_handler(void){
-        pios_dma_handler_map[1]();
+        HANDLER(1);
 }
 static void PIOS_DMA_13_irq_handler(void){
-        pios_dma_handler_map[2]();
+        HANDLER(2);
 }
 static void PIOS_DMA_14_irq_handler(void){
-        pios_dma_handler_map[3]();
+        HANDLER(3);
 }
 static void PIOS_DMA_15_irq_handler(void){
-        pios_dma_handler_map[4]();
+        HANDLER(4);
 }
 static void PIOS_DMA_16_irq_handler(void){
-        pios_dma_handler_map[5]();
+        HANDLER(5);
 }
 static void PIOS_DMA_17_irq_handler(void){
-        pios_dma_handler_map[6]();
+        HANDLER(6);
 }
 static void PIOS_DMA_21_irq_handler(void){
-        pios_dma_handler_map[7]();
+        HANDLER(7);
 }
 static void PIOS_DMA_22_irq_handler(void){
-        pios_dma_handler_map[8]();
+        HANDLER(8);
 }
 static void PIOS_DMA_23_irq_handler(void){
-        pios_dma_handler_map[9]();
+        HANDLER(9);
 }
 static void PIOS_DMA_24_irq_handler(void){
-        pios_dma_handler_map[10]();
+        HANDLER(10);
 }
 static void PIOS_DMA_25_irq_handler(void){
-        pios_dma_handler_map[11]();
+        HANDLER(11);
 }
