47d1ab78474c72bd31c9665cd0c31da088160d53
guilhermito <guilhermito.santiago@gmail.com>
2013-03-27 16:52:55 +0000

    Flight/F3 ADC-Added new functions to the driver
    Tested with master only, 1 and 2 channels.

--- flight/PiOS/inc/pios_internal_adc_priv.h
+++ flight/PiOS/inc/pios_internal_adc_priv.h
@@ -55,7 +55,7 @@ struct pios_internal_adc_cfg {
 	uint16_t max_downsample;
 	uint32_t oversampling;
 	uint8_t number_of_used_pins;
-        adc_pin adc_pins[];
+        adc_pin *adc_pins;
 };
 
 extern int32_t PIOS_INTERNAL_ADC_Init(uint32_t * internal_adc_id, const struct pios_internal_adc_cfg * cfg);
