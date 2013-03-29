37615d0ae9aa695215f5186a8fd309900a090ab3
James Cotton <peabody124@gmail.com>
2013-03-29 10:28:39 -0500

    Revert "Revert "GCS: Remove MyListWidget""
    This reverts commit 792a0d800fd30e9989b246094f810582c5c1c1af.
    
    I thought removing MyListWidget (which inherits a class and
    does nothing) was breaking the layout, but actually it just
    depends on if the tab bar is visible when onConnected occurs.

--- ground/gcs/src/libs/utils/mytabbedstackwidget.cpp
+++ ground/gcs/src/libs/utils/mytabbedstackwidget.cpp
@@ -36,8 +36,7 @@ MyTabbedStackWidget::MyTabbedStackWidget(QWidget *parent, bool isVertical, bool 
       m_vertical(isVertical),
       m_iconAbove(iconAbove)
 {
-    m_listWidget = new MyListWidget(this);
-    m_listWidget->setIconAbove(m_iconAbove);
+    m_listWidget = new QListWidget(this);
     m_stackWidget = new QStackedWidget();
     m_stackWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
