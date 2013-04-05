21834ce1c4a31e5909d4b5b408e1ca68bce2174c
James Cotton <peabody124@gmail.com>
2013-04-05 13:19:39 -0500

    Sparky: Probe for MPU9150 before using MPU6050
    The MPU9150 is essentially an MPU6050 with an additional mag
    inside so probe for it before the MPU6050.  This might not be
    the way to go in the long run - we might want a separate driver
    for the mag and MPU6050 and run them independently.  However a
    little code duplication means we can avoid situations of bus
    contention more easily.

--- flight/targets/Sparky/System/pios_board.c
+++ flight/targets/Sparky/System/pios_board.c
@@ -606,46 +606,99 @@ void PIOS_Board_Init(void) {
 	PIOS_DELAY_WaitmS(200);
 	PIOS_WDG_Clear();
 
-#if defined(PIOS_INCLUDE_MPU6050)
+	bool mpu9150_found = false;
+#if defined(PIOS_INCLUDE_MPU9150)
 
-	if (PIOS_MPU6050_Init(pios_i2c_internal_id, PIOS_MPU6050_I2C_ADD_A0_LOW, &pios_mpu6050_cfg) != 0)
-		panic(2);
-	if (PIOS_MPU6050_Test() != 0)
-		panic(2);
+	if (PIOS_MPU9150_Probe(pios_i2c_internal_id, PIOS_MPU6050_I2C_ADD_A0_LOW) == 0) {
+		mpu9150_found = true;
 
-	// To be safe map from UAVO enum to driver enum
-	uint8_t hw_gyro_range;
-	HwSparkyGyroRangeGet(&hw_gyro_range);
-	switch(hw_gyro_range) {
-		case HWSPARKY_GYRORANGE_250:
-			PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_250_DEG);
-			break;
-		case HWSPARKY_GYRORANGE_500:
-			PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_500_DEG);
-			break;
-		case HWSPARKY_GYRORANGE_1000:
-			PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_1000_DEG);
-			break;
-		case HWSPARKY_GYRORANGE_2000:
-			PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_2000_DEG);
-			break;
+		if (PIOS_MPU9150_Init(pios_i2c_internal_id, PIOS_MPU6050_I2C_ADD_A0_LOW, &pios_mpu6050_cfg) != 0)
+			panic(2);
+		if (PIOS_MPU9150_Test() != 0)
+			panic(2);
+
+		// To be safe map from UAVO enum to driver enum
+		uint8_t hw_gyro_range;
+		HwSparkyGyroRangeGet(&hw_gyro_range);
+		switch(hw_gyro_range) {
+			case HWSPARKY_GYRORANGE_250:
+				PIOS_MPU9150_SetGyroRange(PIOS_MPU60X0_SCALE_250_DEG);
+				break;
+			case HWSPARKY_GYRORANGE_500:
+				PIOS_MPU9150_SetGyroRange(PIOS_MPU60X0_SCALE_500_DEG);
+				break;
+			case HWSPARKY_GYRORANGE_1000:
+				PIOS_MPU9150_SetGyroRange(PIOS_MPU60X0_SCALE_1000_DEG);
+				break;
+			case HWSPARKY_GYRORANGE_2000:
+				PIOS_MPU9150_SetGyroRange(PIOS_MPU60X0_SCALE_2000_DEG);
+				break;
+		}
+
+		uint8_t hw_accel_range;
+		HwSparkyAccelRangeGet(&hw_accel_range);
+		switch(hw_accel_range) {
+			case HWSPARKY_ACCELRANGE_2G:
+				PIOS_MPU9150_SetAccelRange(PIOS_MPU60X0_ACCEL_2G);
+				break;
+			case HWSPARKY_ACCELRANGE_4G:
+				PIOS_MPU9150_SetAccelRange(PIOS_MPU60X0_ACCEL_4G);
+				break;
+			case HWSPARKY_ACCELRANGE_8G:
+				PIOS_MPU9150_SetAccelRange(PIOS_MPU60X0_ACCEL_8G);
+				break;
+			case HWSPARKY_ACCELRANGE_16G:
+				PIOS_MPU9150_SetAccelRange(PIOS_MPU60X0_ACCEL_16G);
+				break;
+		}
 	}
 
-	uint8_t hw_accel_range;
-	HwSparkyAccelRangeGet(&hw_accel_range);
-	switch(hw_accel_range) {
-		case HWSPARKY_ACCELRANGE_2G:
-			PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_2G);
-			break;
-		case HWSPARKY_ACCELRANGE_4G:
-			PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_4G);
-			break;
-		case HWSPARKY_ACCELRANGE_8G:
-			PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_8G);
-			break;
-		case HWSPARKY_ACCELRANGE_16G:
-			PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_16G);
-			break;
+#endif /* PIOS_INCLUDE_MPU9150 */
+
+#if defined(PIOS_INCLUDE_MPU6050)
+
+	// MPU9150 looks like an MPU6050 _plus_ additional hardware.  So we cannot try and
+	// probe if MPU9150 is found or we will find a duplicate
+	if (mpu9150_found == false) {
+		if (PIOS_MPU6050_Init(pios_i2c_internal_id, PIOS_MPU6050_I2C_ADD_A0_LOW, &pios_mpu6050_cfg) != 0)
+			panic(2);
+		if (PIOS_MPU6050_Test() != 0)
+			panic(2);
+
+		// To be safe map from UAVO enum to driver enum
+		uint8_t hw_gyro_range;
+		HwSparkyGyroRangeGet(&hw_gyro_range);
+		switch(hw_gyro_range) {
+			case HWSPARKY_GYRORANGE_250:
+				PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_250_DEG);
+				break;
+			case HWSPARKY_GYRORANGE_500:
+				PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_500_DEG);
+				break;
+			case HWSPARKY_GYRORANGE_1000:
+				PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_1000_DEG);
+				break;
+			case HWSPARKY_GYRORANGE_2000:
+				PIOS_MPU6050_SetGyroRange(PIOS_MPU60X0_SCALE_2000_DEG);
+				break;
+		}
+
+		uint8_t hw_accel_range;
+		HwSparkyAccelRangeGet(&hw_accel_range);
+		switch(hw_accel_range) {
+			case HWSPARKY_ACCELRANGE_2G:
+				PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_2G);
+				break;
+			case HWSPARKY_ACCELRANGE_4G:
+				PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_4G);
+				break;
+			case HWSPARKY_ACCELRANGE_8G:
+				PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_8G);
+				break;
+			case HWSPARKY_ACCELRANGE_16G:
+				PIOS_MPU6050_SetAccelRange(PIOS_MPU60X0_ACCEL_16G);
+				break;
+		}
 	}
 
 #endif /* PIOS_INCLUDE_MPU6050 */
