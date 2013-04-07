8a1103c9f5897ade34d697e32f393cd7189ea908
Kenn Sebesta <kenn@eissq.com>
2013-04-07 17:26:20 +0200

    Removed gyroscope data from 6-point calibration. It was not used in the calculations.

--- ground/gcs/src/plugins/config/calibration.cpp
+++ ground/gcs/src/plugins/config/calibration.cpp
@@ -475,9 +475,6 @@ void Calibration::doStartSixPoint()
     accel_accum_x.clear();
     accel_accum_y.clear();
     accel_accum_z.clear();
-    gyro_accum_x.clear();
-    gyro_accum_y.clear();
-    gyro_accum_z.clear();
     mag_accum_x.clear();
     mag_accum_y.clear();
     mag_accum_z.clear();
@@ -485,8 +482,6 @@ void Calibration::doStartSixPoint()
     Thread::usleep(100000);
 
     connectSensor(ACCEL, true);
-    connectSensor(GYRO, true);
-
     if(calibrateMag) {
         connectSensor(MAG, true);
     }
@@ -508,7 +503,6 @@ void Calibration::doCancelSixPoint(){
     resetSensorCalibrationToOriginalValues();
 
     connectSensor(ACCEL, false);
-    connectSensor(GYRO, false);
     if(calibrateMag) {
         connectSensor(MAG, false);
     }
@@ -805,20 +799,10 @@ bool Calibration::storeSixPointMeasurement(UAVObject * obj, int position)
         mag_accum_z.append(magData.z);
     }
 
-    if (obj->getObjID() == Gyros::OBJID) {
-        Gyros *gyros = Gyros::GetInstance(getObjectManager());
-        Q_ASSERT(gyros);
-        Gyros::DataFields gyrosData = gyros->getData();
-        gyro_accum_x.append(gyrosData.x);
-        gyro_accum_y.append(gyrosData.y);
-        gyro_accum_z.append(gyrosData.z);
-    }
-
     emit sixPointProgressChanged((float) accel_accum_x.size() / NUM_SENSOR_UPDATES_SIX_POINT * 100);
 
     // If enough data is collected, average it for this position
     if(accel_accum_x.size() >= NUM_SENSOR_UPDATES_SIX_POINT &&
-            gyro_accum_x.size() >= NUM_SENSOR_UPDATES_SIX_POINT &&
             (!calibrateMag || mag_accum_x.size() >= NUM_SENSOR_UPDATES_SIX_POINT)) {
 
         // Store the average accel value in that position
@@ -828,14 +812,6 @@ bool Calibration::storeSixPointMeasurement(UAVObject * obj, int position)
         accel_accum_x.clear();
         accel_accum_y.clear();
         accel_accum_z.clear();
-
-        // Store the average gyro value in that position
-        gyro_data_x[position] = listMean(gyro_accum_x);
-        gyro_data_y[position] = listMean(gyro_accum_y);
-        gyro_data_z[position] = listMean(gyro_accum_z);
-        gyro_accum_x.clear();
-        gyro_accum_y.clear();
-        gyro_accum_z.clear();
 
         if (calibrateMag) {
             mag_data_x[position] = listMean(mag_accum_x);
@@ -1116,12 +1092,12 @@ int Calibration::computeScaleBias()
 
     bool good_calibration = true;
 
-    qDebug() << "Gyro measurements";
-    for(int i = 0; i < 6; i++)
-        qDebug() << gyro_data_x[i] << ", " << gyro_data_y[i] << ", " << gyro_data_z[i] << ";";
     qDebug() << "Accel measurements";
     for(int i = 0; i < 6; i++)
         qDebug() << accel_data_x[i] << ", " << accel_data_y[i] << ", " << accel_data_z[i] << ";";
+    qDebug() << "Mag measurements";
+    for(int i = 0; i < 6; i++)
+        qDebug() << mag_data_x[i] << ", " << mag_data_y[i] << ", " << mag_data_z[i] << ";";
 
     // Calibrate accelerometer
     double S[3], b[3];
