d0998179a8283ca151e0aa40b1f7836cdd07aa65
lilvinz <lilvinz@nscit.com>
2013-05-11 17:44:25 +0200

    f4 usb hook: pass on length of the transfer instead of max length

--- flight/PiOS/STM32F4xx/pios_usbhook.c
+++ flight/PiOS/STM32F4xx/pios_usbhook.c
@@ -417,7 +417,7 @@ static uint8_t PIOS_USBHOOK_CLASS_DataOut(void *pdev, uint8_t epnum)
 
 	if ((epnum_idx < NELEMENTS(usb_epout_table)) && usb_epout_table[epnum_idx].cb) {
 		struct usb_ep_entry *ep = &(usb_epout_table[epnum_idx]);
-		if (!ep->cb(ep->context, epnum_idx, ep->max_len)) {
+		if (!ep->cb(ep->context, epnum_idx, USBD_GetRxCount(pdev, epnum))) {
 			/* NOTE: use real endpoint number including direction bit */
 			DCD_SetEPStatus(pdev, epnum, USB_OTG_EP_RX_NAK);
 		}
