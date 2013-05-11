98e169ec2c8726feae5823c55c3a34ab4b8f1ef9
Stacey Sheldon <stac@solidgoldbomb.org>
2013-05-10 21:36:08 -0400

    usb pid: stop using Revolution's PID on ST eval boards
    FlyingF3, FlyingF4 and DiscoveryF4 are all now using a "spare"
    PID that is one of OP's allocation.  These should be migrated
    to a more appropriate PID as soon as one is available.
    
    Proper entries have been added to the Windows CDC driver .inf
    file for these boards as well.

--- flight/PiOS/inc/pios_usb_defs.h
+++ flight/PiOS/inc/pios_usb_defs.h
@@ -368,6 +368,8 @@ enum usb_product_ids {
 
 	/* ST Eval Boards */
 	USB_PRODUCT_ID_DISCOVERYF4    = USB_PRODUCT_ID_SPARE,
+	USB_PRODUCT_ID_FLYINGF3       = USB_PRODUCT_ID_SPARE,
+	USB_PRODUCT_ID_FLYINGF4       = USB_PRODUCT_ID_SPARE,
 } __attribute__((packed));
 
 enum usb_op_board_ids {
