8973438d94ee67621ca077aa383f5fcf0a949c97
guilhermito <guilhermito.santiago@gmail.com>
2013-05-22 21:51:34 +0100

    Flight ADC-created configuration for RCFlyers's discoveryF3 shield

--- flight/targets/board_hw_defs/flyingf3/board_hw_defs.c
+++ flight/targets/board_hw_defs/flyingf3/board_hw_defs.c
@@ -2174,7 +2174,30 @@ const struct pios_usb_cdc_cfg pios_usb_cdc_cfg = {
 #include "pios_adc_priv.h"
 #include "pios_dma.h"
 
-//TODO Write valid configuration for this target
-static const struct pios_internal_adc_cfg internal_adc_cfg = {};
-
+static const struct pios_internal_adc_cfg internal_adc_cfg_rcflyer_shield = {
+	.dma = {
+		.irq = {
+			.flags   = (DMA1_FLAG_TC1 | DMA1_FLAG_TE1 | DMA1_FLAG_HT1 | DMA1_FLAG_GL1),
+			.init    = {
+			.NVIC_IRQChannel                   = DMA1_Channel1_IRQn,
+			.NVIC_IRQChannelPreemptionPriority = PIOS_IRQ_PRIO_HIGH,
+			.NVIC_IRQChannelSubPriority        = 0,
+			.NVIC_IRQChannelCmd                = ENABLE,
+			},
+		},
+		.rx = {
+			.channel = DMA1_Channel1,
+			.init    = {
+			.DMA_Priority           = DMA_Priority_High,
+			},
+		}
+	},
+	.half_flag = DMA1_IT_HT1,
+	.full_flag = DMA1_IT_TC1,
+	.oversampling = 32,
+	.adc_dev_master = ADC1,
+	.adc_dev_slave = ADC2,
+	.number_of_used_pins = 2,
+	.adc_pins = (struct adc_pin[]){{GPIOC,GPIO_Pin_3,ADC_Channel_9,true},{GPIOC,GPIO_Pin_4,ADC_Channel_5,false},},
+};
 #endif //PIOS_INCLUDE_ADC
