19bb6fc391e920d0157b31c7384c24f2b1dc91dd
lilvinz <lilvinz@nscit.com>
2013-05-23 20:12:44 +0200

    license header update to http://taulabs.org: fifth pass git ls-files -z | xargs -0 sed -i -e 's/Tau Labs, http:\/\/www.taulabs.org, Copyright/Tau Labs, http:\/\/taulabs.org, Copyright/g'

--- flight/targets/Quanton/System/inc/pios_config.h
+++ flight/targets/Quanton/System/inc/pios_config.h
@@ -2,7 +2,7 @@
  ******************************************************************************
  * @file       pios_config.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- * @author     Tau Labs, http://www.taulabs.org, Copyright (C) 2012-2013
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2013
  * @addtogroup OpenPilotSystem OpenPilot System
  * @{
  * @addtogroup OpenPilotCore OpenPilot Core
