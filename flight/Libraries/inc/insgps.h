d0b1ee362ec8144e0764bf77955a91ade5ad1573
lilvinz <lilvinz@nscit.com>
2013-05-23 19:52:51 +0200

    license header update to http://taulabs.org: second pass git ls-files -z | xargs -0 sed -i -e 's/Tau Labs, http:\/\/github.com\/TauLabs Copyright/Tau Labs, http:\/\/taulabs.org, Copyright/g'

--- flight/Libraries/inc/insgps.h
+++ flight/Libraries/inc/insgps.h
@@ -8,7 +8,7 @@
  *
  * @file       insgps.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- * @author     Tau Labs, http://github.com/TauLabs Copyright (C) 2012-2013.
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2013.
  * @brief      Include file of the INSGPS exposed functionality.
  *
  * @see        The GNU Public License (GPL) Version 3
