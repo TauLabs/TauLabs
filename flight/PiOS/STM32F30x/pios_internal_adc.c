c19a518e47381871c4c533df424e0cb35fc6bae4
guilhermito <guilhermito.santiago@gmail.com>
2013-05-25 01:56:47 +0100

    Flight F3 ADC-Changed the ADC clock to RCC_ADC12PLLCLK_Div16
    This gives all greens on system health gadget.
    Properly handle ADC3 and 4 clock config.

--- flight/PiOS/STM32F30x/pios_internal_adc.c
+++ flight/PiOS/STM32F30x/pios_internal_adc.c
@@ -231,7 +231,11 @@ static void PIOS_INTERNAL_ADC_Converter_Config(uint32_t internal_adc_id)
         if (adc_dev->cfg->adc_dev_slave)
                 ADC_DeInit(adc_dev->cfg->adc_dev_slave);
 
-        RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div8);//TODO
+        if(adc_dev->cfg->adc_dev_master == ADC1 || adc_dev->cfg->adc_dev_master == ADC2)
+        	RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div16);
+        else
+        	RCC_ADCCLKConfig(RCC_ADC34PLLCLK_Div16);
+
         ADC_VoltageRegulatorCmd(adc_dev->cfg->adc_dev_master, ENABLE);
         PIOS_DELAY_WaituS(10);
         ADC_SelectCalibrationMode(adc_dev->cfg->adc_dev_master, ADC_CalibrationMode_Single);
