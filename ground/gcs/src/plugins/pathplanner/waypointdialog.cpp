67664ab9fdefe38d593e35aba04c4a4b71883f19
James Cotton <peabody124@gmail.com>
2013-05-26 17:43:05 -0500

    WaypointDialog: documentation update

--- ground/gcs/src/plugins/pathplanner/waypointdialog.cpp
+++ ground/gcs/src/plugins/pathplanner/waypointdialog.cpp
@@ -3,7 +3,7 @@
  * @file       waypointdialog.cpp
  * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2013
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2012.
- * @addtogroup TauLabs GCS Plugins
+ * @addtogroup GCSPlugins GCS Plugins
  * @{
  * @addtogroup PathPlanner Map Plugin
  * @{
