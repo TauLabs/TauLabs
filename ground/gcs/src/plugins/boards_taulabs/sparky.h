9884f40ec315a752598795f1f84bd76d7a6c097a
James Cotton <peabody124@gmail.com>
2013-06-02 07:23:42 -0500

    Sparky: add connection diagram

--- ground/gcs/src/plugins/boards_taulabs/sparky.h
+++ ground/gcs/src/plugins/boards_taulabs/sparky.h
@@ -61,6 +61,11 @@ public:
      */
     virtual enum InputType getInputOnPort(int port_num = 0);
 
+    /**
+     * @brief getConnectionDiagram get the connection diagram for this board
+     * @return a string with the name of the resource for this board diagram
+     */
+    virtual QString getConnectionDiagram() { return ":/taulabs/images/sparky-connection-diagram.svg"; }
 };
 
 
