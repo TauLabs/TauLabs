11d060967f351848b53683f3c9aeb020fe1b66a1
Stacey Sheldon <stac@solidgoldbomb.org>
2013-06-22 17:25:07 -0400

    flash partitions: fixup missing copyright header

--- flight/PiOS/Common/pios_flash.c
+++ flight/PiOS/Common/pios_flash.c
@@ -1,3 +1,30 @@
+/**
+ ******************************************************************************
+ * @file       pios_flash.c
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2013
+ * @addtogroup PIOS PIOS Core hardware abstraction layer
+ * @{
+ * @addtogroup PIOS_FLASH Flash Partition Abstraction
+ * @{
+ * @brief Flash Partition Abstraction to hide details of underlying flash device details
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* Project Includes */
 #include "pios_config.h"
 
 #if defined(PIOS_INCLUDE_FLASH)
@@ -374,3 +401,8 @@ int32_t PIOS_FLASH_read_data(uintptr_t partition_id, uint32_t partition_offset, 
 }
 
 #endif	/* PIOS_INCLUDE_FLASH */
+
+/**
+ * @}
+ * @}
+ */
