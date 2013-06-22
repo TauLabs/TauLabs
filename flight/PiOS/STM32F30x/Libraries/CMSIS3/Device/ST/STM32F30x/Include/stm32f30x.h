695b838e962bce7a84085fc43aa79c9f7e9f7fc4
lilvinz <lilvinz@nscit.com>
2013-06-22 17:12:27 +0200

    stm32f30x.h: fixed a typo introducted by st library update

--- flight/PiOS/STM32F30x/Libraries/CMSIS3/Device/ST/STM32F30x/Include/stm32f30x.h
+++ flight/PiOS/STM32F30x/Libraries/CMSIS3/Device/ST/STM32F30x/Include/stm32f30x.h
@@ -94,7 +94,7 @@
         can define the HSE value in your toolchain compiler preprocessor.
   */           
 #if !defined  (HSE_VALUE) 
- #define HSE_VALUE            ((uint32_t)8000000) /*!< Value of the External oscillator in Hz *//RESET
+ #define HSE_VALUE            ((uint32_t)8000000) /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 /**
