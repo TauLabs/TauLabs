9460edbd801f18aa194709d9d28676a9d0b9a22d
James Cotton <peabody124@gmail.com>
2013-06-26 21:04:30 -0500

    PathPlanner: fix double promotion

--- flight/Modules/PathPlanner/pathplanner.c
+++ flight/Modules/PathPlanner/pathplanner.c
@@ -488,8 +488,8 @@ static void createPathLogo()
 	WaypointData waypoint;
 	waypoint.Velocity = 5; // Since for now this isn't directional just set a mag
 	for(uint32_t i = 0; i < 20; i++) {
-		waypoint.Position[1] = scale * 30 * cosf(i / 19.0 * 2 * PI);
-		waypoint.Position[0] = scale * 50 * sinf(i / 19.0 * 2 * PI);
+		waypoint.Position[1] = scale * 30 * cosf(i / 19.0f * 2 * PI);
+		waypoint.Position[0] = scale * 50 * sinf(i / 19.0f * 2 * PI);
 		waypoint.Position[2] = -50;
 		waypoint.Mode = WAYPOINT_MODE_FLYVECTOR;
 		WaypointCreateInstance();
@@ -497,8 +497,8 @@ static void createPathLogo()
 
 	// Draw P
 	for(uint32_t i = 20; i < 35; i++) {
-		waypoint.Position[1] = scale * (55 + 20 * cosf(i / 10.0 * PI - PI / 2));
-		waypoint.Position[0] = scale * (25 + 25 * sinf(i / 10.0 * PI - PI / 2));
+		waypoint.Position[1] = scale * (55 + 20 * cosf(i / 10.0f * PI - PI / 2));
+		waypoint.Position[0] = scale * (25 + 25 * sinf(i / 10.0f * PI - PI / 2));
 		waypoint.Position[2] = -50;
 		waypoint.Mode = WAYPOINT_MODE_FLYVECTOR;
 		WaypointCreateInstance();
