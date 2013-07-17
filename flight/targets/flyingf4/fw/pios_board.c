cf1b3ecd8b4df084c9ebaf972331c4307c72929b
lilvinz <lilvinz@nscit.com>
2013-07-18 00:19:10 +0200

    flyingf4: handle returnvalue from PIOS_HMC5883_Init

--- flight/targets/flyingf4/fw/pios_board.c
+++ flight/targets/flyingf4/fw/pios_board.c
@@ -855,7 +855,8 @@ void PIOS_Board_Init(void) {
 		panic(6);
 
 #if defined(PIOS_INCLUDE_HMC5883)
-	PIOS_HMC5883_Init(PIOS_I2C_MAIN_ADAPTER, &pios_hmc5883_cfg);
+	if (PIOS_HMC5883_Init(PIOS_I2C_MAIN_ADAPTER, &pios_hmc5883_cfg) != 0)
+		panic(3);
 	if (PIOS_HMC5883_Test() != 0)
 		panic(3);
 #endif
