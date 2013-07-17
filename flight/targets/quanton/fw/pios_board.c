8d2d275dabacb5a408b11ae2320321a7028403fb
lilvinz <lilvinz@nscit.com>
2013-07-18 00:20:34 +0200

    quanton: handle returnvalue from PIOS_MS5611_Init

--- flight/targets/quanton/fw/pios_board.c
+++ flight/targets/quanton/fw/pios_board.c
@@ -1226,7 +1226,8 @@ void PIOS_Board_Init(void) {
 	PIOS_WDG_Clear();
 
 #if defined(PIOS_INCLUDE_MS5611)
-	PIOS_MS5611_Init(&pios_ms5611_cfg, pios_i2c_internal_adapter_id);
+	if (PIOS_MS5611_Init(&pios_ms5611_cfg, pios_i2c_internal_adapter_id) != 0)
+		panic(4);
 	if (PIOS_MS5611_Test() != 0)
 		panic(4);
 #endif
