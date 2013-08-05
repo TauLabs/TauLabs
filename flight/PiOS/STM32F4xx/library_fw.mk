876e1aa0c0f663e331a75b258b513357a7988b69
James Cotton <peabody124@gmail.com>
2013-08-05 16:26:42 -0500

    F4 Firmware: use library_fw.mk
    This moves the common definitions into library.mk and the fw
    specific source files to a library_fw.mk

---
+++ flight/PiOS/STM32F4xx/library_fw.mk
@@ -0,0 +1,43 @@
+#
+# Rules to (help) build the F4xx device support.
+#
+
+#
+# Directory containing this makefile
+#
+PIOS_DEVLIB			:=	$(dir $(lastword $(MAKEFILE_LIST)))
+include $(PIOS_DEVLIB)/library.mk
+
+#
+# Hardcoded linker script names for now
+#
+LINKER_SCRIPTS_APP	 =	$(PIOS_DEVLIB)/link_STM32F4xx_OP_memory.ld \
+						$(PIOS_DEVLIB)/link_STM32F4xx_sections.ld
+
+#
+# PIOS device library source and includes
+#
+SRC					+=	$(wildcard $(PIOS_DEVLIB)*.c)
+
+#
+# ST Peripheral library
+#
+SRC					+=	$(wildcard $(PERIPHLIB)/src/*.c)
+
+#
+# ST USB OTG library
+#
+SRC				+=	$(addprefix $(USBOTGLIB)/src/,$(USBOTGLIB_SRC))
+
+#
+# ST USB Device library
+#
+SRC				+=	$(wildcard $(USBDEVLIB)/Core/src/*.c)
+
+#
+# FreeRTOS
+#
+# If the application has included the generic FreeRTOS support, then add in
+# the device-specific pieces of the code.
+#
+SRC					+=	$(wildcard $(FREERTOS_PORTDIR)/portable/GCC/ARM_CM4F/*.c)
