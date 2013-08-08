724dc024e4924e6a27a57d4a83a7ebf8a12b978c
James Cotton <peabody124@gmail.com>
2013-08-07 21:19:34 -0500

    F4 BL: add required files to library.mk

--- flight/PiOS/STM32F4xx/library_bl.mk
+++ flight/PiOS/STM32F4xx/library_bl.mk
@@ -15,6 +15,28 @@ LINKER_SCRIPTS_BL	 =	$(PIOS_DEVLIB)/link_STM32F4xx_BL_memory.ld \
 						$(PIOS_DEVLIB)/link_STM32F4xx_sections.ld
 
 #
+# PIOS device library source and includes
+#
+SRC					+=	$(PIOS_DEVLIB)/pios_bl_helper.c
+SRC					+=	$(PIOS_DEVLIB)/pios_delay.c
+SRC					+=	$(PIOS_DEVLIB)/pios_exti.c
+SRC					+=	$(PIOS_DEVLIB)/pios_flash_internal.c
+SRC					+=	$(PIOS_DEVLIB)/pios_gpio.c
+SRC					+=	$(PIOS_DEVLIB)/pios_iap.c
+SRC					+=	$(PIOS_DEVLIB)/pios_irq.c
+SRC					+=	$(PIOS_DEVLIB)/pios_led.c
+SRC					+=	$(PIOS_DEVLIB)/pios_rtc.c
+SRC					+=	$(PIOS_DEVLIB)/pios_spi.c
+SRC					+=	$(PIOS_DEVLIB)/pios_sys.c
+SRC					+=	$(PIOS_DEVLIB)/pios_tim.c
+SRC					+=	$(PIOS_DEVLIB)/pios_usb.c
+SRC					+=	$(PIOS_DEVLIB)/pios_usb_cdc.c
+SRC					+=	$(PIOS_DEVLIB)/pios_usb_hid.c
+SRC					+=	$(PIOS_DEVLIB)/pios_usbhook.c
+SRC					+=	$(PIOS_DEVLIB)/pios_wdg.c
+SRC					+=	$(PIOS_DEVLIB)/startup.c
+SRC					+=	$(PIOS_DEVLIB)/vectors_stm32f4xx.c
+#
 # ST Peripheral library
 #
 SRC					+=	$(wildcard $(PERIPHLIB)/src/*.c)
