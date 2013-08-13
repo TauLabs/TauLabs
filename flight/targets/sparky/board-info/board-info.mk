b84696b632438ec792d2ab59033597d819bb0018
Stacey Sheldon <stac@solidgoldbomb.org>
2013-08-13 01:54:12 -0400

    bl unified: use cable connected instead of check available
    PIOS_USB_CheckAvailable() checks for both cable connected
    as well as whether the device is fully enumerated.  This
    more stringent check takes a more variable amount of time
    on different computers.
    
    It is much more appropriate to simply check whether the
    USB cable is connected to determine whether we should wait
    further in the bootloader.

--- flight/targets/sparky/board-info/board-info.mk
+++ flight/targets/sparky/board-info/board-info.mk
@@ -1,6 +1,6 @@
 BOARD_TYPE          := 0x88
 BOARD_REVISION      := 0x02
-BOOTLOADER_VERSION  := 0x80
+BOOTLOADER_VERSION  := 0x81
 HW_TYPE             := 0x00		# seems to be unused
 
 MCU                 := cortex-m4
