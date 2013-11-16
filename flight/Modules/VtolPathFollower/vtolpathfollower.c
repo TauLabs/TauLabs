c78086809ed14b67d784efa1a288c08e3575ecf5
James Cotton <peabody124@gmail.com>
2013-11-15 23:47:08 -0600

    NEDAccel: move from guidance to state estimation
    Preivously this was calculated in the guidance loop instead
    of for every sensor update. This isn't optimal and now is
    run at the full sensor rate.

--- flight/Modules/VtolPathFollower/vtolpathfollower.c
+++ flight/Modules/VtolPathFollower/vtolpathfollower.c
@@ -45,7 +45,6 @@
 #include "flightstatus.h"
 #include "gpsvelocity.h"
 #include "gpsposition.h"
-#include "nedaccel.h"
 #include "nedposition.h"
 #include "pathstatus.h"
 #include "stabilizationdesired.h"
@@ -71,7 +70,6 @@ static VtolPathFollowerSettingsData guidanceSettings;
 
 // Private functions
 static void vtolPathFollowerTask(void *parameters);
-static void updateNedAccel();
 static bool module_enabled = false;
 
 /**
@@ -111,7 +109,6 @@ int32_t VtolPathFollowerInitialize()
 		return -1;
 	}
 
-	NedAccelInitialize();
 	PathDesiredInitialize();
 	PathStatusInitialize();
 	VelocityDesiredInitialize();
@@ -171,10 +168,6 @@ static void vtolPathFollowerTask(void *parameters)
 
 		// Continue collecting data if not enough time
 		vTaskDelayUntil(&lastUpdateTime, MS2TICKS(guidanceSettings.UpdatePeriod));
-
-		// Convert the accels into the NED frame. 
-		// TODO: This should be moved somewhere else and run at the sensor rate.
-		updateNedAccel();
 		
 		static uint8_t last_flight_mode;
 		FlightStatusGet(&flightStatus);
@@ -219,46 +212,6 @@ static void vtolPathFollowerTask(void *parameters)
 		AlarmsClear(SYSTEMALARMS_ALARM_PATHFOLLOWER);
 
 	}
-}
-
-/**
- * Keep a running filtered version of the acceleration in the NED frame
- */
-static void updateNedAccel()
-{
-	float accel[3];
-	float q[4];
-	float Rbe[3][3];
-	float accel_ned[3];
-
-	// Collect downsampled attitude data
-	AccelsData accels;
-	AccelsGet(&accels);		
-	accel[0] = accels.x;
-	accel[1] = accels.y;
-	accel[2] = accels.z;
-	
-	//rotate avg accels into earth frame and store it
-	AttitudeActualData attitudeActual;
-	AttitudeActualGet(&attitudeActual);
-	q[0]=attitudeActual.q1;
-	q[1]=attitudeActual.q2;
-	q[2]=attitudeActual.q3;
-	q[3]=attitudeActual.q4;
-	Quaternion2R(q, Rbe);
-	for (uint8_t i=0; i<3; i++){
-		accel_ned[i]=0;
-		for (uint8_t j=0; j<3; j++)
-			accel_ned[i] += Rbe[j][i]*accel[j];
-	}
-	accel_ned[2] += GRAVITY;
-	
-	NedAccelData accelData;
-	NedAccelGet(&accelData);
-	accelData.North = accel_ned[0];
-	accelData.East = accel_ned[1];
-	accelData.Down = accel_ned[2];
-	NedAccelSet(&accelData);
 }
 
 /**
