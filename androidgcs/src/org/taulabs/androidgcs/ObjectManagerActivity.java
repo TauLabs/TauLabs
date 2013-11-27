1fb13c48b4d84487ae0f5cbd4f81a22412465748
James Cotton <peabody124@gmail.com>
2013-11-26 21:26:08 -0600

    Android: start implementing navigation bar

--- androidgcs/src/org/taulabs/androidgcs/ObjectManagerActivity.java
+++ androidgcs/src/org/taulabs/androidgcs/ObjectManagerActivity.java
@@ -58,9 +58,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public abstract class ObjectManagerActivity extends Activity {
 
@@ -82,11 +87,28 @@ public abstract class ObjectManagerActivity extends Activity {
 	//! Maintain a list of all the UAVObject listeners for this activity
 	private HashMap<Observer, UAVObject> listeners;
 
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private CharSequence mTitle;
 
 	/** Called when the activity is first created. */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.drawer);
+
+		mPlanetTitles = getResources().getStringArray(R.array.activities_array);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+		         R.layout.drawer_list_item, mPlanetTitles));
+
+		// Set the list's click listener
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 	}
 
 	/**
@@ -652,5 +674,24 @@ public abstract class ObjectManagerActivity extends Activity {
 		updateTelemetryStats();
 		return true;
 	}
+	
+	/************ Deals with drawer navigation ************/
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
 
 }
