65c0c23fd14b155ba6c7f72f2ca28a87adce93a2
Stacey Sheldon <stac@solidgoldbomb.org>
2014-01-12 22:01:10 -0500

    python: add python library for parsing/representing uavo xml files

---
+++ python/taulabs/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import # Use 3.x import model.
+
+#-------------------------------------------------------------------------------
+__all__ = ()
+
+from . import uavo
+from . import uavo_collection
+from . import uavo_list
