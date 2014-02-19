364284d5f0837abee5cdef40471204a247ae922b
lilvinz <lilvinz@nscit.com>
2014-02-19 22:09:47 +0100

    pios/stm32f4xx: add support for ChibiOS

--- flight/PiOS/STM32F4xx/pios_usb_hid.c
+++ flight/PiOS/STM32F4xx/pios_usb_hid.c
@@ -9,7 +9,7 @@
  *
  * @file       pios_usb_hid.c
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2013
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2014
  * @brief      USB COM functions (STM32 dependent code)
  * @see        The GNU Public License (GPL) Version 3
  *
@@ -214,7 +214,7 @@ static bool PIOS_USB_HID_SendReport(struct pios_usb_hid_dev * usb_hid_dev)
 
 #if defined(PIOS_INCLUDE_FREERTOS)
 	portEND_SWITCHING_ISR(need_yield ? pdTRUE : pdFALSE);
-#endif	/* PIOS_INCLUDE_FREERTOS */
+#endif	/* defined(PIOS_INCLUDE_FREERTOS) */
 
 	return true;
 }
@@ -519,7 +519,7 @@ static bool PIOS_USB_HID_EP_OUT_Callback(uintptr_t usb_hid_id, uint8_t epnum, ui
 
 #if defined(PIOS_INCLUDE_FREERTOS)
 	portEND_SWITCHING_ISR(need_yield ? pdTRUE : pdFALSE);
-#endif	/* PIOS_INCLUDE_FREERTOS */
+#endif	/* defined(PIOS_INCLUDE_FREERTOS) */
 
 	return rc;
 }
