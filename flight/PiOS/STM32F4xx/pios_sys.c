7216dd1b4ac93c3cc288e857656840898701d87c
lilvinz <lilvinz@nscit.com>
2014-02-19 22:09:47 +0100

    pios/stm32f4xx: add support for ChibiOS

--- flight/PiOS/STM32F4xx/pios_sys.c
+++ flight/PiOS/STM32F4xx/pios_sys.c
@@ -6,26 +6,27 @@
  * @brief PIOS System Initialization code
  * @{
  *
- * @file       pios_sys.c  
+ * @file       pios_sys.c
  * @author     Michael Smith Copyright (C) 2011
  * 			The OpenPilot Team, http://www.openpilot.org Copyright (C) 2012.
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2014
  * @brief      Sets up basic STM32 system hardware, functions are called from Main.
  * @see        The GNU Public License (GPL) Version 3
  *
  *****************************************************************************/
-/* 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 3 of the License, or 
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
@@ -48,9 +49,11 @@ void SysTick_Handler(void);
 */
 void PIOS_SYS_Init(void)
 {
+#if !defined(PIOS_INCLUDE_CHIBIOS)
 	/* Setup STM32 system (RCC, clock, PLL and Flash configuration) - CMSIS Function */
 	SystemInit();
 	SystemCoreClockUpdate();	/* update SystemCoreClock for use elsewhere */
+#endif /* !defined(PIOS_INCLUDE_CHIBIOS) */
 
 	/*
 	 * @todo might make sense to fetch the bus clocks and save them somewhere to avoid
