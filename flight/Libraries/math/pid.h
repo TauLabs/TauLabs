af618f88cf36587e33264ed2b228fb58c64a4bba
James Cotton <peabody124@gmail.com>
2014-03-23 09:22:52 -0500

    pid: update copyright header

--- flight/Libraries/math/pid.h
+++ flight/Libraries/math/pid.h
@@ -7,7 +7,7 @@
  *
  * @file       pid.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2013
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2014
  * @brief      PID Control algorithms
  *
  * @see        The GNU Public License (GPL) Version 3
