ffe6b9bca3ae54982ef4e355033ae60026f48c4e
James Cotton <peabody124@gmail.com>
2014-04-22 16:35:10 -0500

    matlab: system ident only use non-zero covariance terms
    Store the covariance matrix in a linear array and generate
    C code that has the indexes into the full array replaced with
    those mapping into the spare array of non-zero elements. This
    also allows only working with the elements in the upper
    triangular component.
    
    This reduces the covariance storage from 169 floats to 43, which
    is twice those savings since the array must be duplicated.

--- matlab/system_ident/generate_model.m
+++ matlab/system_ident/generate_model.m
@@ -149,147 +149,180 @@ P3 = (I - K*H)*P;  % Output state covariance
 
 N = 13;
 
-Pnew = simplify(P3);
-
-Pstrings=cell(N,N);
-for i=1:N
-    for j=i:N
-        if P(i,j) == 0
-            Pstrings{i,j} = num2str(0);
-        else
-            Pstrings{i,j} = char(Pnew(i,j));
-            Pstrings{i,j} = strrep(Pstrings{i,j},'Ts^2','Tsq');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'Ts^3','Tsq3');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'Ts^4','Tsq4');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'P','D');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b1)','e_b1');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b2)','e_b2');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b3)','e_b3');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*b1)','(e_b1*e_b1)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*b2)','(e_b2*e_b2)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*b3)','(e_b3*e_b3)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*tau)','e_tau2');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(3*tau)','e_tau3');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(4*tau)','e_tau4');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(tau)','e_tau');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'s_a^2','s_a2');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'s_a^3','s_a3');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'u1^2','(u1*u1)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'u2^2','(u2*u2)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'u3^2','(u3*u3)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'bias1^2','(bias1*bias1)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'bias2^2','(bias2*bias2)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'bias3^2','(bias3*bias3)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'u1_in^2','(u1_in*u1_in)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'u2_in^2','(u2_in*u2_in)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'u3_in^2','(u3_in*u3_in)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'(Ts + e_tau)^2','Ts_e_tau2');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'(Ts + e_tau)^3','Ts_e_tau2');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'(Ts + e_tau)^4','Ts_e_tau4');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b1 + tau)','(e_b1*e_tau)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b2 + tau)','(e_b2*e_tau)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b3 + tau)','(e_b3*e_tau)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b1 + 2*tau)','(e_b1*e_tau2)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b2 + 2*tau)','(e_b2*e_tau2)');
-            Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b3 + 2*tau)','(e_b3*e_tau2)');
-            
-            for n1 = 1:N
-                for n2 = 1:N
-                    Pstrings{i,j} = strrep(Pstrings{i,j},sprintf('D_%d_%d^2',n1,n2),sprintf('D_%d_%d*D_%d_%d',n1,n2,n1,n2));
-                end
-            end
-        end
-        s1 = sprintf('P[%d][%d] = ',i-1,j-1);
-        s2 = sprintf('P[%d][%d] = ',j-1,i-1);
-        if (i==j)
-            Pstrings{i,j} = [s1, Pstrings{i,j}, ';'];
-        else
-            Pstrings{i,j} = [s1, s2, Pstrings{i,j}, ';'];
-        end
-    end
-end
-
-for k = N:-1:1
-    s1=sprintf('_%d',k);
-    s2=sprintf('[%d]',k-1);
-    for i=1:N
-        for j=i:N
-            Pstrings{i,j} = strrep(Pstrings{i,j},s1,s2);
-        end
-    end
-end
 
 fid = fopen('autotune_filter.c','w');
 
 
-for i=1:N;
-    for(j=i:N);
-        fprintf(fid,'\t%s\n',Pstrings{i,j});
+for Pnew = {P2, P3}
+    nummults=0;
+    numadds =0;
+
+    fprintf(fid, '\n\n\n\n\t// Covariance calculation\n');
+    Pnew = simplify(Pnew{1});
+
+
+    Pstrings=cell(N,N);
+    for i=1:N
+        for j=i:N
+            if P(i,j) == 0
+                Pstrings{i,j} = num2str(0);
+            else           
+                % replace with lots of precalculated constants or invalid C
+                Pstrings{i,j} = char(Pnew(i,j));
+                Pstrings{i,j} = strrep(Pstrings{i,j},'Ts^2','Tsq');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'Ts^3','Tsq3');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'Ts^4','Tsq4');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'P','D');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b1)','e_b1');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b2)','e_b2');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b3)','e_b3');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*b1)','(e_b1*e_b1)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*b2)','(e_b2*e_b2)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*b3)','(e_b3*e_b3)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(2*tau)','e_tau2');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(3*tau)','e_tau3');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(4*tau)','e_tau4');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(tau)','e_tau');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'s_a^2','s_a2');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'s_a^3','s_a3');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'u1^2','(u1*u1)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'u2^2','(u2*u2)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'u3^2','(u3*u3)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'bias1^2','(bias1*bias1)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'bias2^2','(bias2*bias2)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'bias3^2','(bias3*bias3)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'u1_in^2','(u1_in*u1_in)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'u2_in^2','(u2_in*u2_in)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'u3_in^2','(u3_in*u3_in)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'(Ts + e_tau)^2','Ts_e_tau2');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'(Ts + e_tau)^3','Ts_e_tau2');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'(Ts + e_tau)^4','Ts_e_tau4');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b1 + tau)','(e_b1*e_tau)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b2 + tau)','(e_b2*e_tau)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b3 + tau)','(e_b3*e_tau)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b1 + 2*tau)','(e_b1*e_tau2)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b2 + 2*tau)','(e_b2*e_tau2)');
+                Pstrings{i,j} = strrep(Pstrings{i,j},'exp(b3 + 2*tau)','(e_b3*e_tau2)');
+                
+                for n1 = 13:-1:1
+                    Pstrings{i,j} = strrep(Pstrings{i,j},sprintf('Q_%d',n1),sprintf('Q[%d]', n1-1));
+                    Pstrings{i,j} = strrep(Pstrings{i,j},sprintf('S_%d',n1),sprintf('S[%d]', n1-1));
+                end
+
+                for n1 = 1:N
+                    for n2 = 1:N
+                        Pstrings{i,j} = strrep(Pstrings{i,j},sprintf('D_%d_%d^2',n1,n2),sprintf('D_%d_%d*D_%d_%d',n1,n2,n1,n2));
+                    end
+                end
+            end
+            s1 = sprintf('P_%d_%d = ',i,j);
+            Pstrings{i,j} = [s1, Pstrings{i,j}, ';'];
+        end
     end
+
+    Pstrings = Pstrings(P_idx);
+    
+    for i = 1:length(P_idx)
+        s_out = Pstrings{i};
+        
+        % replace indexes into array with indexes into spare linear
+        % array of non-zero elements
+        for j = length(P_idx):-1:1
+            % index backwards to make sure the big numbers get replaced
+            % first
+            [k, l] = ind2sub([N N], P_idx(j));
+            s1 = sprintf('_%d_%d', k, l);
+            s2 = sprintf('[%d]',j-1);
+            s_out = strrep(s_out, s1, s2);
+        end
+        
+        nummults=nummults + length(strfind(s_out,'*'));
+        numadds=numadds + length(strfind(s_out,'+'));
+        numadds=numadds + length(strfind(s_out,'-'));
+        
+        strings_out{i} = s_out; % for debugging
+        fprintf(fid,'\t%s\n',s_out);
+    end
+    
+    fprintf('Multplies: %d Adds: %d\n', nummults, numadds);
+    
 end
 
 fclose(fid);
 
-nummults=0;
-numadds =0;
-for i=1:N;
-    for(j=i:N);
-        nummults=nummults + length(strfind(Pstrings{i,j},'*'));
-        numadds=numadds + length(strfind(Pstrings{i,j},'+'));
-        numadds=numadds + length(strfind(Pstrings{i,j},'-'));
-    end
-end
-nummults
-numadds
 %% Create strings for update of x
-
 
 N = 13;
 
-Pstrings2=cell(N,1);
-for i=1:N
-    Pstrings2{i} = char(x_new(i));
-    Pstrings2{i} = strrep(Pstrings2{i},'Ts^2','Tsq');
-    Pstrings2{i} = strrep(Pstrings2{i},'exp(b1)','e_b1');
-    Pstrings2{i} = strrep(Pstrings2{i},'exp(b2)','e_b2');
-    Pstrings2{i} = strrep(Pstrings2{i},'exp(b3)','e_b3');
-    Pstrings2{i} = strrep(Pstrings2{i},'exp(2*tau)','e_tau*e_tau');
-    Pstrings2{i} = strrep(Pstrings2{i},'exp(tau)','e_tau');
-    Pstrings2{i} = strrep(Pstrings2{i},'s_a^2','s_a2');
-    Pstrings2{i} = strrep(Pstrings2{i},'s_a^3','s_a3');
-    Pstrings2{i} = strrep(Pstrings2{i},'(Ts + e_tau)^2','Ts_e_tau2');
-        for n1 = 1:N
-            for n2 = 1:N
-                Pstrings2{i} = strrep(Pstrings2{i},sprintf('P_%d_%d^2',n1,n2),sprintf('P_%d_%d*P_%d_%d',n1,n2,n1,n2));
-            end
-        end
-    s1 = sprintf('X[%d] = ',i-1);
-    Pstrings2{i} = [s1, Pstrings2{i}, ';'];
-end
+fid = fopen('autotune_filter.c','a');
 
-for k = N:-1:1
-    s1=sprintf('_%d',k);
-    s2=sprintf('[%d]',k-1);
+for x = {f x_new}
+    
+    fprintf(fid, '\n\n\n\n\t// X update\n');
+    
+    Pstrings2=cell(N,1);
     for i=1:N
-        Pstrings2{i} = strrep(Pstrings2{i},s1,s2);
+        Pstrings2{i} = char(x{1}(i));
+        Pstrings2{i} = strrep(Pstrings2{i},'Ts^2','Tsq');
+        Pstrings2{i} = strrep(Pstrings2{i},'exp(b1)','e_b1');
+        Pstrings2{i} = strrep(Pstrings2{i},'exp(b2)','e_b2');
+        Pstrings2{i} = strrep(Pstrings2{i},'exp(b3)','e_b3');
+        Pstrings2{i} = strrep(Pstrings2{i},'exp(2*tau)','e_tau*e_tau');
+        Pstrings2{i} = strrep(Pstrings2{i},'exp(tau)','e_tau');
+        Pstrings2{i} = strrep(Pstrings2{i},'s_a^2','s_a2');
+        Pstrings2{i} = strrep(Pstrings2{i},'s_a^3','s_a3');
+        Pstrings2{i} = strrep(Pstrings2{i},'(Ts + e_tau)^2','Ts_e_tau2');
+        Pstrings2{i} = strrep(Pstrings2{i},'S_1','S[0]');
+        Pstrings2{i} = strrep(Pstrings2{i},'S_2','S[1]');
+        Pstrings2{i} = strrep(Pstrings2{i},'S_3','S[2]');
+        
+        % replace indexes into array with indexes into spare linear
+        % array of non-zero elements
+        for j = length(P_idx):-1:1
+            % index backwards to make sure the big numbers get replaced
+            % first
+            [k, l] = ind2sub([N N], P_idx(j));
+            s1 = sprintf('_%d_%d', k, l);
+            s2 = sprintf('[%d]',j-1);
+            Pstrings2{i} = strrep(Pstrings2{i}, s1, s2);
+        end
+        
+        s1 = sprintf('X[%d] = ',i-1);
+        Pstrings2{i} = [s1, Pstrings2{i}, ';'];
     end
-end
-
-%% 
-fid = fopen('autotune_filter.c','w');
-
-for i=1:N;
-    fprintf(fid,'\t%s\n',Pstrings2{i});
+    
+    
+    for i=1:N;
+        fprintf(fid,'\t%s\n',Pstrings2{i});
+    end
+        
+    nummults=0;
+    numadds =0;
+    for i=1:N;
+        nummults=nummults + length(strfind(Pstrings2{i},'*'));
+        numadds=numadds + length(strfind(Pstrings2{i},'+'));
+        numadds=numadds + length(strfind(Pstrings2{i},'-'));
+    end
+    fprintf('Multplies: %d Adds: %d\n', nummults, numadds);
 end
 
 fclose(fid);
 
-nummults=0;
-numadds =0;
-for i=1:N;
-    nummults=nummults + length(strfind(Pstrings2{i},'*'));
-    numadds=numadds + length(strfind(Pstrings2{i},'+'));
-    numadds=numadds + length(strfind(Pstrings2{i},'-'));
+
+%%
+
+fid = fopen('autotune_filter.c','a');
+fprintf(fid, '\n\n\n\n\t// P initialization\n');
+for j = 1:length(P_idx)
+    % index backwards to make sure the big numbers get replaced
+    % first
+    [k, l] = ind2sub([N N], P_idx(j));
+    
+    if k == l
+        s_out = sprintf('P[%d] = q_init[%d];',j-1, k);
+    else
+        s_out = sprintf('P[%d] = 0.0f;',j-1);
+    end
+    fprintf(fid,'\t%s\n', s_out);
 end
-nummults
-numadds
+fclose(fid)
