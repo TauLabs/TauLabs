db6e7f04fd68a8842dbef85a2080bee2ce76b4fd
Daniel Strnad <strnadda@gmail.com>
2014-06-19 20:50:01 +0200

    UAVOFrSKYSPortBridge: added module source skeleton

---
+++ flight/Modules/UAVOFrSkySPortBridge/uavofrskysportbridge.c
@@ -0,0 +1,40 @@
+/**
+ ******************************************************************************
+ * @addtogroup TauLabsModules TauLabs Modules
+ * @{
+ * @addtogroup UAVOFrSkySPortBridge UAVO to FrSKY S.PORT Bridge Module
+ * @{
+ *
+ * @file       uavofrskysportbridge.c
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2014
+ * @brief      Bridges selected UAVObjects to FrSKY Sensor Hub
+ * @see        The GNU Public License (GPL) Version 3
+ *
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "pios.h"
+#include "openpilot.h"
+
+#if defined(PIOS_INCLUDE_FRSKY_SPORT_TELEMETRY)
+
+
+#endif
+/**
+ * @}
+ * @}
+ */
