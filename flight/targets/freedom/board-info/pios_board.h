37d8c5589036812af44e8ded3376d7853289a636
Daniel Strnad <strnadda@gmail.com>
2014-07-01 20:28:20 +0200

    UAVOFrSKYSPortBridge: added to freedom target

--- flight/targets/freedom/board-info/pios_board.h
+++ flight/targets/freedom/board-info/pios_board.h
@@ -120,6 +120,7 @@ extern uintptr_t pios_com_hott_id;
 extern uintptr_t pios_com_frsky_sensor_hub_id;
 extern uintptr_t pios_com_lighttelemetry_id;
 extern uintptr_t pios_com_picoc_id;
+extern uintptr_t pios_com_frsky_sport_id;
 
 #define PIOS_COM_GPS                    (pios_com_gps_id)
 #define PIOS_COM_TELEM_USB              (pios_com_telem_usb_id)
@@ -131,6 +132,7 @@ extern uintptr_t pios_com_picoc_id;
 #define PIOS_COM_FRSKY_SENSOR_HUB       (pios_com_frsky_sensor_hub_id)
 #define PIOS_COM_LIGHTTELEMETRY         (pios_com_lighttelemetry_id)
 #define PIOS_COM_PICOC                  (pios_com_picoc_id)
+#define PIOS_COM_FRSKY_SPORT            (pios_com_frsky_sport_id)
 
 #if defined(PIOS_INCLUDE_DEBUG_CONSOLE)
 extern uintptr_t pios_com_debug_id;
