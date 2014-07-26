4573262610043e710970551ebc79410f49723926
henkf <henk.freimuth@gmail.com>
2014-07-26 15:52:11 +0200

    Updated tools.mk for compatibility with Mac OS 10.10

--- make/tools.mk
+++ make/tools.mk
@@ -22,7 +22,7 @@ ifdef OPENOCD_FTDI
 endif
 
 # Set up QT toolchain
-QT_SDK_DIR := $(TOOLS_DIR)/Qt5.3.0
+QT_SDK_DIR := $(TOOLS_DIR)/Qt5.3.1
 
 # Build openocd without FTDI (yes | no)
 OPENOCD_FTDI ?= yes
@@ -43,7 +43,7 @@ ifdef LINUX
 endif
 
 ifdef MACOSX
-  qt_sdk_install: QT_SDK_URL  := http://download.qt-project.org/official_releases/qt/5.3/5.3.0/qt-opensource-mac-x64-clang-5.3.0.dmg
+  qt_sdk_install: QT_SDK_URL  := http://download.qt-project.org/official_releases/qt/5.3/5.3.1/qt-opensource-mac-x64-clang-5.3.1.dmg
   QT_SDK_QMAKE_PATH := $(QT_SDK_DIR)/5.3/clang_64/bin/qmake
 endif
 
@@ -69,7 +69,7 @@ qt_sdk_install: qt_sdk_clean
 
 ifneq (,$(filter $(UNAME), Darwin))
 	$(V1) hdiutil attach -quiet -private -mountpoint /tmp/qt-installer "$(DL_DIR)/$(QT_SDK_FILE)" 
-	$(V1) /tmp/qt-installer/qt-opensource-mac-x64-clang-5.3.0.app/Contents/MacOS/qt-opensource-mac-x64-clang-5.3.0
+	$(V1) /tmp/qt-installer/qt-opensource-mac-x64-clang-5.3.1.app/Contents/MacOS/qt-opensource-mac-x64-clang-5.3.1
 	$(V1) hdiutil detach -quiet /tmp/qt-installer
 endif
  
