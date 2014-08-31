1aeb5ee0c54678cf6ec4a66845b2d102283c3e53
James Cotton <peabody124@gmail.com>
2014-08-31 10:55:09 -0500

    RFM22B Rcvr: variant of PIOS_GCSRCVR for RFM22B module
    This is from pios_oplinkrcvr which was from pios_gcsrcvr. Renaming
    and reformatting because it applies for any modem board with an
    RFM22B module running our code.
    
    The driver is designed to run on the _flight controller_ side and
    catches updates from a UAVO and presents that on a pios_rcvr
    interface. Later, it might make more sense to have a special packet
    type that the RFM22B uses to represent PPM information rather that
    requiring it to stay synced to a UAVTalk stream.

---
+++ flight/PiOS/Common/pios_rfm22b_rcvr.c
@@ -0,0 +1,188 @@
+/**
+ ******************************************************************************
+ * @addtogroup PIOS PIOS Core hardware abstraction layer
+ * @{
+ * @addtogroup PIOS_RFM22b RFM22b receiver functions
+ * @brief Deals with the RFM22b module
+ * @{
+ *
+ * @file       pios_rfm22b_rcvr.c
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2013.
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2014
+ * @brief      RFM22B Rcvr function. This catches UAVO updates from the
+ *             remote RFM22B (normally from PPM) that are in the form of
+ *             a UAVO and presents them as a PIOS_RCVR interface. This is
+ *             almost identical to PIOS_GCSRCVR but looks at a different
+ *             object.
+ * @see        The GNU Public License (GPL) Version 3
+ *
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <pios.h>
+
+#ifdef PIOS_INCLUDE_RFM22B_RCVR
+
+#include <uavobjectmanager.h>
+#include <oplinkreceiver.h>
+#include <pios_rfm22b_rcvr_priv.h>
+
+#define PIOS_RFM22B_RCVR_TIMEOUT_MS  100
+
+static OPLinkReceiverData oplinkreceiverdata;
+
+/* Provide a RCVR driver */
+static int32_t PIOS_RFM22B_Rcvr_Get(uintptr_t rcvr_id, uint8_t channel);
+static void PIOS_RFM22B_Rcvr_Supervisor(uintptr_t ppm_id);
+
+const struct pios_rcvr_driver pios_rfm22b_rcvr_driver = {
+	.read = PIOS_RFM22B_Rcvr_Get,
+};
+
+/* Local Variables */
+enum pios_rfm22b_rcvr_dev_magic {
+	PIOS_RFM22B_RCVR_DEV_MAGIC = 0x07ab9e2544cf5029,
+};
+
+struct pios_rfm22b_rcvr_dev {
+	enum pios_rfm22b_rcvr_dev_magic magic;
+
+	uint8_t supv_timer;
+	bool fresh;
+};
+
+static struct pios_rfm22b_rcvr_dev *global_rfm22b_rcvr_dev;
+
+static bool PIOS_RFM22B_Rcvr_Validate(struct pios_rfm22b_rcvr_dev
+				      *rfm22b_rcvr_dev)
+{
+	return rfm22b_rcvr_dev->magic == PIOS_RFM22B_RCVR_DEV_MAGIC;
+}
+
+static struct pios_rfm22b_rcvr_dev *PIOS_RFM22B_Rcvr_alloc(void)
+{
+	struct pios_rfm22b_rcvr_dev *rfm22b_rcvr_dev;
+
+	rfm22b_rcvr_dev =
+	    (struct pios_rfm22b_rcvr_dev *)
+	    PIOS_malloc(sizeof(*rfm22b_rcvr_dev));
+	if (!rfm22b_rcvr_dev) {
+		return NULL;
+	}
+
+	rfm22b_rcvr_dev->magic = PIOS_RFM22B_RCVR_DEV_MAGIC;
+	rfm22b_rcvr_dev->fresh = false;
+	rfm22b_rcvr_dev->supv_timer = 0;
+
+	/* The update callback cannot receive the device pointer, so set it in a global */
+	global_rfm22b_rcvr_dev = rfm22b_rcvr_dev;
+
+	return rfm22b_rcvr_dev;
+}
+
+static void oplinkreceiver_updated(UAVObjEvent * ev)
+{
+	struct pios_rfm22b_rcvr_dev *rfm22b_rcvr_dev =
+	    global_rfm22b_rcvr_dev;
+
+	if (ev->obj == OPLinkReceiverHandle()) {
+		OPLinkReceiverGet(&oplinkreceiverdata);
+		rfm22b_rcvr_dev->fresh = true;
+	}
+}
+
+extern int32_t PIOS_RFM22B_Rcvr_Init(uintptr_t * oplinkrcvr_id)
+{
+	struct pios_rfm22b_rcvr_dev *rfm22b_rcvr_dev;
+
+	/* Allocate the device structure */
+	rfm22b_rcvr_dev =
+	    (struct pios_rfm22b_rcvr_dev *)PIOS_RFM22B_Rcvr_alloc();
+	if (!rfm22b_rcvr_dev) {
+		return -1;
+	}
+
+	for (uint8_t i = 0; i < OPLINKRECEIVER_CHANNEL_NUMELEM; i++) {
+		/* Flush channels */
+		oplinkreceiverdata.Channel[i] = PIOS_RCVR_TIMEOUT;
+	}
+
+	/* Register uavobj callback */
+	OPLinkReceiverConnectCallback(oplinkreceiver_updated);
+
+	/* Register the failsafe timer callback. */
+	if (!PIOS_RTC_RegisterTickCallback
+	    (PIOS_RFM22B_Rcvr_Supervisor, (uint32_t) rfm22b_rcvr_dev)) {
+		PIOS_DEBUG_Assert(0);
+	}
+
+	return 0;
+}
+
+/**
+ * Get the value of an input channel
+ * \param[in] channel Number of the channel desired (zero based)
+ * \output PIOS_RCVR_INVALID channel not available
+ * \output PIOS_RCVR_TIMEOUT failsafe condition or missing receiver
+ * \output >=0 channel value
+ */
+static int32_t PIOS_RFM22B_Rcvr_Get(uintptr_t rcvr_id, uint8_t channel)
+{
+	if (channel >= OPLINKRECEIVER_CHANNEL_NUMELEM) {
+		/* channel is out of range */
+		return PIOS_RCVR_INVALID;
+	}
+
+	return oplinkreceiverdata.Channel[channel];
+}
+
+static void PIOS_RFM22B_Rcvr_Supervisor(uintptr_t rfm22b_rcvr_id)
+{
+	/* Recover our device context */
+	struct pios_rfm22b_rcvr_dev *rfm22b_rcvr_dev =
+	    (struct pios_rfm22b_rcvr_dev *)rfm22b_rcvr_id;
+
+	if (!PIOS_RFM22B_Rcvr_Validate(rfm22b_rcvr_dev)) {
+		/* Invalid device specified */
+		return;
+	}
+
+	/*
+	 * RTC runs at 625Hz.
+	 */
+	if (++(rfm22b_rcvr_dev->supv_timer) <
+	    (PIOS_RFM22B_RCVR_TIMEOUT_MS * 1000 / 625)) {
+		return;
+	}
+	rfm22b_rcvr_dev->supv_timer = 0;
+
+	if (!rfm22b_rcvr_dev->fresh) {
+		for (int32_t i = 0; i < OPLINKRECEIVER_CHANNEL_NUMELEM;
+		     i++) {
+			oplinkreceiverdata.Channel[i] = PIOS_RCVR_TIMEOUT;
+		}
+	}
+
+	rfm22b_rcvr_dev->fresh = false;
+}
+
+#endif /* PIOS_INCLUDE_RFM22B_RCVR */
+
+/**
+ * @}
+ * @}
+ */
--- flight/PiOS/inc/pios_rfm22b_rcvr_priv.h
+++ flight/PiOS/inc/pios_rfm22b_rcvr_priv.h
@@ -2,12 +2,13 @@
  ******************************************************************************
  * @addtogroup PIOS PIOS Core hardware abstraction layer
  * @{
- * @addtogroup   PIOS_OPLinkRCVR OPLink Receiver Functions
- * @brief PIOS interface to read from OPLink receiver port
+ * @addtogroup PIOS_RFM22b RFM22b receiver functions
+ * @brief Deals with the RFM22b module
  * @{
  *
- * @file       pios_oplinkrcvr_priv.h
+ * @file       pios_rfm22b_rcvr_priv.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2013.
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2014
  * @brief      OPLINK receiver private functions
  * @see        The GNU Public License (GPL) Version 3
  *
@@ -28,16 +29,16 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#ifndef PIOS_OPLINKRCVR_PRIV_H
-#define PIOS_OPLINKRCVR_PRIV_H
+#ifndef PIOS_RFM22B_RCVR_PRIV_H
+#define PIOS_RFM22B_RCVR_PRIV_H
 
 #include <pios.h>
 
-extern const struct pios_rcvr_driver pios_oplinkrcvr_rcvr_driver;
+extern const struct pios_rcvr_driver pios_rfm22b_rcvr_driver;
 
-extern int32_t PIOS_OPLinkRCVR_Init(uintptr_t *oplinkrcvr_id);
+extern int32_t PIOS_RFM22B_Rcvr_Init(uintptr_t * rfm22b_rcvr_id);
 
-#endif /* PIOS_OPLINKRCVR_PRIV_H */
+#endif /* PIOS_RFM22B_RCVR_PRIV_H */
 
 /**
  * @}
