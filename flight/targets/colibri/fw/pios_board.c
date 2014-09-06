1de475d15df82cae96d9d660a0e5428debdefb2f
James Cotton <peabody124@gmail.com>
2014-09-06 08:57:49 -0500

    Colibri: reorder init sequence of TaskMonitor
    This is following #1246. The TaskMonitor must be initialized
    before the EventDispatcher.

--- flight/targets/colibri/fw/pios_board.c
+++ flight/targets/colibri/fw/pios_board.c
@@ -414,9 +414,15 @@ void PIOS_Board_Init(void)
 		panic(1);
 #endif /* PIOS_INCLUDE_FLASH */
 
+	/* Initialize the task monitor library */
+	TaskMonitorInitialize();
+
 	/* Initialize UAVObject libraries */
 	EventDispatcherInitialize();
 	UAVObjInitialize();
+
+	/* Initialize the alarms library */
+	AlarmsInitialize();
 
 	HwColibriInitialize();
 	ModuleSettingsInitialize();
@@ -434,12 +440,6 @@ void PIOS_Board_Init(void)
 		PIOS_WDG_Init();
 	}
 #endif
-
-	/* Initialize the alarms library */
-	AlarmsInitialize();
-
-	/* Initialize the task monitor library */
-	TaskMonitorInitialize();
 
 	/* Set up pulse timers */
 	//Timers used for inputs (1, 2, 5, 8)
