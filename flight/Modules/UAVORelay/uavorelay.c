1b63201863243ed1934ba66c5f13461e3fb497e9
James Cotton <peabody124@gmail.com>
2014-09-11 23:56:33 -0500

    UAVORelay: hacky send and receive custom messages

--- flight/Modules/UAVORelay/uavorelay.c
+++ flight/Modules/UAVORelay/uavorelay.c
@@ -29,6 +29,8 @@
 
 #include "openpilot.h"
 #include "modulesettings.h"
+
+#include "attitudeactual.h"
 #include "cameradesired.h"
 #include "pios_thread.h"
 
@@ -55,6 +57,7 @@ static uintptr_t uavorelay_com_id;
 
 // External variables
 extern uintptr_t pios_com_can_id;
+extern uintptr_t pios_can_id;
 
 /**
  * Initialise the UAVORelay module
@@ -136,30 +139,76 @@ static void register_object(UAVObjHandle obj)
 
 static void uavoRelayTask(void *parameters)
 {
-	UAVObjEvent ev;
+	//UAVObjEvent ev;
 
 	// Loop forever
 	while (1) {
 
 		PIOS_Thread_Sleep(50);
 
-		// Wait for queue message
-		if (xQueueReceive(queue, &ev, 2) == pdTRUE) {
-			// Process event.  This calls transmitData
-			UAVTalkSendObject(uavTalkCon, ev.obj, ev.instId, false, 0);
+		// // Wait for queue message
+		// if (xQueueReceive(queue, &ev, 2) == pdTRUE) {
+		// 	// Process event.  This calls transmitData
+		// 	UAVTalkSendObject(uavTalkCon, ev.obj, ev.instId, false, 0);
+		// }
+
+		// // Process incoming data in sufficient chunks that we keep up
+		// uint8_t serial_data[8];
+		// uint16_t bytes_to_process;
+
+		// bytes_to_process = PIOS_COM_ReceiveBuffer(uavorelay_com_id, serial_data, sizeof(serial_data), 0);
+		// do {
+		// 	bytes_to_process = PIOS_COM_ReceiveBuffer(uavorelay_com_id, serial_data, sizeof(serial_data), 0);
+		// 	for (uint8_t i = 0; i < bytes_to_process; i++)
+		// 		UAVTalkProcessInputStream(uavTalkCon,serial_data[i]);
+		// } while (bytes_to_process > 0);
+
+		struct bgc_message {
+			// Transmit flight controller attitude as euler angles
+			int8_t fc_roll;
+			int8_t fc_pitch;
+			uint8_t fc_yaw;
+			int8_t setpoint_roll;
+			int8_t setpoint_pitch;
+			uint8_t setpoint_yaw;
+		};
+
+		if (true) { // transmitting
+
+			AttitudeActualData attitude;
+			AttitudeActualGet(&attitude);
+
+			CameraDesiredData cameraDesired;
+			CameraDesiredGet(&cameraDesired);
+
+			struct bgc_message bgc_message = {
+				.fc_roll = attitude.Roll,
+				.fc_pitch = attitude.Pitch,
+				.fc_yaw = attitude.Yaw,
+				.setpoint_roll = 0,
+				.setpoint_pitch = cameraDesired.Declination,
+				.setpoint_yaw = cameraDesired.Bearing
+			};
+
+			PIOS_CAN_TxData(pios_can_id, 0x130, (uint8_t *) &bgc_message, sizeof(bgc_message));
+		} else { // receiving
+
+			struct bgc_message bgc_message;
+
+			if (PIOS_CAN_RxData(pios_can_id, 0x130, (uint8_t *) &bgc_message) == sizeof(bgc_message)) {
+
+				CameraDesiredData cameraDesired;
+				CameraDesiredGet(&cameraDesired);
+				cameraDesired.Declination = bgc_message.setpoint_pitch;
+				cameraDesired.Bearing = bgc_message.setpoint_yaw;
+				cameraDesired.Roll = bgc_message.fc_roll;
+				cameraDesired.Pitch = bgc_message.fc_pitch;
+				cameraDesired.Yaw = bgc_message.fc_yaw;
+				CameraDesiredSet(&cameraDesired);
+
+			}
+>>>>>>> UAVORelay: hacky send and receive custom messages
 		}
-
-		// Process incoming data in sufficient chunks that we keep up
-		uint8_t serial_data[8];
-		uint16_t bytes_to_process;
-
-		bytes_to_process = PIOS_COM_ReceiveBuffer(uavorelay_com_id, serial_data, sizeof(serial_data), 0);
-		do {
-			bytes_to_process = PIOS_COM_ReceiveBuffer(uavorelay_com_id, serial_data, sizeof(serial_data), 0);
-			for (uint8_t i = 0; i < bytes_to_process; i++)
-				UAVTalkProcessInputStream(uavTalkCon,serial_data[i]);
-		} while (bytes_to_process > 0);
-
 	}
 }
 
