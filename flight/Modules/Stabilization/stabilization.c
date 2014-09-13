a10152c7217798612ea186991ab371064ecf99c6
James Cotton <peabody124@gmail.com>
2014-09-13 15:02:11 -0500

    Stabilization: call ZeroPids zero_pids
    This is for consistency with the rest of the code that tries
    to avoid CamelCase in the firmware side.

--- flight/Modules/Stabilization/stabilization.c
+++ flight/Modules/Stabilization/stabilization.c
@@ -112,7 +112,7 @@ struct pid pids[PID_MAX];
 
 // Private functions
 static void stabilizationTask(void* parameters);
-static void ZeroPids(void);
+static void zero_pids(void);
 static void calculate_pids(void);
 static void SettingsUpdatedCb(UAVObjEvent * ev);
 
@@ -192,7 +192,7 @@ static void stabilizationTask(void* parameters)
 	uint32_t system_ident_timeval = PIOS_DELAY_GetRaw();
 
 	// Main task loop
-	ZeroPids();
+	zero_pids();
 	while(1) {
 		iteration++;
 
@@ -650,7 +650,7 @@ static void stabilizationTask(void* parameters)
 /**
  * Clear the accumulators and derivatives for all the axes
  */
-static void ZeroPids(void)
+static void zero_pids(void)
 {
 	for(uint32_t i = 0; i < PID_MAX; i++)
 		pid_zero(&pids[i]);
