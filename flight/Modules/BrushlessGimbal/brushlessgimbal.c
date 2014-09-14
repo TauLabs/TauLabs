dd66c1a3b1298a6173bfeaed6242ba9389cc783b
James Cotton <peabody124@gmail.com>
2014-09-13 19:21:10 -0500

    Brushless: lock gimbal for extreme angles
    Whenever the frame is heavily tilted the gimbal will
    lock in the current position. Similarly if the setpoint
    is too low, it will lock.

--- flight/Modules/BrushlessGimbal/brushlessgimbal.c
+++ flight/Modules/BrushlessGimbal/brushlessgimbal.c
@@ -31,9 +31,11 @@
 
 
 #include "openpilot.h"
+#include "misc_math.h"
 
 #include "actuatordesired.h"
 #include "brushlessgimbalsettings.h"
+#include "cameradesired.h"
 #include "gyros.h"
 #include "pios_thread.h"
 #include "pios_queue.h"
@@ -120,31 +122,49 @@ static void brushlessGimbalTask(void* parameters)
 		if (!armed)
 			continue;
 
-		ActuatorDesiredData actuatorDesired;
-		ActuatorDesiredGet(&actuatorDesired);
+		CameraDesiredData cameraDesired;
+		CameraDesiredGet(&cameraDesired);
 
-		// Set the rotation in electrical degrees per second.  Note these
-		// will be divided by the number of physical poles to get real
-		// mechanical degrees per second
-		BrushlessGimbalSettingsData settings;
-		BrushlessGimbalSettingsGet(&settings);
+		bool locked = false;
 
-		PIOS_Brushless_SetScale(settings.PowerScale[0], settings.PowerScale[1], settings.PowerScale[2]);
-		PIOS_Brushless_SetMaxAcceleration(settings.SlewLimit[0], settings.SlewLimit[1], settings.SlewLimit[2]);
+		// If the frame is tilted by a large amount, lock the gimbal
+		locked |= fabsf(cameraDesired.Roll) > 55.0f || fabsf(cameraDesired.Pitch) > 55.0f;
 
-		PIOS_Brushless_SetSpeed(0, actuatorDesired.Roll * settings.MaxDPS[BRUSHLESSGIMBALSETTINGS_MAXDPS_ROLL], 0.001f);
-		PIOS_Brushless_SetSpeed(1, actuatorDesired.Pitch  * settings.MaxDPS[BRUSHLESSGIMBALSETTINGS_MAXDPS_PITCH], 0.001f);
+		// If the difference between the setpoint and the frame is too large, lock the gimbal
+		float pitch_setpoint = -cameraDesired.Declination;
+		locked |= circular_modulus_deg(cameraDesired.Pitch - pitch_setpoint) > 55.0f;
 
-		// Use the gyros to set a damping term.  This creates a phase offset of the integrated
-		// driving position to make the control pull against any momentum.  Essentially the main
-		// output to the driver (above) is a velocity signal which the driver takes care of
-		// integrating to create a position.  The current rate of roll creates a shift in that
-		// position (without changing the integrated position).
-		// This idea was taken from https://code.google.com/p/brushless-gimbal/
-		GyrosData gyros;
-		GyrosGet(&gyros);
-		PIOS_Brushless_SetPhaseLag(0, -gyros.x * settings.Damping[BRUSHLESSGIMBALSETTINGS_DAMPING_ROLL]);
-		PIOS_Brushless_SetPhaseLag(1, -gyros.y * settings.Damping[BRUSHLESSGIMBALSETTINGS_DAMPING_PITCH]);
+		PIOS_Brushless_Lock(locked);
+
+		if (!locked) {
+
+			ActuatorDesiredData actuatorDesired;
+			ActuatorDesiredGet(&actuatorDesired);
+
+			// Set the rotation in electrical degrees per second.  Note these
+			// will be divided by the number of physical poles to get real
+			// mechanical degrees per second
+			BrushlessGimbalSettingsData settings;
+			BrushlessGimbalSettingsGet(&settings);
+
+			PIOS_Brushless_SetScale(settings.PowerScale[0], settings.PowerScale[1], settings.PowerScale[2]);
+			PIOS_Brushless_SetMaxAcceleration(settings.SlewLimit[0], settings.SlewLimit[1], settings.SlewLimit[2]);
+
+			PIOS_Brushless_SetSpeed(0, actuatorDesired.Roll * settings.MaxDPS[BRUSHLESSGIMBALSETTINGS_MAXDPS_ROLL], 0.001f);
+			PIOS_Brushless_SetSpeed(1, actuatorDesired.Pitch  * settings.MaxDPS[BRUSHLESSGIMBALSETTINGS_MAXDPS_PITCH], 0.001f);
+
+			// Use the gyros to set a damping term.  This creates a phase offset of the integrated
+			// driving position to make the control pull against any momentum.  Essentially the main
+			// output to the driver (above) is a velocity signal which the driver takes care of
+			// integrating to create a position.  The current rate of roll creates a shift in that
+			// position (without changing the integrated position).
+			// This idea was taken from https://code.google.com/p/brushless-gimbal/
+			GyrosData gyros;
+			GyrosGet(&gyros);
+			PIOS_Brushless_SetPhaseLag(0, -gyros.x * settings.Damping[BRUSHLESSGIMBALSETTINGS_DAMPING_ROLL]);
+			PIOS_Brushless_SetPhaseLag(1, -gyros.y * settings.Damping[BRUSHLESSGIMBALSETTINGS_DAMPING_PITCH]);
+		}
+
 	}
 }
 
