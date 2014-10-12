cbc90b81f98ed9ac37824544edd805149942d445
James Cotton <peabody124@gmail.com>
2014-10-12 13:23:57 -0500

    PiOS/BMP085: fix indentation to use tabs
    This is the format used by the flight code.

--- flight/PiOS/Common/pios_bmp085.c
+++ flight/PiOS/Common/pios_bmp085.c
@@ -66,41 +66,41 @@ static void PIOS_BMP085_Task(void *parameters);
 /* Local Types */
 
 enum pios_bmp085_dev_magic {
-    PIOS_BMP085_DEV_MAGIC = 0xefa26e3d,
+	PIOS_BMP085_DEV_MAGIC = 0xefa26e3d,
 };
 
 enum conversion_type {
-    PRESSURE_CONV,
-    TEMPERATURE_CONV
+	PRESSURE_CONV,
+	TEMPERATURE_CONV
 };
 
 struct bmp085_dev {
-    const struct pios_bmp085_cfg *cfg;
-    uint32_t i2c_id;
-    struct pios_thread *task;
-    struct pios_queue *queue;
+	const struct pios_bmp085_cfg *cfg;
+	uint32_t i2c_id;
+	struct pios_thread *task;
+	struct pios_queue *queue;
 
-    int64_t pressure_unscaled;
-    int64_t temperature_unscaled;
-    int16_t AC1;
-    int16_t AC2;
-    int16_t AC3;
-    uint16_t AC4;
-    uint16_t AC5;
-    uint16_t AC6;
-    int16_t B1;
-    int16_t B2;
-    int16_t MB;
-    int16_t MC;
-    int16_t MD;
+	int64_t pressure_unscaled;
+	int64_t temperature_unscaled;
+	int16_t AC1;
+	int16_t AC2;
+	int16_t AC3;
+	uint16_t AC4;
+	uint16_t AC5;
+	uint16_t AC6;
+	int16_t B1;
+	int16_t B2;
+	int16_t MB;
+	int16_t MC;
+	int16_t MD;
 
-    enum conversion_type current_conversion_type;
-    enum pios_bmp085_osr oversampling;
-    uint32_t temperature_interleaving;
-    int32_t bmp085_read_flag;
-    enum pios_bmp085_dev_magic magic;
+	enum conversion_type current_conversion_type;
+	enum pios_bmp085_osr oversampling;
+	uint32_t temperature_interleaving;
+	int32_t bmp085_read_flag;
+	enum pios_bmp085_dev_magic magic;
 
-    struct pios_semaphore *busy;
+	struct pios_semaphore *busy;
 };
 
 static struct bmp085_dev *dev;
@@ -112,20 +112,20 @@ static struct bmp085_dev *PIOS_BMP085_alloc(void)
 {
    struct bmp085_dev *bmp085_dev;
 
-    bmp085_dev = (struct bmp085_dev *)PIOS_malloc(sizeof(*bmp085_dev));
-    if (!bmp085_dev) return (NULL);
+	bmp085_dev = (struct bmp085_dev *)PIOS_malloc(sizeof(*bmp085_dev));
+	if (!bmp085_dev) return (NULL);
 
-    bmp085_dev->queue = PIOS_Queue_Create(1, sizeof(struct pios_sensor_baro_data));
-    if (bmp085_dev->queue == NULL) {
-        PIOS_free(bmp085_dev);
-        return NULL;
-    }
+	bmp085_dev->queue = PIOS_Queue_Create(1, sizeof(struct pios_sensor_baro_data));
+	if (bmp085_dev->queue == NULL) {
+		PIOS_free(bmp085_dev);
+		return NULL;
+	}
 
 	bmp085_dev->busy = PIOS_Semaphore_Create();
 
-    bmp085_dev->magic = PIOS_BMP085_DEV_MAGIC;
+	bmp085_dev->magic = PIOS_BMP085_DEV_MAGIC;
 
-    return(bmp085_dev);
+	return(bmp085_dev);
 }
 
 /**
@@ -134,13 +134,13 @@ static struct bmp085_dev *PIOS_BMP085_alloc(void)
  */
 static int32_t PIOS_BMP085_Validate(struct bmp085_dev *dev)
 {
-    if (dev == NULL)
-        return -1;
-    if (dev->magic != PIOS_BMP085_DEV_MAGIC)
-        return -2;
-    if (dev->i2c_id == 0)
-        return -3;
-    return 0;
+	if (dev == NULL)
+		return -1;
+	if (dev->magic != PIOS_BMP085_DEV_MAGIC)
+		return -2;
+	if (dev->i2c_id == 0)
+		return -3;
+	return 0;
 }
 
 /**
@@ -148,51 +148,51 @@ static int32_t PIOS_BMP085_Validate(struct bmp085_dev *dev)
  */
 int32_t PIOS_BMP085_Init(const struct pios_bmp085_cfg *cfg, int32_t i2c_device)
 {
-    dev = (struct bmp085_dev *) PIOS_BMP085_alloc();
-    if (dev == NULL)
-        return -1;
+	dev = (struct bmp085_dev *) PIOS_BMP085_alloc();
+	if (dev == NULL)
+		return -1;
 
-    dev->i2c_id = i2c_device;
+	dev->i2c_id = i2c_device;
 
-    dev->oversampling = cfg->oversampling;
-    dev->temperature_interleaving = (cfg->temperature_interleaving) == 0 ? 1 : cfg->temperature_interleaving;
-    dev->cfg = cfg;
+	dev->oversampling = cfg->oversampling;
+	dev->temperature_interleaving = (cfg->temperature_interleaving) == 0 ? 1 : cfg->temperature_interleaving;
+	dev->cfg = cfg;
 
-    uint8_t data[22];
-    if (PIOS_BMP085_Read(BMP085_CALIB_ADDR, data, 2) != 0)
-    	return -2;
-    dev->AC1 = (data[0] << 8) | data[1];
-    if (PIOS_BMP085_Read(BMP085_CALIB_ADDR, data, 2) != 0)
-    	return -2;
-    dev->AC1 = (data[0] << 8) | data[1];
+	uint8_t data[22];
+	if (PIOS_BMP085_Read(BMP085_CALIB_ADDR, data, 2) != 0)
+		return -2;
+	dev->AC1 = (data[0] << 8) | data[1];
+	if (PIOS_BMP085_Read(BMP085_CALIB_ADDR, data, 2) != 0)
+		return -2;
+	dev->AC1 = (data[0] << 8) | data[1];
 
-    if (PIOS_BMP085_Read(BMP085_CALIB_ADDR, data, 22) != 0)
-    	return -2;
-    /* Parameters AC1-AC6 */
-    dev->AC1 = (data[0] << 8) | data[1];
-    dev->AC2 = (data[2] << 8) | data[3];
-    dev->AC3 = (data[4] << 8) | data[5];
-    dev->AC4 = (data[6] << 8) | data[7];
-    dev->AC5 = (data[8] << 8) | data[9];
-    dev->AC6 = (data[10] << 8) | data[11];
+	if (PIOS_BMP085_Read(BMP085_CALIB_ADDR, data, 22) != 0)
+		return -2;
+	/* Parameters AC1-AC6 */
+	dev->AC1 = (data[0] << 8) | data[1];
+	dev->AC2 = (data[2] << 8) | data[3];
+	dev->AC3 = (data[4] << 8) | data[5];
+	dev->AC4 = (data[6] << 8) | data[7];
+	dev->AC5 = (data[8] << 8) | data[9];
+	dev->AC6 = (data[10] << 8) | data[11];
 
-    /* Parameters B1, B2 */
-    dev->B1  = (data[12] << 8) | data[13];
-    dev->B2  = (data[14] << 8) | data[15];
+	/* Parameters B1, B2 */
+	dev->B1  = (data[12] << 8) | data[13];
+	dev->B2  = (data[14] << 8) | data[15];
 
-    /* Parameters MB, MC, MD */
-    dev->MB  = (data[16] << 8) | data[17];
-    dev->MC  = (data[18] << 8) | data[19];
-    dev->MD  = (data[20] << 8) | data[21];
+	/* Parameters MB, MC, MD */
+	dev->MB  = (data[16] << 8) | data[17];
+	dev->MC  = (data[18] << 8) | data[19];
+	dev->MD  = (data[20] << 8) | data[21];
 
-    dev->task = PIOS_Thread_Create(
-    	PIOS_BMP085_Task, "pios_bmp085", BMP085_TASK_STACK, NULL, BMP085_TASK_PRIORITY);
-    if (dev->task == NULL)
-    	return -3;
+	dev->task = PIOS_Thread_Create(
+		PIOS_BMP085_Task, "pios_bmp085", BMP085_TASK_STACK, NULL, BMP085_TASK_PRIORITY);
+	if (dev->task == NULL)
+		return -3;
 
-    PIOS_SENSORS_Register(PIOS_SENSOR_BARO, dev->queue);
+	PIOS_SENSORS_Register(PIOS_SENSOR_BARO, dev->queue);
 
-    return 0;
+	return 0;
 }
 
 
@@ -227,48 +227,48 @@ static int32_t PIOS_BMP085_ReleaseDevice(void)
 */
 static int32_t PIOS_BMP085_StartADC(enum conversion_type type)
 {
-    if (PIOS_BMP085_Validate(dev) != 0)
-        return -1;
+	if (PIOS_BMP085_Validate(dev) != 0)
+		return -1;
 
-    /* Start the conversion */
-    switch(type) {
-    case TEMPERATURE_CONV:
-        while (PIOS_BMP085_WriteCommand(BMP085_CTRL_ADDR, BMP085_TEMP_ADDR) != 0)
-            continue;
-        break;
-    case PRESSURE_CONV:
-        while (PIOS_BMP085_WriteCommand(BMP085_CTRL_ADDR, BMP085_PRES_ADDR + (dev->oversampling << 6)) != 0)
-            continue;
-        break;
-    default:
-        return -1;
-    }
+	/* Start the conversion */
+	switch(type) {
+	case TEMPERATURE_CONV:
+		while (PIOS_BMP085_WriteCommand(BMP085_CTRL_ADDR, BMP085_TEMP_ADDR) != 0)
+			continue;
+		break;
+	case PRESSURE_CONV:
+		while (PIOS_BMP085_WriteCommand(BMP085_CTRL_ADDR, BMP085_PRES_ADDR + (dev->oversampling << 6)) != 0)
+			continue;
+		break;
+	default:
+		return -1;
+	}
 
-    dev->current_conversion_type = type;
+	dev->current_conversion_type = type;
 
-    return 0;
+	return 0;
 }
 
 /**
  * @brief Return the delay for the current osr
  */
 static int32_t PIOS_BMP085_GetDelay() {
-    if (PIOS_BMP085_Validate(dev) != 0)
-        return 100;
+	if (PIOS_BMP085_Validate(dev) != 0)
+		return 100;
 
-    switch(dev->oversampling) {
-        case BMP085_OSR_0:
-            return 5;
-        case BMP085_OSR_1:
-            return 8;
-        case BMP085_OSR_2:
-            return 14;
-        case BMP085_OSR_3:
-            return 26;
-        default:
-            break;
-    }
-    return 26;
+	switch(dev->oversampling) {
+		case BMP085_OSR_0:
+			return 5;
+		case BMP085_OSR_1:
+			return 8;
+		case BMP085_OSR_2:
+			return 14;
+		case BMP085_OSR_3:
+			return 26;
+		default:
+			break;
+	}
+	return 26;
 }
 
 /**
@@ -277,32 +277,32 @@ static int32_t PIOS_BMP085_GetDelay() {
 */
 static int32_t PIOS_BMP085_ReadADC(void)
 {
-    if (PIOS_BMP085_Validate(dev) != 0)
-        return -1;
+	if (PIOS_BMP085_Validate(dev) != 0)
+		return -1;
 
-    uint8_t data[3];
+	uint8_t data[3];
 
-    /* Read and store the 16bit result */
-    if (dev->current_conversion_type == TEMPERATURE_CONV) {
-        uint32_t raw_temperature;
-        /* Read the temperature conversion */
-        if (PIOS_BMP085_Read(BMP085_ADC_MSB, data, 2) != 0)
-            return -1;
+	/* Read and store the 16bit result */
+	if (dev->current_conversion_type == TEMPERATURE_CONV) {
+		uint32_t raw_temperature;
+		/* Read the temperature conversion */
+		if (PIOS_BMP085_Read(BMP085_ADC_MSB, data, 2) != 0)
+			return -1;
 
-        raw_temperature = ((data[0] << 8) | data[1]);
+		raw_temperature = ((data[0] << 8) | data[1]);
 
-        X1 = (raw_temperature - dev->AC6) * dev->AC5 >> 15;
-        X2 = ((int32_t)dev->MC << 11) / (X1 + dev->MD);
-        B5 = X1 + X2;
-        dev->temperature_unscaled = (B5 + 8) >> 4;//temperature;
-    } else {
-        uint32_t raw_pressure;
+		X1 = (raw_temperature - dev->AC6) * dev->AC5 >> 15;
+		X2 = ((int32_t)dev->MC << 11) / (X1 + dev->MD);
+		B5 = X1 + X2;
+		dev->temperature_unscaled = (B5 + 8) >> 4;//temperature;
+	} else {
+		uint32_t raw_pressure;
 
-        /* Read the pressure conversion */
-        if (PIOS_BMP085_Read(BMP085_ADC_MSB, data, 3) != 0)
-            return -1;
+		/* Read the pressure conversion */
+		if (PIOS_BMP085_Read(BMP085_ADC_MSB, data, 3) != 0)
+			return -1;
 
-        raw_pressure = ((data[0] << 16) | (data[1] << 8) | data[2]) >> (8 - dev->oversampling);
+		raw_pressure = ((data[0] << 16) | (data[1] << 8) | data[2]) >> (8 - dev->oversampling);
 
 		B6 = B5 - 4000;
 		X1 = (dev->B2 * (B6 * B6 >> 12)) >> 11;
@@ -315,12 +315,12 @@ static int32_t PIOS_BMP085_ReadADC(void)
 		B4 = (dev->AC4 * (uint32_t)(X3 + 32768)) >> 15;
 		B7 = ((uint32_t)raw_pressure - B3) * (50000 >> dev->oversampling);
 		P  = B7 < 0x80000000 ? (B7 * 2) / B4 : (B7 / B4) * 2;
-        X1 = (P >> 8) * (P >> 8);
-        X1 = (X1 * 3038) >> 16;
-        X2 = (-7357 * P) >> 16;
-        dev->pressure_unscaled = P + ((X1 + X2 + 3791) >> 4);
-    }
-    return 0;
+		X1 = (P >> 8) * (P >> 8);
+		X1 = (X1 * 3038) >> 16;
+		X2 = (-7357 * P) >> 16;
+		dev->pressure_unscaled = P + ((X1 + X2 + 3791) >> 4);
+	}
+	return 0;
 }
 
 /*
@@ -334,28 +334,28 @@ static int32_t PIOS_BMP085_ReadADC(void)
 static int32_t PIOS_BMP085_Read(uint8_t address, uint8_t *buffer, uint8_t len)
 {
 
-    if (PIOS_BMP085_Validate(dev) != 0)
-        return -1;
+	if (PIOS_BMP085_Validate(dev) != 0)
+		return -1;
 
-    const struct pios_i2c_txn txn_list[] = {
-        {
-            .info = __func__,
-            .addr = BMP085_I2C_ADDR,
-            .rw = PIOS_I2C_TXN_WRITE,
-            .len = 1,
-            .buf = &address,
-        }
-        ,
-        {
-         .info = __func__,
-         .addr = BMP085_I2C_ADDR,
-         .rw = PIOS_I2C_TXN_READ,
-         .len = len,
-         .buf = buffer,
-         }
-    };
+	const struct pios_i2c_txn txn_list[] = {
+		{
+			.info = __func__,
+			.addr = BMP085_I2C_ADDR,
+			.rw = PIOS_I2C_TXN_WRITE,
+			.len = 1,
+			.buf = &address,
+		}
+		,
+		{
+		 .info = __func__,
+		 .addr = BMP085_I2C_ADDR,
+		 .rw = PIOS_I2C_TXN_READ,
+		 .len = len,
+		 .buf = buffer,
+		 }
+	};
 
-    return PIOS_I2C_Transfer(dev->i2c_id, txn_list, NELEMENTS(txn_list));
+	return PIOS_I2C_Transfer(dev->i2c_id, txn_list, NELEMENTS(txn_list));
 }
 
 /**
@@ -367,26 +367,26 @@ static int32_t PIOS_BMP085_Read(uint8_t address, uint8_t *buffer, uint8_t len)
 */
 static int32_t PIOS_BMP085_WriteCommand(uint8_t address, uint8_t buffer)
 {
-    uint8_t data[] = {
-        address,
-        buffer,
-    };
+	uint8_t data[] = {
+		address,
+		buffer,
+	};
 
-    if (PIOS_BMP085_Validate(dev) != 0)
-        return -1;
+	if (PIOS_BMP085_Validate(dev) != 0)
+		return -1;
 
-    const struct pios_i2c_txn txn_list[] = {
-        {
-         .info = __func__,
-         .addr = BMP085_I2C_ADDR,
-         .rw = PIOS_I2C_TXN_WRITE,
-         .len = sizeof(data),
-         .buf = data,
-         }
-        ,
-    };
+	const struct pios_i2c_txn txn_list[] = {
+		{
+		 .info = __func__,
+		 .addr = BMP085_I2C_ADDR,
+		 .rw = PIOS_I2C_TXN_WRITE,
+		 .len = sizeof(data),
+		 .buf = data,
+		 }
+		,
+	};
 
-    return PIOS_I2C_Transfer(dev->i2c_id, txn_list, NELEMENTS(txn_list));
+	return PIOS_I2C_Transfer(dev->i2c_id, txn_list, NELEMENTS(txn_list));
 }
 
 /**
@@ -395,64 +395,64 @@ static int32_t PIOS_BMP085_WriteCommand(uint8_t address, uint8_t buffer)
 */
 int32_t PIOS_BMP085_Test()
 {
-    if (PIOS_BMP085_Validate(dev) != 0)
-        return -1;
+	if (PIOS_BMP085_Validate(dev) != 0)
+		return -1;
 
-    // TODO: Is there a better way to test this than just checking that pressure/temperature has changed?
-    int32_t cur_value = 0;
-
-	PIOS_BMP085_ClaimDevice();
-    cur_value = dev->temperature_unscaled;
-    PIOS_BMP085_StartADC(TEMPERATURE_CONV);
-    PIOS_DELAY_WaitmS(5);
-    PIOS_BMP085_ReadADC();
-    PIOS_BMP085_ReleaseDevice();
-    if (cur_value == dev->temperature_unscaled)
-        return -1;
+	// TODO: Is there a better way to test this than just checking that pressure/temperature has changed?
+	int32_t cur_value = 0;
 
 	PIOS_BMP085_ClaimDevice();
-    cur_value = dev->pressure_unscaled;
-    PIOS_BMP085_StartADC(PRESSURE_CONV);
-    PIOS_DELAY_WaitmS(26);
-    PIOS_BMP085_ReadADC();
-    PIOS_BMP085_ReleaseDevice();
-    if (cur_value == dev->pressure_unscaled)
-        return -1;
+	cur_value = dev->temperature_unscaled;
+	PIOS_BMP085_StartADC(TEMPERATURE_CONV);
+	PIOS_DELAY_WaitmS(5);
+	PIOS_BMP085_ReadADC();
+	PIOS_BMP085_ReleaseDevice();
+	if (cur_value == dev->temperature_unscaled)
+		return -1;
 
-    return 0;
+	PIOS_BMP085_ClaimDevice();
+	cur_value = dev->pressure_unscaled;
+	PIOS_BMP085_StartADC(PRESSURE_CONV);
+	PIOS_DELAY_WaitmS(26);
+	PIOS_BMP085_ReadADC();
+	PIOS_BMP085_ReleaseDevice();
+	if (cur_value == dev->pressure_unscaled)
+		return -1;
+
+	return 0;
 }
 static void PIOS_BMP085_Task(void *parameters)
 {
-    int32_t temp_press_interleave_count = dev->temperature_interleaving;
+	int32_t temp_press_interleave_count = dev->temperature_interleaving;
 
-    while (1) {
+	while (1) {
 
-        temp_press_interleave_count --;
-        if(temp_press_interleave_count <= 0)
-        {
-            // Update the temperature data
+		temp_press_interleave_count --;
+		if(temp_press_interleave_count <= 0)
+		{
+			// Update the temperature data
 			PIOS_BMP085_ClaimDevice();
-            PIOS_BMP085_StartADC(TEMPERATURE_CONV);
-            PIOS_Thread_Sleep(5);
-            PIOS_BMP085_ReadADC();
-            PIOS_BMP085_ReleaseDevice();
-            temp_press_interleave_count = dev->temperature_interleaving;
-        }
-        // Update the pressure data
+			PIOS_BMP085_StartADC(TEMPERATURE_CONV);
+			PIOS_Thread_Sleep(5);
+			PIOS_BMP085_ReadADC();
+			PIOS_BMP085_ReleaseDevice();
+			temp_press_interleave_count = dev->temperature_interleaving;
+		}
+		// Update the pressure data
 		PIOS_BMP085_ClaimDevice();
-        PIOS_BMP085_StartADC(PRESSURE_CONV);
-        PIOS_Thread_Sleep(PIOS_BMP085_GetDelay());
-        PIOS_BMP085_ReadADC();
-        PIOS_BMP085_ReleaseDevice();
+		PIOS_BMP085_StartADC(PRESSURE_CONV);
+		PIOS_Thread_Sleep(PIOS_BMP085_GetDelay());
+		PIOS_BMP085_ReadADC();
+		PIOS_BMP085_ReleaseDevice();
 
-        // Compute the altitude from the pressure and temperature and send it out
-        struct pios_sensor_baro_data data;
-        data.temperature = ((float) dev->temperature_unscaled) / 10.0f;
-        data.pressure = ((float) dev->pressure_unscaled) / 1000.0f;
-        data.altitude = 44330.0f * (1.0f - powf(data.pressure / BMP085_P0, (1.0f / 5.255f)));
+		// Compute the altitude from the pressure and temperature and send it out
+		struct pios_sensor_baro_data data;
+		data.temperature = ((float) dev->temperature_unscaled) / 10.0f;
+		data.pressure = ((float) dev->pressure_unscaled) / 1000.0f;
+		data.altitude = 44330.0f * (1.0f - powf(data.pressure / BMP085_P0, (1.0f / 5.255f)));
 
-        PIOS_Queue_Send(dev->queue, (void*)&data, 0);
-    }
+		PIOS_Queue_Send(dev->queue, (void*)&data, 0);
+	}
 }
 
 #endif /* PIOS_INCLUDE_BMP085 */
