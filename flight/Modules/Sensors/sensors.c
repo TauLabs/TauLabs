441ce46e428108610520290cd3881b8850d2df03
BrainFPV <brainfpv@gmail.com>
2014-10-24 15:52:08 -0400

    FIX: make ext. mag failure brain conditional

--- flight/Modules/Sensors/sensors.c
+++ flight/Modules/Sensors/sensors.c
@@ -96,8 +96,10 @@ static float z_accel_offset = 0;
 static float Rsb[3][3] = {{0}}; //! Rotation matrix that transforms from the body frame to the sensor board frame
 static int8_t rotate = 0;
 
+#if defined(BRAIN)
 // indicates whether the extrnal mag works
 extern bool brain_external_mag_fail;
+#endif
 
 //! Select the algorithm to try and null out the magnetometer bias error
 static enum mag_calibration_algo mag_calibration_algo = MAG_CALIBRATION_PRELEMARI;
@@ -216,7 +218,11 @@ static void SensorsTask(void *parameters)
 			update_baro(&baro);
 		}
 
+#if defined(BRAIN)
 		if ((good_runs > REQUIRED_GOOD_CYCLES) && !brain_external_mag_fail)
+#else
+		if (good_runs > REQUIRED_GOOD_CYCLES)
+#endif
 			AlarmsClear(SYSTEMALARMS_ALARM_SENSORS);
 		else
 			good_runs++;
--- flight/targets/brain/fw/pios_config.h
+++ flight/targets/brain/fw/pios_config.h
@@ -131,6 +131,7 @@
 #define IDLE_COUNTS_PER_SEC_AT_NO_LOAD (6984538)
 
 #define REVOLUTION
+#define BRAIN
 
 #endif /* PIOS_CONFIG_H */
 /**
