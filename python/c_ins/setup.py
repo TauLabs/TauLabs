13f3391c5d7673c66aa328064f975e98c03b1a33
James Cotton <peabody124@gmail.com>
2014-11-28 13:22:43 -0600

    Python/CINS: setup.py uses 14 state INS

--- python/c_ins/setup.py
+++ python/c_ins/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension, Command
 import numpy
 
 module1 = Extension('ins',
-	sources = ['insmodule.c', '../../flight/Libraries/insgps13state.c'],
+	sources = ['insmodule.c', '../../flight/Libraries/insgps14state.c'],
 	include_dirs=['../../flight/Libraries/inc','../../shared/api',numpy.get_include()])
  
 setup (name = 'PackageName',
