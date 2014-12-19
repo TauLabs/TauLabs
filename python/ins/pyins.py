e8452eeae3d92510f83ef8b15361ae6a9c6ca6f0
James Cotton <peabody124@gmail.com>
2014-12-19 05:13:20 -0500

    Python/INS: update same sensors as python implementation
    This does not update the velocity vertical component (for now).

--- python/ins/pyins.py
+++ python/ins/pyins.py
@@ -47,7 +47,7 @@ class PyINS:
 	def prepare(self):
 		""" prepare the C INS wrapper
 		"""
-		ins.init()
+		self.state = ins.init()
 		self.configure(
 			mag_var=default_mag_var,
 			gyro_var=default_gyro_var,
@@ -77,145 +77,21 @@ class PyINS:
 			Z[1] = pos[1]
 
 		if vel is not None:
-			sensors = sensors | 0x0038
+			sensors = sensors | 0x0018
 			Z[3] = vel[0]
 			Z[4] = vel[1]
-			Z[5] = vel[2]
 
 		if mag is not None:
-			sensors = sensors | 0x01C0
+			sensors = sensors | 0x00C0
 			Z[6] = mag[0]
 			Z[7] = mag[1]
-			Z[8] = mag[2]
+			#Z[8] = mag[2]
 
 		if baro is not None:
 			sensors = sensors | 0x0200
 			Z[9] = baro
 
-		ins.correction(Z, sensors)
-
-def run_uavo_list(uavo_list):
-
-	import taulabs
-	import math
-
-	print "Replaying log file"
-
-	attitude = uavo_list.as_numpy_array(taulabs.uavo.UAVO_AttitudeActual)
-	gyros = uavo_list.as_numpy_array(taulabs.uavo.UAVO_Gyros)
-	accels = uavo_list.as_numpy_array(taulabs.uavo.UAVO_Accels)
-	#ned = uavo_list.as_numpy_array(taulabs.uavo.UAVO_NEDPosition)
-	gps = uavo_list.as_numpy_array(taulabs.uavo.UAVO_GPSPosition)
-	vel = uavo_list.as_numpy_array(taulabs.uavo.UAVO_GPSVelocity)
-	mag = uavo_list.as_numpy_array(taulabs.uavo.UAVO_Magnetometer)
-	baro = uavo_list.as_numpy_array(taulabs.uavo.UAVO_BaroAltitude)
-
-	# set home location as first sample and linearize around that
-	lat0 = gps['Latitude'][0,0]
-	lon0 = gps['Longitude'][0,0]
-	alt0 = gps['Altitude'][0,0]
-
-	T = [alt0+6.378137E6, cos(lat0 / 10e6 * math.pi / 180.0)*(alt0+6.378137E6)]
-
-	STEPS = gyros['time'].size
-	history = numpy.zeros((STEPS,16))
-	history_rpy = numpy.zeros((STEPS,3))
-	times = numpy.zeros((STEPS,1))
-
-	ned_history = numpy.zeros((gps['Latitude'].size,3))
-
-	steps = 0
-	t = gyros['time'][0]
-	gyro_idx = 0
-	accel_idx = 0
-	gps_idx = 0
-	vel_idx = 0
-	mag_idx = 0
-	baro_idx = 0
-
-	sim = PyINS()
-	sim.prepare()
-
-	dT = numpy.mean(numpy.diff(gyros['time']))
-
-	for gyro_idx in numpy.arange(STEPS):
-
-		t = gyros['time'][gyro_idx]
-		steps = gyro_idx
-		accel_idx = (numpy.abs(accels['time']-t)).argmin()
-			
-		gyros_dat = numpy.array([gyros['x'][gyro_idx],gyros['y'][gyro_idx],gyros['z'][gyro_idx]]).T[0]
-		accels_dat = numpy.array([accels['x'][accel_idx],accels['y'][accel_idx],accels['z'][accel_idx]]).T[0]
-
-		gyros_dat = gyros_dat / 180 * math.pi
-		sim.predict(gyros_dat,accels_dat,dT)
-
-		if (gps_idx < gps['time'].size) and (gps['time'][gps_idx] < t):
-			pos = [(gps['Latitude'][gps_idx,0] - lat0) / 10e6 * math.pi / 180.0 * T[0], (gps['Longitude'][gps_idx,0] - lon0)  / 10e6 * math.pi / 180.0 * T[1], -gps['Altitude'][gps_idx,0]]
-			sim.correction(pos=pos)
-			ned_history[gps_idx,0] = pos[0]
-			ned_history[gps_idx,1] = pos[1]
-			ned_history[gps_idx,2] = pos[2]
-			gps_idx = gps_idx + 1
-
-		if (vel_idx < vel['time'].size) and (vel['time'][vel_idx] < t):
-			sim.correction(vel=[vel['North'][vel_idx,0], vel['East'][vel_idx,0], vel['Down'][vel_idx,0]])
-			vel_idx = vel_idx + 1
-
-		if (mag_idx < mag['time'].size) and (mag['time'][mag_idx] < t):
-			sim.correction(mag=[mag['x'][mag_idx,0], mag['y'][mag_idx,0], mag['z'][mag_idx,0]])
-			mag_idx = mag_idx + 1
-
-		if (baro_idx < baro['time'].size) and (baro['time'][baro_idx] < t):
-			sim.correction(baro=baro['Altitude'][baro_idx,0])
-			baro_idx = baro_idx + 1
-
-		history[steps,:] = sim.state
-		history_rpy[steps,:] = quat_rpy(sim.state[6:10])
-		times[steps] = t
-
-	print "Plotting results"
-	
-	import matplotlib.pyplot as plt
-	fig, ax = plt.subplots(2,3)
-
-	ax[0][0].cla()
-	ax[0][0].plot(gps['time'],ned_history[:,0],'k',gps['time'],ned_history[:,1],'k',gps['time'],ned_history[:,2],'k--', baro['time'], -baro['Altitude'],'k')
-	ax[0][0].plot(times,history[:,0:3])
-	ax[0][0].set_title('Position')
-	plt.sca(ax[0][0])
-	plt.ylabel('m')
-	ax[0][1].cla()
-	ax[0][1].plot(vel['time'],vel['North'],'k',vel['time'],vel['East'],'k')
-	ax[0][1].plot(times,history[:,3:6])
-	ax[0][1].set_title('Velocity')
-	plt.sca(ax[0][1])
-	plt.ylabel('m/s')
-	#plt.ylim(-2,2)
-	ax[1][0].cla()
-	ax[1][0].plot(attitude['time'],attitude['Roll'],'k',attitude['time'],attitude['Pitch'],'k',attitude['time'],attitude['Yaw'],'k')
-	ax[1][0].plot(times,history_rpy[:,:])
-	ax[1][0].set_title('Attitude')
-	plt.sca(ax[1][0])
-	plt.ylabel('Angle (Deg)')
-	plt.xlabel('Time (s)')
-	#plt.ylim(-1.1,1.1)
-	ax[1][1].cla()
-	ax[1][1].plot(times,history[:,10:])
-	ax[1][1].set_title('Biases')
-	plt.sca(ax[1][1])
-	plt.ylabel('Bias (rad/s)')
-	plt.xlabel('Time (s)')
-
-	ax[0][2].cla()
-	ax[0][2].plot(gyros['time'],gyros['x'],gyros['time'],gyros['y'],gyros['time'],gyros['z'])
-	ax[1][2].cla()
-	ax[1][2].plot(accels['time'],accels['x'],accels['time'],accels['y'],accels['time'],accels['z'])
-
-	plt.draw()
-	plt.show()
-
-	return ins
+		self.state = ins.correction(Z, sensors)
 
 def test():
 	""" test the INS with simulated data
