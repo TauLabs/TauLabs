afae28047d1c539f4a4b6399300be68ea93aac16
James Cotton <peabody124@gmail.com>
2014-12-27 16:08:18 -0600

    DSM/UnitTest: run tests on recorded log files

--- flight/tests/dsm/unittest.cpp
+++ flight/tests/dsm/unittest.cpp
@@ -59,15 +59,21 @@ protected:
  }
  void pack_channels_10bit(uint16_t channels[DSM_CHANNELS_PER_FRAME], struct pios_dsm_state *state, bool frame);
  void pack_channels_11bit(uint16_t channels[DSM_CHANNELS_PER_FRAME], struct pios_dsm_state *state, bool frame);
+ int validate_file(const char *fn, int resolution, int channels);
+ int get_packet(FILE *fid, uint8_t *buf);
  struct pios_dsm_state *state;
  struct pios_dsm_dev dev;
 };
+
+const int idx[] = {1,5,2,3,0,7,6,1,5,2,3,4,8,9};
 
 //! pack data into DSM2 10 bit packets
 void DsmTest::pack_channels_10bit(uint16_t channels[DSM_CHANNELS_PER_FRAME], struct pios_dsm_state *state, bool frame)
 {
   for (int i = 0; i < DSM_CHANNELS_PER_FRAME; i++) {
-    uint16_t word = (frame ? 0x8000 : 0) | ((i & 0x000F) << 10) | (channels[i] & 0x03FF);
+    uint16_t j = idx[i + DSM_CHANNELS_PER_FRAME*frame];
+    uint16_t val = channels[j];
+    uint16_t word = ((frame & (i == 0)) ? 0x8000 : 0) | ((j & 0x000F) << 10) | (val & 0x03FF);
     state->received_data[2 + i * 2 + 1] = word & 0x00FF;
     state->received_data[2 + i * 2] = (word >> 8) & 0x00FF;
   }
@@ -77,7 +83,9 @@ void DsmTest::pack_channels_10bit(uint16_t channels[DSM_CHANNELS_PER_FRAME], str
 void DsmTest::pack_channels_11bit(uint16_t channels[DSM_CHANNELS_PER_FRAME], struct pios_dsm_state *state, bool frame)
 {
   for (int i = 0; i < DSM_CHANNELS_PER_FRAME; i++) {
-    uint16_t word = (frame ? 0x8000 : 0) | ((i & 0x000F) << 11) | (channels[i] & 0x07FF);
+    uint16_t j = idx[i + DSM_CHANNELS_PER_FRAME*frame];
+    uint16_t val = channels[j];
+    uint16_t word = ((frame  & (i == 0)) ? 0x8000 : 0) | ((j & 0x000F) << 11) | (val & 0x07FF);
     state->received_data[2 + i * 2 + 1] = word & 0x00FF;
     state->received_data[2 + i * 2] = (word >> 8) & 0x00FF;
   }
@@ -98,41 +106,136 @@ TEST_F(DsmTest, Invalid) {
   EXPECT_EQ(-2, PIOS_DSM_UnrollChannels(&dev));
 }
 
-TEST_F(DsmTest, DSM2_10BIT) {
-  uint16_t channels[DSM_CHANNELS_PER_FRAME] = {512, 513, 514, 515, 516, 517, 518};
+TEST_F(DsmTest, DSM_10BIT) {
+  uint16_t channels[PIOS_DSM_NUM_INPUTS] = {512, 513, 514, 515, 516, 517, 518, 0, 0, 0, 0, 0};
   pack_channels_10bit(channels, state, false);
+  EXPECT_EQ(0, PIOS_DSM_UnrollChannels(&dev));
+  pack_channels_10bit(channels, state, true);
   EXPECT_EQ(0, PIOS_DSM_UnrollChannels(&dev));
 
   EXPECT_EQ(10, PIOS_DSM_GetResolution(&dev));
   verify_channels(channels, state->channel_data);
 }
 
-TEST_F(DsmTest, DSM2_11BIT) {
-  uint16_t channels[DSM_CHANNELS_PER_FRAME] = {512, 513, 514, 515, 516, 517, 518};
+TEST_F(DsmTest, DSM_11BIT) {
+  uint16_t channels[PIOS_DSM_NUM_INPUTS] = {512, 513, 514, 515, 516, 517, 518, 0, 0, 0, 0, 0};
   pack_channels_11bit(channels, state, false);
+  EXPECT_EQ(0, PIOS_DSM_UnrollChannels(&dev));
+  pack_channels_11bit(channels, state, true);
   EXPECT_EQ(0, PIOS_DSM_UnrollChannels(&dev));
 
   EXPECT_EQ(11, PIOS_DSM_GetResolution(&dev));
   verify_channels(channels, state->channel_data);
 }
 
-TEST_F(DsmTest, DSM2_10BIT_MISMATCH) {
-  // Once the resolution is detected as 11 bit, processing
-  // 10 bit frames should fail
-  dev.resolution = DSM_11BIT;
+int DsmTest::get_packet(FILE *fid, uint8_t *buf)
+{
+  double t;
+  uint8_t val;
 
-  uint16_t channels[DSM_CHANNELS_PER_FRAME] = {512, 513, 514, 515, 516, 517, 518};
-  pack_channels_10bit(channels, state, false);
-  EXPECT_EQ(-1, PIOS_DSM_UnrollChannels(&dev));
+  for (int i = 0; i < DSM_FRAME_LENGTH; i++) {
+    if (fscanf(fid, "%lf,%x,,", &t, (unsigned int*) &val) == 2) {
+      buf[i] = val;
+    } else
+      return -1;
+  }
+  return 0;
 }
 
-TEST_F(DsmTest, DSM2_11BIT_MISMATCH) {
-  // Once the resolution is detected as 10 bit, processing
-  // 11 bit frames should fail
-  dev.resolution = DSM_10BIT;
 
-  uint16_t channels[DSM_CHANNELS_PER_FRAME] = {512, 513, 514, 515, 516, 517, 518};
-  pack_channels_11bit(channels, state, false);
-  EXPECT_EQ(-1, PIOS_DSM_UnrollChannels(&dev));
+int DsmTest::validate_file(const char *fn, int resolution, int channels)
+{
+  FILE *fid = fopen(fn, "r");
+  char *line = NULL;
+  size_t len = 0;
+
+  // throwaway intro line
+  getline(&line, &len, fid);
+
+  const int MIN = (resolution == 11) ? 340 : 150;
+  const int MAX = (resolution == 11) ? 2048 : 1024;
+  if (resolution == 11)
+  // warm up parser
+  get_packet(fid, state->received_data);
+  PIOS_DSM_UnrollChannels(&dev);
+  //EXPECT_EQ(0, PIOS_DSM_UnrollChannels(&dev));
+
+  get_packet(fid, state->received_data);
+  PIOS_DSM_UnrollChannels(&dev);
+  //EXPECT_EQ(0, PIOS_DSM_UnrollChannels(&dev));
+
+  while(get_packet(fid, state->received_data) == 0) {
+    EXPECT_EQ(0, PIOS_DSM_UnrollChannels(&dev));
+    EXPECT_EQ(resolution, PIOS_DSM_GetResolution(&dev));
+
+    bool valid[PIOS_DSM_NUM_INPUTS];
+    for (int i = 0; i < PIOS_DSM_NUM_INPUTS; i++) {
+      // this file only has 7 channels
+      valid[i] = ((i >= channels) && dev.state.channel_data[i] == 0) ||
+              ((i < channels) && ((dev.state.channel_data[i] > MIN) && (dev.state.channel_data[i] <= MAX)));
+      //fprintf(stdout, "%d %d %d %d\r\n", i, valid[i], dev.state.channel_data[i], channels);
+      EXPECT_TRUE(valid[i]);
+
+      if (!valid[i]) {
+        for (int i = 0; i < PIOS_DSM_NUM_INPUTS; i++) {
+          fprintf(stdout, "%d, ", dev.state.channel_data[i]);
+        }
+        fprintf(stdout, "\r\n");
+
+        fclose(fid);
+        return 0;
+      }
+    }
+
+    /*for (int i = 0; i < PIOS_DSM_NUM_INPUTS; i++) {
+      fprintf(stdout, "%d, ", dev.state.channel_data[i]);
+    }
+    fprintf(stdout, "\r\n");*/
+  }
+
+  fclose(fid);
+
+  return 0;
 }
 
+TEST_F(DsmTest, DX7_DSM2) {
+
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX7_11msDSM2.txt",11,8);
+
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX7_22msDSM2.txt",11,8);
+}
+
+TEST_F(DsmTest, DX7_DSMX) {
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX7_11msDSMX.txt",11,8);
+
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX7_22msDSMX.txt",11,8);
+}
+
+TEST_F(DsmTest, DX18_DSM2) {
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX18_11msDSM2_2048res.txt",11,10);
+
+  // Note: even though this file is named like it has 10 bit
+  // resolution, it appears to differ in having 12 channels
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX18_22msDSM2_1024res.txt",11,12);
+
+  // Note: we do not decode the XPlus channels but it is
+  // important to make sure they are handled appropriately
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX18_22msDSM2_XPlus_1024res.txt",11,12);
+}
+
+TEST_F(DsmTest, DX18_DSMX) {
+  validate_file("DX18_22msDSMX.txt",11,12);
+
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX18_11msDSMX.txt",11,10);
+
+  PIOS_DSM_Reset(&dev);
+  validate_file("DX18_22msDSMX_XPlus.txt",11,12);
+}
