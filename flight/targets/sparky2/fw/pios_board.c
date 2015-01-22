fbe36991bff75b885ec1165298edad7e1fb09383
James Cotton <peabody124@gmail.com>
2015-01-22 10:34:54 -0600

    Sparky2: temporary commit to use internal flash

--- flight/targets/sparky2/fw/pios_board.c
+++ flight/targets/sparky2/fw/pios_board.c
@@ -339,8 +339,6 @@ void PIOS_Board_Init(void) {
 
 #if defined(PIOS_INCLUDE_FLASH)
 	/* Inititialize all flash drivers */
-	if (PIOS_Flash_Jedec_Init(&pios_external_flash_id, pios_spi_telem_flash_id, 1, &flash_m25p_cfg) != 0)
-		panic(1);
 	PIOS_Flash_Internal_Init(&pios_internal_flash_id, &flash_internal_cfg);
 
 	/* Register the partition table */
@@ -351,8 +349,6 @@ void PIOS_Board_Init(void) {
 
 	/* Mount all filesystems */
 	if (PIOS_FLASHFS_Logfs_Init(&pios_uavo_settings_fs_id, &flashfs_settings_cfg, FLASH_PARTITION_LABEL_SETTINGS))
-		panic(1);
-	if (PIOS_FLASHFS_Logfs_Init(&pios_waypoints_settings_fs_id, &flashfs_waypoints_cfg, FLASH_PARTITION_LABEL_WAYPOINTS) != 0)
 		panic(1);
 #endif	/* PIOS_INCLUDE_FLASH */
 
