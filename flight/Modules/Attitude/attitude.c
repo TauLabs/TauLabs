5623c954b08b1a87d15b1907b133e1e9b7cd4d47
James Cotton <peabody124@gmail.com>
2015-05-10 14:06:24 -0600

    Attitude: take barometric altitude on powerup as zero
    Previously we tried to compare the home location altitude
    against the current gps altitude and preserve that offset.
    In practice, this is inconvenient and we should take the
    powerup altitude as zero.

--- flight/Modules/Attitude/attitude.c
+++ flight/Modules/Attitude/attitude.c
@@ -1101,7 +1101,7 @@ static int32_t updateAttitudeINSGPS(bool first_run, bool outdoor_mode)
 			getNED(&gpsData, NED);
 
 			// Initialize barometric offset to current GPS NED coordinate
-			baro_offset = -NED[2] - baroData.Altitude;
+			baro_offset = -baroData.Altitude;
 
 			INSSetState(NED, zeros, q, zeros, zeros);
 		} 
