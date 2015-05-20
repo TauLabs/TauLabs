fb5f7c8d641182312dc1067d909c9634ca89672b
jihlein <jihlein@hotmail.com>
2015-05-19 20:08:52 -0600

    20150519 - Code review format changes.

--- ground/gcs/src/plugins/coreplugin/iboardtype.cpp
+++ ground/gcs/src/plugins/coreplugin/iboardtype.cpp
@@ -11,64 +11,45 @@ QStringList IBoardType::queryChannelBanks()
 {
     QStringList banksStringList = QStringList();
 
-    foreach(QVector<qint32> channelBank, channelBanks) 
-	{
-        quint8 sequential = false;
+    foreach(QVector<qint32> channelBank, channelBanks) {
+        bool sequential = false;
         QString banksString;
 
-		for (int i = 0; i < channelBank.size(); i++) 
-		{
-			if (i == 0)															 // First channel in bank
-			{
+		for (int i = 0; i < channelBank.size(); i++) {
+			if (i == 0)	{														 // First channel in bank
 				banksString.append(QString("%1").arg(channelBank[i]));
 				
-				if (channelBank.size() > 1)
-				{
-					if ((channelBank[i] + 1) == channelBank[i + 1])
-					{
-						banksString.append(QString("-"));							 // Next channel is sequential, add '-' to the string
+				if (channelBank.size() > 1)	{									 // If more than 1 channel in bank, add separator
+					if ((channelBank[i] + 1) == channelBank[i + 1])	{
+						banksString.append(QString("-"));						 // Next channel is sequential, add '-' to the string
 						sequential = true;
 					}
-					else
-					{
-						banksString.append(QString(","));							 // Next channel is not sequential, add ',' to the string
+					else {
+						banksString.append(QString(","));						 // Next channel is not sequential, add ',' to the string
 						sequential = false;
 					}
 				}
 			}
-					
-			///////////////////////////
-				
 			else if (i == channelBank.size() - 1)								 // Last channel in bank
 				banksString.append(QString("%1").arg(channelBank[i]));
-					
-			///////////////////////////
-				
-			else																 // Mid channel(s) in bank
-			{
-				if (sequential)
-				{
-					if ((channelBank[i] + 1) == channelBank[i + 1])				 // Still sequential channels, nothing to add to string
-					{
+			else {																 // Mid channel(s) in bank
+				if (sequential)	{
+					if ((channelBank[i] + 1) == channelBank[i + 1]) {			 // Still sequential channels, nothing to add to string
 						sequential = true;
 					}
-					else
-					{
+					else {
 						banksString.append(QString("%1,").arg(channelBank[i]));  // End of sequence, add channel and ',' to string
 						sequential = false;
 					}
 				}
-				else
-				{
+				else {
 					banksString.append(QString("%1").arg(channelBank[i]));
 
-					if ((channelBank[i] + 1) == channelBank[i + 1])
-					{
+					if ((channelBank[i] + 1) == channelBank[i + 1])	{
 						banksString.append(QString("-"));						 // Next channel is sequential, add '-' to the string
 						sequential = true;
 					}
-					else
-					{
+					else {
 						banksString.append(QString(","));						 // Next channel is not sequential, add ',' to the string
 						sequential = false;
 					}
