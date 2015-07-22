079e1e9874c940f2346bdb314040f2136446d1e1
guilhermito <guilhermito.santiago@gmail.com>
2015-07-23 00:22:05 +0100

    GCS/PathPlanner-fully qualify needed functions with std namespace

--- ground/gcs/src/plugins/pathplanner/algorithms/pathfillet.cpp
+++ ground/gcs/src/plugins/pathplanner/algorithms/pathfillet.cpp
@@ -23,7 +23,8 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <QInputDialog>
 #include <cmath>
 #include <algorithms/pathfillet.h>
