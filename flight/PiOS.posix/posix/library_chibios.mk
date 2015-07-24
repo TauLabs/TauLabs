d9dc73d50a1a8b1059a2b60721036c9c465584e2
Michael Lyle <mlyle@lyle.org>
2015-07-24 09:57:17 -0700

    simulation: conditionalize compile flags by arch

--- flight/PiOS.posix/posix/library_chibios.mk
+++ flight/PiOS.posix/posix/library_chibios.mk
@@ -1,6 +1,8 @@
 #
-# Rules to (help) build the F4xx device support.
+# Rules to (help) build the Posix-targeted code.
 #
+
+include $(MAKE_INC_DIR)/system-id.mk
 
 #
 # Directory containing this makefile
@@ -19,5 +21,22 @@ PIOS_DEVLIB			:=	$(dir $(lastword $(MAKEFILE_LIST)))
 #
 # Compiler options implied by posix
 #
-ARCHFLAGS			+= -DARCH_POSIX -pthread
+ARCHFLAGS			+= -DARCH_POSIX
 ARCHFLAGS			+= -D_GNU_SOURCE
+
+# The posix code is not threaded but it is possibly re-entrant into the
+# system libraries because of task switching.  As a result, request the
+# re-entrant libraries and -D_REENTRANT
+ifdef MACOSX
+# If we are building with clang, it doesn't like the pthread argument being
+# passed at link time.  Annoying.
+CONLYFLAGS			+= -pthread
+else
+ARCHFLAGS			+= -pthread
+endif
+
+# Build 32 bit code.
+ifdef AMD64
+ARCHFLAGS                      += -m32
+endif
+
