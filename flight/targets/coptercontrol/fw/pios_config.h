05d57e8ad79a475383b6d2475cb37f30a4d75d5e
Dustin Sallings <dustin@spy.net>
2015-10-23 17:23:20 -0700

    MSP support for cc3d
    This provides MSP (multiwii serial protocol) support on the main port or
    flex port of the cc3d with the intention of being useful for OSDs.

--- flight/targets/coptercontrol/fw/pios_config.h
+++ flight/targets/coptercontrol/fw/pios_config.h
@@ -63,6 +63,7 @@
 #define PIOS_INCLUDE_GPS_NMEA_PARSER /* Include the NMEA protocol parser */
 #define PIOS_INCLUDE_GPS_UBX_PARSER  /* Include the UBX protocol parser */
 #define PIOS_INCLUDE_MAVLINK
+#define PIOS_INCLUDE_MSP_BRIDGE
 #define PIOS_INCLUDE_FRSKY_SENSOR_HUB
 #define PIOS_INCLUDE_LIGHTTELEMETRY
 
