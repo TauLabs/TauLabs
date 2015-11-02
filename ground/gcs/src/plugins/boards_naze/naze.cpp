9a8c3cbbc7e84682d18fd55caa36abfa40179a3e
Michael Corcoran <mrc93@uclive.ac.nz>
2015-11-02 18:39:52 +1300

    Naze: Add setup wizard support Can't do input configuration yet as board reset isn't handled correctly for serial devices.

--- ground/gcs/src/plugins/boards_naze/naze.cpp
+++ ground/gcs/src/plugins/boards_naze/naze.cpp
@@ -129,3 +129,89 @@ int Naze::queryMaxGyroRate()
         return 500;
     }
 }
+
+//! Determine if this board supports configuring the receiver
+bool Naze::isInputConfigurationSupported()
+{
+    // doesn't work for now since the board can't reconnect  automatically after reboot
+    return false;
+}
+
+/**
+ * Configure the board to use a receiver input type on a port number
+ * @param type the type of receiver to use
+ * @param port_num which input port to configure (board specific numbering)
+ * @return true if successfully configured or false otherwise
+ */
+bool Naze::setInputOnPort(enum InputType type, int port_num)
+{
+    if (port_num != 0)
+        return false;
+
+    ExtensionSystem::PluginManager *pm = ExtensionSystem::PluginManager::instance();
+    UAVObjectManager *uavoManager = pm->getObject<UAVObjectManager>();
+    HwNaze *hwNaze = HwNaze::GetInstance(uavoManager);
+    Q_ASSERT(hwNaze);
+    if (!hwNaze)
+        return false;
+
+    HwNaze::DataFields settings = hwNaze->getData();
+
+    switch(type) {
+    case INPUT_TYPE_PPM:
+        settings.RcvrPort = HwNaze::RCVRPORT_PPM;
+        break;
+    case INPUT_TYPE_DSM:
+        settings.RcvrPort = HwNaze::RCVRPORT_PPMSERIAL;
+        settings.RcvrSerial = HwNaze::RCVRSERIAL_DSM;
+        break;
+    case INPUT_TYPE_HOTTSUMD:
+        settings.RcvrPort = HwNaze::RCVRPORT_PPMSERIAL;
+        settings.RcvrSerial = HwNaze::RCVRSERIAL_HOTTSUMD;
+        break;
+    default:
+        return false;
+    }
+
+    // Apply these changes
+    hwNaze->setData(settings);
+
+    return true;
+}
+
+/**
+ * @brief Naze::getInputOnPort fetch the currently selected input type
+ * @param port_num the port number to query (must be zero)
+ * @return the selected input type
+ */
+enum Core::IBoardType::InputType Naze::getInputOnPort(int port_num)
+{
+    if (port_num != 0)
+        return INPUT_TYPE_UNKNOWN;
+
+    ExtensionSystem::PluginManager *pm = ExtensionSystem::PluginManager::instance();
+    UAVObjectManager *uavoManager = pm->getObject<UAVObjectManager>();
+    HwNaze *hwNaze = HwNaze::GetInstance(uavoManager);
+    Q_ASSERT(hwNaze);
+    if (!hwNaze)
+        return INPUT_TYPE_UNKNOWN;
+
+    HwNaze::DataFields settings = hwNaze->getData();
+
+    switch(settings.RcvrPort) {
+    case HwNaze::RCVRPORT_PPM:
+        return INPUT_TYPE_PPM;
+    case HwNaze::RCVRPORT_PPMSERIAL:
+        switch(settings.RcvrSerial) {
+        case HwNaze::RCVRSERIAL_DSM:
+            return INPUT_TYPE_DSM;
+        case HwNaze::RCVRSERIAL_HOTTSUMD:
+            return INPUT_TYPE_HOTTSUMD;
+        default:
+            // can still use PPM
+            return INPUT_TYPE_PPM;
+        }
+    default:
+        return INPUT_TYPE_UNKNOWN;
+    }
+}
