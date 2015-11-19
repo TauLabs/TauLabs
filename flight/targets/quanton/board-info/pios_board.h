860735429874e1cbf69e5c79f524b805708f69d3
Martin Luessi <mluessi@gmail.com>
2015-11-19 14:29:52 -0500

    Quanton: Use new ADC

--- flight/targets/quanton/board-info/pios_board.h
+++ flight/targets/quanton/board-info/pios_board.h
@@ -233,24 +233,8 @@ extern uintptr_t pios_com_debug_id;
 
 // PIOS_ADC_PinGet(0) = IN7
 // PIOS_ADC_PinGet(1) = IN8
-//-------------------------
-#define PIOS_DMA_PIN_CONFIG                                                                   \
-{                                                                                             \
-	{ GPIOA, GPIO_Pin_0,     ADC_Channel_0 },                                                 \
-	{ GPIOA, GPIO_Pin_1,     ADC_Channel_1 },                                                 \
-	{ NULL,  0,              ADC_Channel_Vrefint },           /* Voltage reference */         \
-	{ NULL,  0,              ADC_Channel_TempSensor },        /* Temperature sensor */        \
-	{ NULL,  0,              ADC_Channel_TempSensor },        /* Temperature sensor */        \
-}
-
-/* we have to do all this to satisfy the PIOS_ADC_MAX_SAMPLES define in pios_adc.h */
-/* which is annoying because this then determines the rate at which we generate buffer turnover events */
-/* the objective here is to get enough buffer space to support 100Hz averaging rate */
-#define PIOS_ADC_NUM_CHANNELS           5
 #define PIOS_ADC_MAX_OVERSAMPLING       2
-#define PIOS_ADC_USE_ADC2               0
-
-#define VREF_PLUS			3.3
+#define VREF_PLUS                     3.3
 
 //-------------------------
 // USB
