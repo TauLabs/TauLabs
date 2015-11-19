5153be8c05d41094bf67848fd19310d41c8f2f11
Martin Luessi <mluessi@gmail.com>
2015-11-19 15:49:43 -0500

    Sparky: Use new ADC

--- flight/targets/sparky/board-info/board_hw_defs.c
+++ flight/targets/sparky/board-info/board_hw_defs.c
@@ -1420,12 +1420,12 @@ static struct pios_internal_adc_cfg internal_adc_cfg = {
 	.half_flag = DMA1_IT_HT1,
 	.full_flag = DMA1_IT_TC1,
 	.oversampling = 32,
-	.number_of_used_pins = 3,
-	.adc_pins = (struct adc_pin[]){
+	.adc_pins = {
 		{GPIOA,GPIO_Pin_1,ADC_Channel_2,true},
 		{GPIOA,GPIO_Pin_4,ADC_Channel_1,false},
 		{GPIOA,GPIO_Pin_7,ADC_Channel_4,false},
 	},
+	.adc_pin_count = 3,
 	.adc_dev_master = ADC1,
 	.adc_dev_slave = ADC2,
 };
--- flight/targets/sparky/fw/pios_board.c
+++ flight/targets/sparky/fw/pios_board.c
@@ -467,7 +467,7 @@ void PIOS_Board_Init(void)
 
 #if defined(PIOS_INCLUDE_ADC)
 	if (number_of_adc_ports > 0) {
-		internal_adc_cfg.number_of_used_pins = number_of_adc_ports;
+		internal_adc_cfg.adc_pin_count = number_of_adc_ports;
 		uint32_t internal_adc_id;
 		if (PIOS_INTERNAL_ADC_Init(&internal_adc_id, &internal_adc_cfg) < 0)
 			PIOS_Assert(0);
