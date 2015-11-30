30a9c3a7b1b5a968e7a367879e8ad9e5092cb3d8
James Cotton <peabody124@gmail.com>
2015-11-30 14:27:49 -0600

    PIOS DACBEEP: support changing output frequency

--- flight/PiOS/STM32F4xx/pios_dacbeep.c
+++ flight/PiOS/STM32F4xx/pios_dacbeep.c
@@ -2,13 +2,13 @@
  ******************************************************************************
  * @addtogroup PIOS PIOS Core hardware abstraction layer
  * @{
- * @addtogroup   PIOS_DACBEEP DAC Beep Functions
+  * @addtogroup   PIOS_DACBEEP DAC Beep Code Functions
  * @brief PIOS interface for DAC Beep implementation
  * @{
  *
  * @file       pios_dacbeep.c
  * @author     Tau Labs, http://taulabs.org, Copyright (C) 2015
- * @brief      Generates a sequence of pulsed beeps
+ * @brief      Generates Bel202 encoded serial data on the DAC channel
  * @see        The GNU Public License (GPL) Version 3
  *
  *****************************************************************************/
@@ -30,26 +30,22 @@
 
 /* Project Includes */
 #include "pios.h"
-#if defined(PIOS_INCLUDE_DACBEEP)
+#if defined(PIOS_INCLUDE_DAC_BEEPS)
 
-#include "pios_dacbeep_priv.h"
+#include "pios_fskdac_priv.h"
 
 #if defined(PIOS_INCLUDE_FREERTOS)
 #include "FreeRTOS.h"
 #endif /* defined(PIOS_INCLUDE_FREERTOS) */
 
-
-/* Provide a COM driver */
-static int32_t pios_dacbeep_playtone(struct pios_fskdac_dev * fskdac_dev, uint32_t freq)
-
-
 enum pios_dacbeep_dev_magic {
-	PIOS_DACBEEP_DEV_MAGIC = 0x1453835A,
+	PIOS_DACBEEP_DEV_MAGIC = 0x3A53834A,
 };
 
 struct pios_dacbeep_dev {
 	enum pios_dacbeep_dev_magic     magic;
-	const struct pios_dacbeep_config * cfg;
+	const struct pios_fskdac_config * cfg;
+	bool cycles_remaining;
 };
 
 // Lookup tables for symbols
@@ -69,10 +65,31 @@ const uint16_t MARK_SAMPLES[] = {
 1378, 1471, 1565, 1660, 1756, 1853, 1950, 2047,
 };
 
+const uint16_t SPACE_SAMPLES[] = {
+2048, 2242, 2435, 2624, 2808, 2984, 3151, 3307, 3451, 3581, 
+3696, 3795, 3877, 3941, 3986, 4013, 4020, 4008, 3977, 3926, 
+3858, 3772, 3669, 3550, 3416, 3269, 3110, 2941, 2763, 2578, 
+2387, 2194, 1999, 1804, 1612, 1424, 1242, 1068, 904, 751, 
+610, 484, 373, 278, 201, 141, 100, 79, 76, 93, 
+129, 184, 257, 347, 455, 577, 714, 865, 1026, 1198, 
+1378, 1565, 1756, 1950, 2145, 2339, 2530, 2717, 2897, 3069, 
+3230, 3381, 3518, 3640, 3748, 3838, 3911, 3966, 4002, 4019, 
+4016, 3995, 3954, 3894, 3817, 3722, 3611, 3485, 3344, 3191, 
+3027, 2853, 2671, 2483, 2291, 2096, 1901, 1708, 1517, 1332, 
+1154, 985, 826, 679, 545, 426, 323, 237, 169, 118, 
+87, 75, 82, 109, 154, 218, 300, 399, 514, 644, 
+788, 944, 1111, 1287, 1471, 1660, 1853, 2047,
+};
+const uint32_t SAMPLES_PER_BIT = NELEMENTS(MARK_SAMPLES);
+
+
 #define   DAC_DHR12R1_ADDR  0x40007408
 
 static bool PIOS_DACBEEP_validate(struct pios_dacbeep_dev * dacbeep_dev)
 {
+	if (dacbeep_dev == NULL)
+		return false;
+
 	return (dacbeep_dev->magic == PIOS_DACBEEP_DEV_MAGIC);
 }
 
@@ -80,7 +97,7 @@ static struct pios_dacbeep_dev * PIOS_DACBEEP_alloc(void)
 {
 	struct pios_dacbeep_dev * dacbeep_dev;
 
-	fskdac_dev = (struct pios_dacbeep_dev *)PIOS_malloc(sizeof(*dacbeep_dev));
+	dacbeep_dev = (struct pios_dacbeep_dev *)PIOS_malloc(sizeof(*dacbeep_dev));
 	if (!dacbeep_dev) return(NULL);
 
 	memset(dacbeep_dev, 0, sizeof(*dacbeep_dev));
@@ -93,14 +110,14 @@ struct pios_dacbeep_dev * g_dacbeep_dev;
 /**
 * Initialise a single USART device
 */
-int32_t PIOS_DACBEEP_Init(uintptr_t * dacbeep_id, const struct pios_dacbeep_config * cfg)
+int32_t PIOS_DACBEEP_Init(uintptr_t * dacbeep_id, const struct pios_fskdac_config * cfg)
 {
 	PIOS_DEBUG_Assert(dacbeep_id);
 	PIOS_DEBUG_Assert(cfg);
 
 	struct pios_dacbeep_dev * dacbeep_dev;
 
-	dacbeep_dev = (struct pios_dacbeep_dev *) PIOS_FSKDAC_alloc();
+	dacbeep_dev = (struct pios_dacbeep_dev *) PIOS_DACBEEP_alloc();
 	if (!dacbeep_dev) return -1;
 
 	// Handle for the IRQ
@@ -109,13 +126,6 @@ int32_t PIOS_DACBEEP_Init(uintptr_t * dacbeep_id, const struct pios_dacbeep_conf
 	/* Bind the configuration to the device instance */
 	dacbeep_dev->cfg = cfg; // TODO: use this
 
-	// TODO:
-	// 1. create task, pass dev structure
-	// 2. in task call pios_dacbeep_playtone
-}
-
-static int32_t pios_dacbeep_playtone(struct pios_dacbeep_dev * dacbeep_dev, uint32_t freq)
-{
 	GPIO_InitTypeDef gpio_init;
 	gpio_init.GPIO_Pin  = GPIO_Pin_4;
 	gpio_init.GPIO_Mode = GPIO_Mode_AN;
@@ -130,7 +140,7 @@ static int32_t pios_dacbeep_playtone(struct pios_dacbeep_dev * dacbeep_dev, uint
 
 	// TODO: move into board_hw_defs and cfg structure
 	TIM_TimeBaseStructInit(&TIM6_TimeBase); 
-	TIM6_TimeBase.TIM_Period        = (PIOS_PERIPHERAL_APB1_CLOCK / (freq * SAMPLES_PER_BIT));
+	TIM6_TimeBase.TIM_Period        = (PIOS_PERIPHERAL_APB1_CLOCK / (5000 * SAMPLES_PER_BIT));
 	TIM6_TimeBase.TIM_Prescaler     = 0;
 	TIM6_TimeBase.TIM_ClockDivision = 0;
 	TIM6_TimeBase.TIM_CounterMode   = TIM_CounterMode_Up;
@@ -165,8 +175,8 @@ static int32_t pios_dacbeep_playtone(struct pios_dacbeep_dev * dacbeep_dev, uint
 	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
 	DMA_Init(DMA1_Stream5, &DMA_InitStructure);
 
-	DMA_DoubleBufferModeConfig(fskdac_dev->cfg->dma.tx.channel, (uint32_t)&SPACE_SAMPLES[0], DMA_Memory_0);
-	DMA_DoubleBufferModeCmd(fskdac_dev->cfg->dma.tx.channel, ENABLE);
+	//DMA_DoubleBufferModeConfig(dacbeep_dev->cfg->dma.tx.channel, (uint32_t)&SPACE_SAMPLES[0], DMA_Memory_0);
+	//DMA_DoubleBufferModeCmd(dacbeep_dev->cfg->dma.tx.channel, ENABLE);
 	//DMA_ITConfig(fskdac_dev->cfg->dma.tx.channel, DMA_IT_TC, ENABLE);
 
 	/* Enable DMA1_Stream5 */
@@ -178,11 +188,49 @@ static int32_t pios_dacbeep_playtone(struct pios_dacbeep_dev * dacbeep_dev, uint
 	/* Enable DMA for DAC Channel1 */
 	DAC_DMACmd(DAC_Channel_1, ENABLE);
 
+	*dacbeep_id = (uintptr_t) dacbeep_dev;
 	return 0;
 }
 
+/**
+ * Generate a beep (sine wave) of defined frequency and duration
+ * @param[in] dacbeep_id opaque handle to the dacbeep device
+ * @param[in] freq frequency in Hz for sine wave
+ * @param[in] duration_ms duration in milliseconds
+ * @return 0 if success, -1 if failure
+ */
+int32_t PIOS_DACBEEP_Beep(uintptr_t dacbeep_id, uint32_t freq, uint32_t duration_ms)
+{
+	struct pios_dacbeep_dev * dacbeep_dev = (struct pios_dacbeep_dev *)dacbeep_id;
+	
+	if (!PIOS_DACBEEP_validate(dacbeep_dev))
+		return -1;
 
-#endif /* PIOS_INCLUDE_DACBEEP */
+	// Do not start a beep while previous is running
+	if (dacbeep_dev->cycles_remaining)
+		return -1;
+
+	TIM_Cmd(TIM6, DISABLE);
+	TIM_TimeBaseInitTypeDef TIM6_TimeBase;
+	TIM_TimeBaseStructInit(&TIM6_TimeBase); 
+	TIM6_TimeBase.TIM_Period        = (PIOS_PERIPHERAL_APB1_CLOCK / (freq * SAMPLES_PER_BIT));
+	TIM6_TimeBase.TIM_Prescaler     = 0;
+	TIM6_TimeBase.TIM_ClockDivision = 0;
+	TIM6_TimeBase.TIM_CounterMode   = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM6, &TIM6_TimeBase);
+	TIM_SelectOutputTrigger(TIM6, TIM_TRGOSource_Update);
+	TIM_Cmd(TIM6, ENABLE);
+
+	int32_t cycles = freq * duration_ms / 1000;
+
+	if (false) { // enable once IRQ handler installed and counting down cycles
+		dacbeep_dev->cycles_remaining = cycles;
+	}
+
+	return 0;
+}
+
+#endif /* PIOS_INCLUDE_DAC_BEEPS */
 
 /**
   * @}
---
+++ flight/PiOS/inc/pios_dacbeep_priv.h
@@ -0,0 +1,49 @@
+/**
+ ******************************************************************************
+ * @addtogroup PIOS PIOS Core hardware abstraction layer
+ * @{
+ * @addtogroup   PIOS_DACBEEP DAC Beep Code Functions
+ * @brief PIOS interface for DAC Beep implementation
+ * @{
+ *
+ * @file       pios_dacbeep.h
+ * @author     Tau Labs, http://taulabs.org, Copyright (C) 2015
+ * @brief      Produces a series of beeps out the DAC port
+ * @see        The GNU Public License (GPL) Version 3
+ *
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+#ifndef PIOS_DACBEEP_PRIV_H
+#define PIOS_DACBEEP_PRIV_H
+
+#include <pios.h>
+#include <pios_stm32.h>
+#include "fifo_buffer.h"
+
+//! Configuration structure for FSK DAC
+struct pios_fskdac_config {
+	struct stm32_dma dma;
+	struct pios_tim_channel tim;
+	TIM_TimeBaseInitTypeDef tim_base;
+};
+
+extern int32_t PIOS_DACBEEP_Init(uintptr_t * dacbeep_id, const struct pios_fskdac_config * cfg);
+extern int32_t PIOS_DACBEEP_Beep(uintptr_t dacbeep_id, uint32_t freq, uint32_t durations_ms);
+
+#endif /* PIOS_DACBEEP_PRIV_H */
