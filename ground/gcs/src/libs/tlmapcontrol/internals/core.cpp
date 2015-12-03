ae420b06846f14cbbdc856c63ad9ce71512cb2f4
AJ Christensen <aj@junglistheavy.industries>
2015-12-03 20:18:04 +1300

    Add an explicit uint bitsWithZoom
    * Checked against sizeof(some ridiculous shit) which is uint.
    
    Signed-off-by: James Cotton <peabody124@gmail.com>

--- ground/gcs/src/libs/tlmapcontrol/internals/core.cpp
+++ ground/gcs/src/libs/tlmapcontrol/internals/core.cpp
@@ -379,9 +379,9 @@ namespace internals {
             while( (1 << (numBits-1)) < projection->TileSize().Width()){
                 numBits++;
             }
-			
+            unsigned int bitsWithZoom = numBits + (quint8)maxzoom;
             //Ensure that no matter what the zoom can never exceed the number of bits required to display it
-            if (numBits + (quint8)maxzoom > sizeof(((core::Point *) 0)->X())*8 - 1){ //Remove one because of the sign bit.
+            if (bitsWithZoom > sizeof(((core::Point *) 0)->X())*8 - 1){ //Remove one because of the sign bit.
                 maxzoom = sizeof(((core::Point *) 0)->X())*8 - 1 - numBits;
             }
 			
