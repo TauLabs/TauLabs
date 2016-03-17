9c86528bd9c5f95a1fd3863fb5ff887a840be2c9
Kenn Sebesta <kenn@eissq.com>
2016-03-17 00:46:28 -0400

    FlyingF3: Disable stack check for ChibiOS, undo accidental change
    The intended change was to disable stack checking, not change the compiler
    optimizations.

--- flight/targets/flyingf3/fw/chconf.h
+++ flight/targets/flyingf3/fw/chconf.h
@@ -109,7 +109,7 @@
  * @note    The default is @p TRUE.
  */
 #if !defined(CH_OPTIMIZE_SPEED) || defined(__DOXYGEN__)
-#define CH_OPTIMIZE_SPEED               FALSE
+#define CH_OPTIMIZE_SPEED               TRUE
 #endif
 
 /** @} */
@@ -405,7 +405,7 @@
  *          @p panic_msg variable set to @p NULL.
  */
 #if !defined(CH_DBG_ENABLE_STACK_CHECK) || defined(__DOXYGEN__)
-#define CH_DBG_ENABLE_STACK_CHECK       TRUE
+#define CH_DBG_ENABLE_STACK_CHECK       FALSE
 #endif
 
 /**
