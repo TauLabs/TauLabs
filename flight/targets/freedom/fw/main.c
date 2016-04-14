8b811ebad0373589c7a32cdd0c172201a08674b4
James Cotton <peabody124@gmail.com>
2016-04-13 21:39:55 -0500

    Freedom: enable ChiBios

--- flight/targets/freedom/fw/main.c
+++ flight/targets/freedom/fw/main.c
@@ -67,16 +67,24 @@ int main()
 {
 	/* NOTE: Do NOT modify the following start-up sequence */
 	/* Any new initialization functions should be added in OpenPilotInit() */
-	PIOS_heap_initialize_blocks();
+	PIOS_heap_initialize_blocks();  
+
+#if defined(PIOS_INCLUDE_CHIBIOS)
+	halInit();
+	chSysInit();
+
+	boardInit();
+#endif /* defined(PIOS_INCLUDE_CHIBIOS) */
 
 	/* Brings up System using CMSIS functions, enables the LEDs. */
 	PIOS_SYS_Init();
 	
-	/* For Revolution we use a FreeRTOS task to bring up the system so we can */
-	/* always rely on FreeRTOS primitive */
+	/* For Sparky2 we use a FreeRTOS task to bring up the system so we can */
+	/* always rely on FreeRTOS primitive */	
 	initTaskHandle = PIOS_Thread_Create(initTask, "init", INIT_TASK_STACK, NULL, INIT_TASK_PRIORITY);
 	PIOS_Assert(initTaskHandle != NULL);
 	
+#if defined(PIOS_INCLUDE_FREERTOS)
 	/* Start the FreeRTOS scheduler */
 	vTaskStartScheduler();
 
@@ -87,6 +95,10 @@ int main()
 		PIOS_LED_Toggle(PIOS_LED_HEARTBEAT); \
 		PIOS_DELAY_WaitmS(100); \
 	};
+#elif defined(PIOS_INCLUDE_CHIBIOS)
+	while(1)
+		PIOS_Thread_Sleep(PIOS_THREAD_TIMEOUT_MAX);
+#endif /* defined(PIOS_INCLUDE_CHIBIOS) */
 
 	return 0;
 }
