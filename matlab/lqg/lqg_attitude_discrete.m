e15e95ef3ddbf84916064a66b8eee89e7b066c12
James Cotton <peabody124@gmail.com>
2016-05-08 15:56:57 -0500

    Matlab/LQG Solver: small cosmetic tweaks

--- matlab/lqg/lqg_attitude_discrete.m
+++ matlab/lqg/lqg_attitude_discrete.m
@@ -12,9 +12,9 @@ Ts = 1/400; % the time step
 
 % rotation rate is slowly altered by torques and
 % torques stay the same without input
-b = exp(10.15)*Ts;
+b = exp(10)*Ts;
 bd = exp(-10)*Ts;
-t = Ts/(exp(-2.92) + Ts);
+t = Ts/(exp(-3.5) + Ts);
 
 A = [1 Ts 0; 0 1 b; 0 0 1];
 B = [0; bd; t];
@@ -23,10 +23,31 @@ B = [0; bd; t];
 
 q_angle = 1/(1)^2;
 q_rate = 1/(100)^2;
-q_torque = 1/(1e-2)^2;
+q_torque = 1/(100)^2*(b/Ts)^2;
 Q = diag([q_angle q_rate q_torque]);  % const on state errors
 R = 1;                                % const on inputs
 N = zeros(3,1);                       % cross coupling costs between error and control
+
+% solution is invariant on these transformations as long as we
+% account on the output gains
+k1 = 1e0;
+A(2,3) = A(2,3) * k1;
+B(3) = B(3) / k1;
+R = R / (k1^2);
+Q(3,3) = Q(3,3) * k1^2;
+
+k2 = 1e0;
+A(1,2) = A(1,2) * k2;
+A(2,3) = A(2,3) / k2;
+Q(2,2) = Q(2,2) * k2^2;
+
+k3 = 1e0;
+B(3) = B(3) / k3;
+R = R / (k3 * k3);
+
+k4 = 1e0;
+Q = Q * k4;
+R = R * k4;
 
 % Calculate LQR control weights if we have full state knowledge
 % (which the corresponding kalman filter will provide)
@@ -37,12 +58,10 @@ sys = ss(A,B,diag(ones(1,3)),[],Ts,...
     'OutputName',{'a','w','t'});
 [L,S] = lqr(sys,Q,R,N);
 
-L
+conds = [cond(S) cond(inv(S)+B*inv(R)*B')] / 1e6;
 
-% format matrix for C code
-s1 = sprintf('%ff,',L); s = ['{' s1(1:end-1) sprintf('},\n')]; s
-
-%L = dlqr(A,B,Q,R,N)
+disp(['L = ' sprintf('%f ', L)]);
+disp(['conds = ' sprintf('%f ', conds)]);
 
 %% can use same dynamics to calculate the controller for rate
 % here the integral error should be 0 (instead of setpoint as
