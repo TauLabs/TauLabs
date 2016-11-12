bdb6e1b5a05d86e5abf032b24b9b2779c88a78f1
James Cotton <peabody124@gmail.com>
2016-11-12 16:15:35 -0600

    Colibri: increase flash size to 512k

--- flight/targets/colibri/board-info/board-info.mk
+++ flight/targets/colibri/board-info/board-info.mk
@@ -19,7 +19,7 @@ BL_BANK_SIZE        := 0x00008000  # Should include BD_INFO region (32kb)
 
 # Leave the remaining 16KB and 64KB sectors for other uses
 FW_BANK_BASE        := 0x08020000  # Start of firmware flash (128kb)
-FW_BANK_SIZE        := 0x00040000  # Should include FW_DESC_SIZE (256kb)
+FW_BANK_SIZE        := 0x00080000  # Should include FW_DESC_SIZE (512kb)
 
 FW_DESC_SIZE        := 0x00000064
 
@@ -27,7 +27,7 @@ EE_BANK_BASE        := 0x00000000
 EE_BANK_SIZE        := 0x00000000
 
 EF_BANK_BASE        := 0x08000000  # Start of entire flash image (usually start of bootloader as well)
-EF_BANK_SIZE        := 0x00060000  # Size of the entire flash image (from bootloader until end of firmware)
+EF_BANK_SIZE        := 0x00080000  # Size of the entire flash image (from bootloader until end of firmware)
 
 OSCILLATOR_FREQ     :=  16000000
 SYSCLK_FREQ         := 168000000
